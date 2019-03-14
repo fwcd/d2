ae0b4eccd99205aef4a645e89be1ba7b257f1860
fwcd <fwcdmail@gmail.com>
2019-03-14 16:38:23 +0100

    Switched to the 'Sword' library

--- Package.swift
+++ Package.swift
@@ -8,14 +8,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/nuclearace/SwiftDiscord.git", .revision("77a9cf9e99e64a54b9f61588c2e82675523b5c2b"))
+        .package(url: "https://github.com/Azoy/Sword.git", from: "0.9.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "D2",
-            dependencies: ["SwiftDiscord"]),
+            dependencies: ["Sword"]),
         .testTarget(
             name: "D2Tests",
             dependencies: ["D2"]),
