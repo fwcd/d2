22a41367fb38742182d075446a6633e02d255b98
fwcd <fwcdmail@gmail.com>
2019-03-14 16:21:32 +0100

    Moved discordToken into a swift file to ensure cross-platform compatibility

--- README.md
+++ README.md
@@ -3,8 +3,16 @@ General-purpose Discord API bot.
 
 ## Installation
 * Install the required system dependencies:
-	* Swift
+	* Swift 4.1
     * `sudo apt-get install libopus-dev libsodium-dev libssl1.0-dev`
+* Create a file named `authtoken.swift` in `Sources/D2` containing the API token:
 
-## Usage
-Create a file named `authtoken.txt` in the repository folder containing the API token.
+```swift
+let discordToken = "YOUR_API_TOKEN"
+```
+
+## Building
+* `swift build`
+
+## Running
+* `swift run`
--- Sources/D2/main.swift
+++ Sources/D2/main.swift
@@ -2,9 +2,8 @@ import Foundation
 import SwiftDiscord
 
 func main() throws {
-	let path = Bundle.main.path(forResource: "authtoken", ofType: "txt")
-	let token = try String(contentsOfFile: path!, encoding: String.Encoding.utf8)
-	let client = DiscordClient(token: DiscordToken(stringLiteral: token), delegate: D2ClientDelegate(), configuration: [.log(.info)])
+	// 'discordToken' should be declared in 'authtoken.swift'
+	let client = DiscordClient(token: DiscordToken(stringLiteral: discordToken), delegate: D2ClientDelegate(), configuration: [.log(.info)])
 }
 
 try main()
