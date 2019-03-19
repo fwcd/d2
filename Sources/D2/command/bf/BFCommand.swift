692601729c7c99b99d21f4112c72a9fad8c0bebd
fwcd <fwcdmail@gmail.com>
2019-03-19 20:31:56 +0100

    Improved BF code matching

--- Sources/D2/command/bf/BFCommand.swift
+++ Sources/D2/command/bf/BFCommand.swift
@@ -1,18 +1,25 @@
 import Sword
 
+// The first group matches the BF code
+fileprivate let codePattern = try! Regex(from: "(?:`(?:``(?:\\w*\n)?)?)?([^`]+)`*")
+
 class BFCommand: Command {
 	let description = "Interprets BF code"
 	
 	func invoke(withMessage message: Message, args: String) {
-		do {
-			var interpreter = BFInterpreter()
-			let output = try interpreter.interpret(program: args)
-			
-			message.channel.send("```\n\(output)\n```")
-		} catch BFError.parenthesesMismatch(let msg) {
-			message.channel.send("Parentheses mismatch error: `\(msg)`")
-		} catch {
-			message.channel.send("Error while executing code")
+		if let program = codePattern.firstGroups(in: args)?[1] {
+			do {
+				var interpreter = BFInterpreter()
+				let output = try interpreter.interpret(program: program)
+				
+				message.channel.send("```\n\(output)\n```")
+			} catch BFError.parenthesesMismatch(let msg) {
+				message.channel.send("Parentheses mismatch error: `\(msg)`")
+			} catch {
+				message.channel.send("Error while executing code")
+			}
+		} else {
+			message.channel.send("Syntax error")
 		}
 	}
 }
