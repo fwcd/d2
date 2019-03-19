9790b56097f59c97098f2aec93fc21e3eb9d4f2b
fwcd <fwcdmail@gmail.com>
2019-03-19 20:16:40 +0100

    Updated command matcher to support multi-line commands

--- Sources/D2/CommandHandler.swift
+++ Sources/D2/CommandHandler.swift
@@ -9,7 +9,7 @@ class CommandHandler: ClientHandler {
 		let escapedPrefix = NSRegularExpression.escapedPattern(for: msgPrefix)
 		// The first group matches the command name,
 		// the second matches the arguments (the rest of the message content)
-		commandPattern = try Regex(from: "\(escapedPrefix)(\\w+)(?:\\s+(.*))?")
+		commandPattern = try Regex(from: "\(escapedPrefix)(\\w+)(?:\\s+([\\s\\S]*))?")
 	}
 	
 	func on(createMessage message: Message) {
