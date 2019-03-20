fe9114a0af6a45f0cc97a99780eab21e0e6ac500
fwcd <fwcdmail@gmail.com>
2019-03-20 13:54:38 +0100

    Added required query parameters to UnivISQuery

--- Sources/D2/util/univis/UnivISError.swift
+++ Sources/D2/util/univis/UnivISError.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 enum UnivISError: Error {
 	case urlError(URLComponents)
 }
--- Sources/D2/util/univis/UnivISQuery.swift
+++ Sources/D2/util/univis/UnivISQuery.swift
@@ -4,17 +4,20 @@ struct UnivISQuery {
 	let url: URL
 	
 	init(
+		scheme: String,
 		host: String,
 		path: String,
 		search: UnivISSearchKey,
-		params: [UnivISSearchParameter : String],
-		scheme: String = "https"
+		params: [UnivISSearchParameter : String]
 	) throws {
 		var components = URLComponents()
 		components.scheme = scheme
 		components.host = host
 		components.path = path
-		components.queryItems = params.map { URLQueryItem($0.key.rawValue, $0.value) }
+		components.queryItems = [
+			URLQueryItem(name: "search", value: search.rawValue),
+			URLQueryItem(name: "show", value: "xml")
+		] + params.map { URLQueryItem(name: $0.key.rawValue, value: $0.value) }
 		
 		guard let url = components.url else { throw UnivISError.urlError(components) }
 		self.url = url
