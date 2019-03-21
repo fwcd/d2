3f0ddd9f243c83266c79a47c2b90a0565d95ade8
fwcd <fwcdmail@gmail.com>
2019-03-21 21:09:34 +0100

    Fixed MapQuest URL

--- Sources/D2/util/mapquest/MapQuestStaticMap.swift
+++ Sources/D2/util/mapquest/MapQuestStaticMap.swift
@@ -15,7 +15,7 @@ struct MapQuestStaticMap {
 		
 		mapQuestKey.withCString { key in
 			formattedURL = String(
-				format: "https://www.mapquestapi.com/staticmap/v4/getmap?key=%s&size=%d,%d&type=map&zoom=%d&scalebar=false&traffic=false&center=%.6f,%.6f&pois=mcenter,%.6f,%.6f|",
+				format: "https://www.mapquestapi.com/staticmap/v4/getmap?key=%s&size=%d,%d&type=map&zoom=%d&scalebar=false&traffic=false&center=%.6f,%.6f&pois=mcenter,%.6f,%.6f",
 				key,
 				width,
 				height,
