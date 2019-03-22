1279211b8efd0bd0f7f9270d4757eb7aad373e55
fwcd <fwcdmail@gmail.com>
2019-03-22 20:52:45 +0100

    Add 'hidden' property to Command

--- Sources/D2/command/Command.swift
+++ Sources/D2/command/Command.swift
@@ -3,6 +3,11 @@ import SwiftDiscord
 protocol Command {
 	var description: String { get }
 	var requiredPermissionLevel: PermissionLevel { get }
+	var hidden: Bool { get }
 	
 	func invoke(withMessage message: DiscordMessage, guild: DiscordGuild?, args: String)
+}
+
+extension Command {
+	var hidden: Bool { return false }
 }
