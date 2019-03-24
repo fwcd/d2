01bd990a5ebaaafb7f80878a35192f191bb8efaf
fwcd <fwcdmail@gmail.com>
2019-03-24 17:08:05 +0100

    WIP: Construct command execution pipe in CommandHandler

--- Sources/D2/CommandHandler.swift
+++ Sources/D2/CommandHandler.swift
@@ -1,8 +1,22 @@
 import SwiftDiscord
 import Foundation
 
+fileprivate class PipeComponent {
+	let command: Command
+	let context: CommandContext
+	let args: String
+	var output: CommandOutput? = nil
+	
+	init(command: Command, context: CommandContext, args: String) {
+		self.command = command
+		self.context = context
+		self.args = args
+	}
+}
+
 class CommandHandler: DiscordClientDelegate {
 	private let chainSeparator: String
+	private let pipeSeparator: String
 	private let msgPrefix: String
 	private let commandPattern: Regex
 	private var currentIndex = 0
@@ -10,9 +24,11 @@ class CommandHandler: DiscordClientDelegate {
 	private(set) var registry = CommandRegistry()
 	let permissionManager = PermissionManager()
 	
-	init(withPrefix msgPrefix: String, chainSeparator: String = ";") throws {
+	init(withPrefix msgPrefix: String, chainSeparator: String = ";", pipeSeparator: String = "|") throws {
 		self.msgPrefix = msgPrefix
 		self.chainSeparator = chainSeparator
+		self.pipeSeparator = pipeSeparator
+		
 		// The first group matches the command name,
 		// the second matches the arguments (the rest of the message content)
 		commandPattern = try Regex(from: "(\\w+)(?:\\s+([\\s\\S]*))?")
@@ -25,32 +41,56 @@ class CommandHandler: DiscordClientDelegate {
 		currentIndex += 1
 		
 		if !fromBot && message.content.starts(with: msgPrefix) {
-			for rawCommand in message.content.components(separatedBy: chainSeparator) {
-				let trimmedCommand = rawCommand.trimmingCharacters(in: .whitespacesAndNewlines)
+			// Precedence: Chain < Pipe
+			
+			for rawPipeCommand in message.content.components(separatedBy: chainSeparator) {
+				var pipe = [PipeComponent]()
 				
-				if let groups = commandPattern.firstGroups(in: trimmedCommand) {
-					print("Got command #\(msgIndex): \(groups)")
-					let name = groups[1]
-					let args = groups[2]
+				// Construct the pipe
+				for rawCommand in rawPipeCommand.components(separatedBy: pipeSeparator) {
+					let trimmedCommand = rawCommand.trimmingCharacters(in: .whitespacesAndNewlines)
 					
-					if let command = registry[name] {
-						let hasPermission = permissionManager.user(message.author, hasPermission: command.requiredPermissionLevel)
-						if hasPermission {
-							print("Invoking '\(name)'")
-							
-							let context = CommandContext(
-								guild: client.guildForChannel(message.channelId),
-								registry: registry
-							)
-							command.invoke(withMessage: message, context: context, args: args)
+					if let groups = commandPattern.firstGroups(in: trimmedCommand) {
+						print("Got command #\(msgIndex): \(groups)")
+						let name = groups[1]
+						let args = groups[2]
+						
+						if let command = registry[name] {
+							let hasPermission = permissionManager.user(message.author, hasPermission: command.requiredPermissionLevel)
+							if hasPermission {
+								print("Invoking '\(name)'")
+								
+								let context = CommandContext(
+									guild: client.guildForChannel(message.channelId),
+									registry: registry,
+									message: message
+								)
+								
+								pipe.append(PipeComponents(command: command, context: context, args: args))
+							} else {
+								print("Rejected '\(name)' due to insufficient permissions")
+								message.channel?.send("Sorry, you are not permitted to execute `\(name)`.")
+							}
 						} else {
-							print("Rejected '\(name)' due to insufficient permissions")
-							message.channel?.send("Sorry, you are not permitted to execute `\(name)`.")
+							print("Did not recognize command '\(name)'")
+							message.channel?.send("Sorry, I do not know the command `\(name)`.")
 						}
-					} else {
-						print("Did not recognize command '\(name)'")
-						message.channel?.send("Sorry, I do not know the command `\(name)`.")
 					}
+				}
+				
+				// Setup the pipe outputs
+				if let pipeSink = pipe.last {
+					pipeSink.output = DiscordChannelOutput(channel: message.channel)
+				}
+				
+				for i in stride(from: pipe.count - 2, through: 0, by: -1) {
+					let pipeNext = pipe[i + 1]
+					pipe[i].output = PipeOutput(withSink: pipeNext.command, context: pipeNext.context, args: pipeNext.args, next: pipeNext.output)
+				}
+				
+				// Execute the pipe
+				if let pipeSource = pipe.first {
+					pipeSource.command.invoke(withInput: nil, output: pipeSource.output, context: pipeSource.context, args: pipeSource.args)
 				}
 			}
 		}
--- Sources/D2/command/output/PipeOutput.swift
+++ Sources/D2/command/output/PipeOutput.swift
@@ -4,9 +4,9 @@ class PipeOutput: CommandOutput {
 	private let sink: Command
 	private let context: CommandContext
 	private let args: String
-	private let next: PipeOutput?
+	private let next: CommandOutput?
 	
-	init(withSink sink: Command, context: CommandContext, args: String, next: PipeOutput? = nil) {
+	init(withSink sink: Command, context: CommandContext, args: String, next: CommandOutput? = nil) {
 		self.sink = sink
 		self.args = args
 		self.context = context
@@ -14,6 +14,6 @@ class PipeOutput: CommandOutput {
 	}
 	
 	func append(_ message: DiscordMessage) {
-		sink.invoke(withInput: message, output: next ?? DiscordChannelOutput(message.channel), context: context, args: args)
+		sink.invoke(withInput: message, output: next ?? DiscordChannelOutput(channel: message.channel), context: context, args: args)
 	}
 }
