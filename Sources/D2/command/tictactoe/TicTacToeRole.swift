1cdcc0fb2ff60551fe2b1eea57176bb12796a1d2
fwcd <fwcdmail@gmail.com>
2019-03-25 16:15:18 +0100

    Add 'discordEncoded' property to TicTacToeRole

--- Sources/D2/command/tictactoe/TicTacToeRole.swift
+++ Sources/D2/command/tictactoe/TicTacToeRole.swift
@@ -2,4 +2,13 @@ enum TicTacToeRole: String {
 	case x = "x"
 	case o = "o"
 	case empty = " "
+	
+	var discordEncoded: String {
+		switch self {
+			case x: return ":x:"
+			case o: return ":o:"
+			case empty: return ":white_large_square:"
+			default: return ":question:"
+		}
+	}
 }
