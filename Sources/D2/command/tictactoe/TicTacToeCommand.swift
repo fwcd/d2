2b434e1969e65a31474677f64b5ef02a78617ad0
fwcd <fwcdmail@gmail.com>
2019-03-26 15:08:53 +0100

    Extract 'set' command handler in TicTacToeCommand into separate method

--- Sources/D2/command/tictactoe/TicTacToeCommand.swift
+++ Sources/D2/command/tictactoe/TicTacToeCommand.swift
@@ -59,55 +59,60 @@ class TicTacToeCommand: StringCommand {
 	func onSubscriptionMessage(withContent content: String, output: CommandOutput, context: CommandContext) -> CommandSubscriptionAction {
 		if let match = currentMatch {
 			if let setArgs = setMessageRegex.firstGroups(in: content) {
-				let roles = match.rolesOf(player: context.author)
-				
-				guard roles.contains(match.currentPlayer) else {
-					print("Current player: \(match.currentPlayer), roles: \(roles)")
-					output.append("It is not your turn, `\(context.author.username)`")
-					return .continueSubscription
-				}
-				
-				let role = match.currentPlayer
-				let rowIndex: Int
-				let colIndex: Int
-				
-				if let row = Row(rawValue: setArgs[1]), let col = Column(rawValue: setArgs[2]) {
-					rowIndex = row.index
-					colIndex = col.index
-				} else if let row = Row(rawValue: setArgs[2]), let col = Column(rawValue: setArgs[1]) {
-					rowIndex = row.index
-					colIndex = col.index
-				} else if let row = Int(setArgs[1]), let col = Int(setArgs[2]) {
-					rowIndex = row
-					colIndex = col
-				} else {
-					output.append("Invalid coordinates, try a combination of `\(Row.allCases.map { $0.rawValue })` and `\(Column.allCases.map { $0.rawValue })` with the syntax: `set [row] [column]` or `set [column] [row]`")
-					return .continueSubscription
-				}
-				
-				do {
-					try match.perform(moveBy: role, atRow: rowIndex, col: colIndex)
-					output.append(match.board.discordEncoded)
-					
-					if let winner = match.board.winner {
-						// Game over
-						if let winnerPlayer = match.playerOf(role: winner) {
-							output.append("\(winner.discordEncoded) aka. `\(winnerPlayer.username)` won the game!")
-						} else {
-							output.append("Invalid winner: \(winner.discordEncoded)")
-						}
-						
-						return .cancelSubscription
-					}
-				} catch TicTacToeError.invalidMove(let role, let row, let col) {
-					output.append("Invalid move by \(role.discordEncoded): Could not place at `[row = \(row), col = \(col)]`")
-				} catch TicTacToeError.outOfBounds(let row, let col) {
-					output.append("Out of bounds: `[row = \(row), col = \(col)]`")
-				} catch {
-					output.append("Error while attempting move")
-					print(error)
-				}
+				return handleSetMessage(withMatch: match, setArgs: setArgs, output: output, context: context)
 			}
+		}
+		return .continueSubscription
+	}
+	
+	func handleSetMessage(withMatch match: TicTacToeMatch, setArgs: [String], output: CommandOutput, context: CommandContext) -> CommandSubscriptionAction {
+		let roles = match.rolesOf(player: context.author)
+		
+		guard roles.contains(match.currentPlayer) else {
+			print("Current player: \(match.currentPlayer), roles: \(roles)")
+			output.append("It is not your turn, `\(context.author.username)`")
+			return .continueSubscription
+		}
+		
+		let role = match.currentPlayer
+		let rowIndex: Int
+		let colIndex: Int
+		
+		if let row = Row(rawValue: setArgs[1]), let col = Column(rawValue: setArgs[2]) {
+			rowIndex = row.index
+			colIndex = col.index
+		} else if let row = Row(rawValue: setArgs[2]), let col = Column(rawValue: setArgs[1]) {
+			rowIndex = row.index
+			colIndex = col.index
+		} else if let row = Int(setArgs[1]), let col = Int(setArgs[2]) {
+			rowIndex = row
+			colIndex = col
+		} else {
+			output.append("Invalid coordinates, try a combination of `\(Row.allCases.map { $0.rawValue })` and `\(Column.allCases.map { $0.rawValue })` with the syntax: `set [row] [column]` or `set [column] [row]`")
+			return .continueSubscription
+		}
+		
+		do {
+			try match.perform(moveBy: role, atRow: rowIndex, col: colIndex)
+			output.append(match.board.discordEncoded)
+			
+			if let winner = match.board.winner {
+				// Game over
+				if let winnerPlayer = match.playerOf(role: winner) {
+					output.append("\(winner.discordEncoded) aka. `\(winnerPlayer.username)` won the game!")
+				} else {
+					output.append("Invalid winner: \(winner.discordEncoded)")
+				}
+				
+				return .cancelSubscription
+			}
+		} catch TicTacToeError.invalidMove(let role, let row, let col) {
+			output.append("Invalid move by \(role.discordEncoded): Could not place at `[row = \(row), col = \(col)]`")
+		} catch TicTacToeError.outOfBounds(let row, let col) {
+			output.append("Out of bounds: `[row = \(row), col = \(col)]`")
+		} catch {
+			output.append("Error while attempting move")
+			print(error)
 		}
 		return .continueSubscription
 	}
