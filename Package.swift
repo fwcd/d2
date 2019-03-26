46238649e092ad2229affb117ef7d162f9fd0c93
fwcd <fwcdmail@gmail.com>
2019-03-26 22:31:42 +0100

    Add PNG library for image processing

--- Package.swift
+++ Package.swift
@@ -8,7 +8,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/nuclearace/SwiftDiscord.git", .upToNextMajor(from: "9.0.0"))
+        .package(url: "https://github.com/nuclearace/SwiftDiscord.git", .upToNextMajor(from: "9.0.0")),
+        .package(url: "https://github.com/kelvin13/png.git", .upToNextMajor(from: "3.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
