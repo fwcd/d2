4667acb9dfed30be7b0d2bacfe6e87eacb94aa4c
fwcd <fwcdmail@gmail.com>
2019-03-27 16:17:43 +0100

    Add 'dev' permission level

--- Sources/D2/util/PermissionLevel.swift
+++ Sources/D2/util/PermissionLevel.swift
@@ -1,11 +1,13 @@
 enum PermissionLevel: Int, Codable {
 	case admin = 500
+	case dev = 100
 	case vip = 50
 	case basic = 10
 	
 	static func of(_ str: String) -> PermissionLevel? {
 		switch str {
 			case "admin": return .admin
+			case "dev": return .dev
 			case "vip": return .vip
 			case "basic": return .basic
 			default: return nil
