9a9ea115ab60f42cce1ec5060e0a1b18626f0382
fwcd <fwcdmail@gmail.com>
2019-03-28 16:00:42 +0100

    Update DrawCommand to use a graphics context and add LineSeg<T> initializer

--- Sources/D2/command/misc/DrawCommand.swift
+++ Sources/D2/command/misc/DrawCommand.swift
@@ -2,12 +2,14 @@ import SwiftDiscord
 import D2Utils
 
 class DrawCommand: StringCommand {
-	let description = "Creates an image"
+	let description = "Creates a demo image"
 	let requiredPermissionLevel = PermissionLevel.basic
 	
 	func invoke(withStringInput input: String, output: CommandOutput, context: CommandContext) {
 		do {
-			try output.append(Image(width: 300, height: 100))
+			let graphics = ImageGraphics(Image(width: 300, height: 300))
+			// TODO
+			try output.append(graphics.image)
 		} catch {
 			print(error)
 			output.append("An error occurred while encoding/sending the image")
--- Sources/D2Utils/graphics/LineSegment.swift
+++ Sources/D2Utils/graphics/LineSegment.swift
@@ -2,4 +2,14 @@ public struct LineSegment<T: VecComponent> {
 	public let start: Vec2<T>
 	public let end: Vec2<T>
 	public let color: Color
+	
+	public init(start: Vec2<T>, end: Vec2<T>, color: Color = .white) {
+		self.start = start
+		self.end = end
+		self.color = color
+	}
+	
+	public init(startX: T, startY: T, endX: T, endY: T) {
+		self.init(start: Vec2(x: startX, y: startY), end: Vec2(x: endX, y: endY))
+	}
 }
