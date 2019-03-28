f3a895bbfdb44cdbde4d31de0c4b91c7cb29786a
fwcd <fwcdmail@gmail.com>
2019-03-28 15:43:26 +0100

    Implement DrawCommand for demonstration purposes

--- Sources/D2/command/misc/DrawCommand.swift
+++ Sources/D2/command/misc/DrawCommand.swift
@@ -1,10 +1,16 @@
 import SwiftDiscord
+import D2Utils
 
 class DrawCommand: StringCommand {
 	let description = "Creates an image"
 	let requiredPermissionLevel = PermissionLevel.basic
 	
 	func invoke(withStringInput input: String, output: CommandOutput, context: CommandContext) {
-		
+		do {
+			try output.append(Image(width: 300, height: 100))
+		} catch {
+			print(error)
+			output.append("An error occurred while encoding/sending the image")
+		}
 	}
 }
