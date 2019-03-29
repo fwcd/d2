7170de9602e129ef78298005de42d1ce99e6532e
fwcd <fwcdmail@gmail.com>
2019-03-29 20:21:35 +0100

    WIP: Expose TimeTableCommand.hidden

--- Sources/D2Commands/timetable/TimeTableCommand.swift
+++ Sources/D2Commands/timetable/TimeTableCommand.swift
@@ -4,7 +4,7 @@ import D2Permissions
 class TimeTableCommand: StringCommand {
 	public let description = "Generates a TimeTable from CAU modules"
 	public let requiredPermissionLevel = PermissionLevel.basic
-	let hidden = true
+	public let hidden = true
 	
 	public func invoke(withStringInput input: String, output: CommandOutput, context: CommandContext) {
 		// TODO
