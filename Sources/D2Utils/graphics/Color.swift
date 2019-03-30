99ab08777f1c9471b1a7329103e996c355e9547a
fwcd <fwcdmail@gmail.com>
2019-03-30 23:56:19 +0100

    Add Color.asDoubleTuple

--- Sources/D2Utils/graphics/Color.swift
+++ Sources/D2Utils/graphics/Color.swift
@@ -8,6 +8,10 @@ public struct Color {
 	public var rgba: UInt32 { return UInt32((red << 24) | (green << 16) | (blue << 8) | alpha) }
 	public var argb: UInt32 { return UInt32((alpha << 24) | (red << 16) | (green << 8) | blue) }
 	
+	public var asDoubleTuple: (red: Double, green: Double, blue: Double, alpha: Double) {
+		return (red: Double(red), green: Double(green), blue: Double(blue), alpha: Double(alpha))
+	}
+	
 	public init(red: UInt8, green: UInt8, blue: UInt8, alpha: UInt8 = 255) {
 		self.red = red
 		self.green = green
