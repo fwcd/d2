f291e8b00b0fed4eb1ab3cde78043ce36f1e6817
fwcd <fwcdmail@gmail.com>
2019-03-30 14:50:33 +0100

    Add GamePlayer struct for easier testability

---
+++ Sources/D2Commands/game/GamePlayer.swift
@@ -0,0 +1,16 @@
+import SwiftDiscord
+
+/** An "actual" player representing a DiscordUser. */
+public struct GamePlayer: Hashable {
+	public let username: String
+	public let id: UserID
+	
+	public init(from user: DiscordUser) {
+		self.init(username: user.username, id: user.id)
+	}
+	
+	public init(username: String, id: UserID) {
+		self.username = username
+		self.id = id
+	}
+}
