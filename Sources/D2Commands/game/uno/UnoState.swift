98d7b50f08b7c66b106438fe3674b272c4ad6988
fwcd <fwcdmail@gmail.com>
2019-03-31 21:43:48 +0200

    WIP: Add Card game state snippet, card game hand snippet and update UnoState implementation

--- Sources/D2Commands/game/uno/UnoState.swift
+++ Sources/D2Commands/game/uno/UnoState.swift
@@ -5,22 +5,26 @@ public struct UnoState: GameState, CustomStringConvertible {
 	public typealias Role = Int
 	public typealias Board = UnoBoard
 	public typealias Move = UnoMove
+	public typealias Hand = UnoHand
 	
 	private let players: [GamePlayer]
 	public private(set) var board = Board()
 	public private(set) var currentRole: Role = 0
+	public var hands: [Role: Hand]
 	public var description: String { return players.map { "`\($0.username)`" }.joined(separator: " vs. ") }
 	
-	public var possibleMoves: Set<Move> {
-		return hands[currentRole]!.map { Move(playing: $0) }
-	}
+	public var possibleMoves: Set<Move> { return Set(hands[currentRole]?.cards.map { Move(playing: ) } ?? [])
+	
+	public var winner: Role? {}
+	public var isDraw: Bool {}
 	
 	public init(players: [GamePlayer]) {
 		self.players = players
+		hands = Dictionary(uniqueKeysWithValues: Array(repeating: UnoHand(), count: players.count).enumerated())
 	}
 	
 	public init(firstPlayer: GamePlayer, secondPlayer: GamePlayer) {
-		players = [firstPlayer, secondPlayer]
+		self.init([firstPlayer, secondPlayer])
 	}
 	
 	public mutating func perform(move: Move) throws {
