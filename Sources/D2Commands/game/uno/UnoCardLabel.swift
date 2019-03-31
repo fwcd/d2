e61c3a09dcdc5fde417de03578cfd18f0728de79
fwcd <fwcdmail@gmail.com>
2019-04-01 00:56:36 +0200

    Add UnoCardLabel.resourcePath

--- Sources/D2Commands/game/uno/UnoCardLabel.swift
+++ Sources/D2Commands/game/uno/UnoCardLabel.swift
@@ -6,6 +6,17 @@ public enum UnoCardLabel: Hashable {
 	case wild
 	case wildDrawFour
 	
+	public var resourcePath: String? {
+		switch self {
+			case .skip: return "Resources/uno/skip.png"
+			case .reverse: return "Resources/uno/reverse.png"
+			case .drawTwo: return "Resources/uno/drawTwo.png"
+			case .wild: return "Resources/uno/wild.png"
+			case .wildDrawFour: return "Resources/uno/wildDrawFour.png"
+			default: return nil
+		}
+	}
+	
 	public static func of(actionLabel: String) -> UnoCardLabel? {
 		switch actionLabel {
 			case "skip": return .skip
