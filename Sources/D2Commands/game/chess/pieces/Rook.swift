10656a660b9cd4c0c11abfc77add3e385a570d59
fwcd <fwcdmail@gmail.com>
2019-04-02 00:09:09 +0200

    Add Rook and fix Bishop moves

--- Sources/D2Commands/game/chess/pieces/Rook.swift
+++ Sources/D2Commands/game/chess/pieces/Rook.swift
@@ -1,11 +1,11 @@
 import D2Utils
 
-public struct Bishop: ChessPiece {
-	public let pieceType: ChessPieceType = .bishop
-	public let notationLetters: [Character] = ["B", "L"]
+public struct Rook: ChessPiece {
+	public let pieceType: ChessPieceType = .rook
+	public let notationLetters: [Character] = ["R"]
 	
 	public func possibleMoves(from position: Vec2<Int>, board: [[ChessPiece?]], role: ChessRole, firstMove: Bool) -> [Vec2<Int>] {
-		return [Vec2(x: -1, y: -1), Vec2(x: 1, y: 1), Vec2(x: 1, y: -1), Vec2(x: -1, y: 1)]
+		return [Vec2(x: -1), Vec2(x: 1), Vec2(y: -1), Vec2(y: 1)]
 			.map { position + $0 }
 			.flatMap { moves(into: $0, from: position, board: board) }
 	}
