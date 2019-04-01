ea75033c57a5dfe2bc0319fab839b6a02ceaeb10
fwcd <fwcdmail@gmail.com>
2019-04-01 14:02:46 +0200

    WIP: Remove legacy 'move' and 'cancel' methods in TwoPlayerGameCommand

--- Tests/D2CommandTests/game/TicTacToeCommandTests.swift
+++ Tests/D2CommandTests/game/TicTacToeCommandTests.swift
@@ -21,25 +21,25 @@ final class TicTacToeCommandTests: XCTestCase {
 		let output = CommandTestOutput()
 		command.startMatch(between: playerX, and: playerO, output: output)
 		
-		command.move(withArgs: ["top left"], output: output, author: playerO)
+		command.perform("move", withArgs: "top left", output: output, author: playerO)
 		XCTAssertEqual(output.lastContent, "It is not your turn, `\(nameO)`")
 		
-		command.move(withArgs: ["top left"], output: output, author: playerX)
+		command.perform("move", withArgs: "top left", output: output, author: playerX)
 		XCTAssertEqual(output.lastContent, "\(x)\(e)\(e)\n\(e)\(e)\(e)\n\(e)\(e)\(e)")
 		
-		command.move(withArgs: ["top right"], output: output, author: playerX)
+		command.perform("move", withArgs: "top right", output: output, author: playerX)
 		XCTAssertEqual(output.lastContent, "It is not your turn, `\(nameX)`")
 		
-		command.move(withArgs: ["center center"], output: output, author: playerO)
+		command.perform("move", withArgs: "center center", output: output, author: playerO)
 		XCTAssertEqual(output.lastContent, "\(x)\(e)\(e)\n\(e)\(o)\(e)\n\(e)\(e)\(e)")
 		
-		command.move(withArgs: ["left bottom"], output: output, author: playerX)
+		command.perform("move", withArgs: "left bottom", output: output, author: playerX)
 		XCTAssertEqual(output.lastContent, "\(x)\(e)\(e)\n\(e)\(o)\(e)\n\(x)\(e)\(e)")
 		
-		command.move(withArgs: ["0 2"], output: output, author: playerO)
+		command.perform("move", withArgs: "0 2", output: output, author: playerO)
 		XCTAssertEqual(output.lastContent, "\(x)\(e)\(o)\n\(e)\(o)\(e)\n\(x)\(e)\(e)")
 		
-		command.move(withArgs: ["1 0"], output: output, author: playerX)
+		command.perform("move", withArgs: "1 0", output: output, author: playerX)
 		XCTAssertEqual(output.nthLastContent(2), "\(x)\(e)\(o)\n\(x)\(o)\(e)\n\(x)\(e)\(e)")
 		
 		let result = output.last?.embeds.first
@@ -52,15 +52,15 @@ final class TicTacToeCommandTests: XCTestCase {
 		let output = CommandTestOutput()
 		command.startMatch(between: playerX, and: playerO, output: output)
 		
-		command.move(withArgs: ["0 0"], output: output, author: playerX)
-		command.move(withArgs: ["1 1"], output: output, author: playerO)
-		command.move(withArgs: ["2 2"], output: output, author: playerX)
-		command.move(withArgs: ["0 1"], output: output, author: playerO)
-		command.move(withArgs: ["2 1"], output: output, author: playerX)
-		command.move(withArgs: ["2 0"], output: output, author: playerO)
-		command.move(withArgs: ["1 0"], output: output, author: playerX)
-		command.move(withArgs: ["1 2"], output: output, author: playerO)
-		command.move(withArgs: ["0 2"], output: output, author: playerX)
+		command.perform("move", withArgs: "0 0", output: output, author: playerX)
+		command.perform("move", withArgs: "1 1", output: output, author: playerO)
+		command.perform("move", withArgs: "2 2", output: output, author: playerX)
+		command.perform("move", withArgs: "0 1", output: output, author: playerO)
+		command.perform("move", withArgs: "2 1", output: output, author: playerX)
+		command.perform("move", withArgs: "2 0", output: output, author: playerO)
+		command.perform("move", withArgs: "1 0", output: output, author: playerX)
+		command.perform("move", withArgs: "1 2", output: output, author: playerO)
+		command.perform("move", withArgs: "0 2", output: output, author: playerX)
 		
 		let result = output.last?.embeds.first
 		XCTAssertEqual(result?.title, ":crown: Game Over")
