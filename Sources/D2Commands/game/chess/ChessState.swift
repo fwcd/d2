0b5e475707bfa07ae701b1bf2c296efb38a73cbf
fwcd <fwcdmail@gmail.com>
2019-04-01 16:14:14 +0200

    WIP: Add ChessState

---
+++ Sources/D2Commands/game/chess/ChessState.swift
@@ -0,0 +1,46 @@
+import SwiftDiscord
+import D2Permissions
+
+public struct ChessState: GameState, CustomStringConvertible {
+	public typealias Role = ChessRole
+	public typealias Board = ChessBoard
+	public typealias Move = ChessMove
+	
+	private let whitePlayer: GamePlayer
+	private let blackPlayer: GamePlayer
+	public private(set) var board = Board()
+	public private(set) var currentRole: Role = .white
+	public var description: String { return "`\(whitePlayer.username)` as :white_circle: vs. `\(blackPlayer.username)` as :black_circle:" }
+	
+	public var possibleMoves: Set<Move> {
+		return [] // TODO
+	}
+	
+	public var winner: Role? { return nil /* TODO */ }
+	public var isDraw: Bool { return false /* TODO */ }
+	
+	public init(firstPlayer whitePlayer: GamePlayer, secondPlayer blackPlayer: GamePlayer) {
+		self.whitePlayer = whitePlayer
+		self.blackPlayer = blackPlayer
+	}
+	
+	public mutating func perform(move: Move) throws {
+		// TODO
+	}
+	
+	public func playerOf(role: Role) -> GamePlayer? {
+		switch role {
+			case .white: return whitePlayer
+			case .black: return blackPlayer
+		}
+	}
+	
+	public func rolesOf(player: GamePlayer) -> [Role] {
+		var roles = []
+		
+		if player == whitePlayer { roles.add(.white) }
+		if player == blackPlayer { roles.add(.black) }
+		
+		return roles
+	}
+}
