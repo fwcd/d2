fcef0ac5d9b69d1f9a0c44d337910e5d1e4bf56b
fwcd <fwcdmail@gmail.com>
2019-04-02 00:20:52 +0200

    Support partial ParsedChessMoves

--- Sources/D2Commands/game/chess/ParsedChessMove.swift
+++ Sources/D2Commands/game/chess/ParsedChessMove.swift
@@ -2,6 +2,16 @@ import D2Utils
 
 struct ParsedChessMove {
 	let piece: ChessPiece
-	let origin: Vec2<Int>
-	let destination: Vec2<Int>
+	let originX: Int?
+	let originY: Int?
+	let destinationX: Int?
+	let destinationY: Int?
+	
+	init(piece: ChessPiece, originX: Int? = nil, originY: Int? = nil, destinationX: Int? = nil, destinationY: Int? = nil) {
+		self.piece = piece
+		self.originX = originX
+		self.originY = originY
+		self.destinationX = destinationX
+		self.destinationY = destinationY
+	}
 }
