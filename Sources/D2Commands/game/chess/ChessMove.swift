76b63c3355cb9f6c7fdda72acd4979cc355731db
fwcd <fwcdmail@gmail.com>
2019-04-02 14:04:11 +0200

    Make ChessMove.isCapture and .isEnPassant optional

--- Sources/D2Commands/game/chess/ChessMove.swift
+++ Sources/D2Commands/game/chess/ChessMove.swift
@@ -4,24 +4,24 @@ public struct ChessMove: Hashable {
 	public let pieceType: ChessPieceType?
 	public let originX: Int?
 	public let originY: Int?
-	public let isCapture: Bool
+	public let isCapture: Bool?
 	public let destinationX: Int?
 	public let destinationY: Int?
 	public let promotionPieceType: ChessPieceType?
 	public let checkType: CheckType?
-	public let isEnPassant: Bool
+	public let isEnPassant: Bool?
 	public let castlingType: CastlingType?
 	
 	init(
 		pieceType: ChessPieceType? = nil,
 		originX: Int? = nil,
 		originY: Int? = nil,
-		isCapture: Bool = false,
+		isCapture: Bool? = nil,
 		destinationX: Int? = nil,
 		destinationY: Int? = nil,
 		promotionPieceType: ChessPieceType? = nil,
 		checkType: CheckType? = nil,
-		isEnPassant: Bool = false,
+		isEnPassant: Bool? = nil,
 		castlingType: CastlingType? = nil
 	) {
 		self.pieceType = pieceType
