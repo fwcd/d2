fc4ea9c59f84db9680e58eac52b5b97c3ed554f8
fwcd <fwcdmail@gmail.com>
2019-04-02 21:11:05 +0200

    Add 'possibleMoves' action to ChessGame

--- Sources/D2Commands/game/chess/ChessGame.swift
+++ Sources/D2Commands/game/chess/ChessGame.swift
@@ -6,6 +6,7 @@ public struct ChessGame: Game {
 		"move": { state, args in ActionResult(
 			nextState: try state.childState(after: try state.unambiguouslyResolve(move: try ChessGame.parse(move: args)))
 		) },
+		"possibleMoves": { state, _ in ActionResult(nextState: state, additionalOutput: "`\(state.possibleMoves)`") },
 		"cancel": { state, _ in ActionResult(nextState: state, cancelsMatch: true) }
 	]
 	
