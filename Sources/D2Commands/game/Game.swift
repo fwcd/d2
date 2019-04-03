09eb9c430b8b25920b8d7c5856c25e7d6873fb6b
fwcd <fwcdmail@gmail.com>
2019-04-04 00:36:21 +0200

    Define Game.apiActions

--- Sources/D2Commands/game/Game.swift
+++ Sources/D2Commands/game/Game.swift
@@ -3,8 +3,12 @@ import D2Graphics
 public protocol Game {
 	associatedtype State: GameState
 	
-	var name: String { get }
+	/** Actions define ways of interacting with the game. */
 	var actions: [String: (State, String) throws -> ActionResult<State>] { get }
+	/** API actions can be invoked by other applications and not just users. */
+	var apiActions: Set<String> { get }
+	
+	var name: String { get }
 	var renderFirstBoard: Bool { get }
 	var onlySendHandToCurrentRole: Bool { get }
 	var helpText: String { get }
@@ -18,4 +22,5 @@ public extension Game {
 	var onlySendHandToCurrentRole: Bool { return true }
 	var themeColor: Color? { return nil }
 	var helpText: String { return "No help text found for \(name)" }
+	var apiActions: Set<String> { return [] }
 }
