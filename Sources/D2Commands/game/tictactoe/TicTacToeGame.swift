42863c5227de20dd0faa12f06d104ebe0319266e
fwcd <fwcdmail@gmail.com>
2019-04-03 19:04:08 +0200

    Add helpText for tic-tac-toe

--- Sources/D2Commands/game/tictactoe/TicTacToeGame.swift
+++ Sources/D2Commands/game/tictactoe/TicTacToeGame.swift
@@ -1,10 +1,17 @@
 public struct TicTacToeGame: Game {
 	public typealias State = TicTacToeState
 	
-	public let name: String = "tic tac toe"
+	public let name: String = "tic-tac-toe"
 	public let actions: [String: (State, String) throws -> ActionResult<State>] = [
 		"move": { state, args in ActionResult(nextState: try state.childState(after: try TicTacToeGame.parse(move: args))) }
 	]
+	public let helpText = """
+		Tic-Tac-Toe moves can be written either as array indices with x and y in (0..<2) or in english:
+		
+		`move top left`
+		`move bottom center`
+		`move 1 2` (equivalent to `center right`)
+		"""
 	
 	public init() {}
 	
