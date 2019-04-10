6fe523c76affda46492c29e0d8a92c017aecc2be
fwcd <fwcdmail@gmail.com>
2019-04-11 00:53:42 +0200

    Trim out code block syntax by default before saving to a file

--- Sources/D2Commands/file/ToFileCommand.swift
+++ Sources/D2Commands/file/ToFileCommand.swift
@@ -2,7 +2,9 @@ import SwiftDiscord
 import D2Permissions
 import D2Utils
 
-fileprivate let argsPattern = try! Regex(from: "(\\S+)\\s+(.+)")
+fileprivate let argsPattern = try! Regex(from: "(\\S+)((?:\\s+--\\S+)*)\\s+([\\s\\S]+)")
+fileprivate let flagPattern = try! Regex(from: "--(\\S+)")
+fileprivate let trimPattern = try! Regex(from: "`(?:``(?:.+)?\n?)?([^`]+)`(?:``)?")
 
 public class ToFileCommand: Command {
 	public let description = "Writes text to a file"
@@ -16,7 +18,10 @@ public class ToFileCommand: Command {
 		
 		if let parsedArgs = argsPattern.firstGroups(in: combinedArgs) {
 			let filename = parsedArgs[1]
-			let content = parsedArgs[2]
+			let flags = parsedArgs[2].nilIfEmpty.map { flagPattern.allGroups(in: $0).map { $0[1] } } ?? []
+			let rawContent = parsedArgs[3]
+			let content = flags.contains("raw") ? rawContent : trim(rawContent: rawContent)
+			
 			guard let data = content.data(using: .utf8) else {
 				output.append("Could not encode file data as UTF-8")
 				return
@@ -24,7 +29,11 @@ public class ToFileCommand: Command {
 			
 			output.append([DiscordFileUpload(data: data, filename: filename, mimeType: "plain/text")])
 		} else {
-			output.append("Syntax error: Use `[filename] [content...]`")
+			output.append("Syntax error: Use `[filename] [--raw]? [content...]`")
 		}
+	}
+	
+	private func trim(rawContent: String) -> String {
+		return trimPattern.firstGroups(in: rawContent)?[safely: 1] ?? rawContent
 	}
 }
