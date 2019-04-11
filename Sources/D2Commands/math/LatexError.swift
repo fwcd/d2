c3b1ba958d1e76cb7531a2373d1d9c4bba626716
fwcd <fwcdmail@gmail.com>
2019-04-12 01:02:58 +0200

    Implement template reading in LatexRenderer

---
+++ Sources/D2Commands/math/LatexError.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+enum LatexError: Error {
+	case templateFileNotFound(URL)
+	case noTemplateFileData(URL)
+	case invalidTemplateFileEncoding(URL)
+}
--- Sources/D2Commands/math/LatexRenderer.swift
+++ Sources/D2Commands/math/LatexRenderer.swift
@@ -8,6 +8,16 @@ struct LatexRenderer {
 	}
 	
 	init(templateFilePath: String = "Resources/latex/LatexTemplate.tex") {
-		self.init(URL(fileURLWithPath: templateFilePath))
+		self.init(templateURL: URL(fileURLWithPath: templateFilePath))
+	}
+	
+	private func readTemplate() throws -> String {
+		let fileManager = FileManager.default
+		
+		guard fileManager.fileExists(atPath: templateURL.path) else { throw LatexError.templateFileNotFound(templateURL) }
+		guard let data = fileManager.contents(atPath: templateURL.path) else { throw LatexError.noTemplateFileData(templateURL) }
+		guard let contents = String(data: data, encoding: .utf8) else { throw LatexError.invalidTemplateFileEncoding(templateURL) }
+		
+		return contents
 	}
 }
