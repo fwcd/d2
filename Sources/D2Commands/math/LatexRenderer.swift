4db1b62fa89ea788a0f30b166bec0cbdaacf36a5
fwcd <fwcdmail@gmail.com>
2019-04-12 01:05:36 +0200

    Implement LatexRenderer.applyTemplate

--- Sources/D2Commands/math/LatexRenderer.swift
+++ Sources/D2Commands/math/LatexRenderer.swift
@@ -2,13 +2,15 @@ import Foundation
 
 struct LatexRenderer {
 	private let templateURL: URL
+	private let textPlaceholder: String
 	
-	init(templateURL: URL) {
+	init(templateURL: URL, textPlaceholder: String) {
 		self.templateURL = templateURL
+		self.textPlaceholder = textPlaceholder
 	}
 	
-	init(templateFilePath: String = "Resources/latex/LatexTemplate.tex") {
-		self.init(templateURL: URL(fileURLWithPath: templateFilePath))
+	init(templateFilePath: String = "Resources/latex/LatexTemplate.tex", textPlaceholder: String = "TextPlaceholder") {
+		self.init(templateURL: URL(fileURLWithPath: templateFilePath), textPlaceholder: textPlaceholder)
 	}
 	
 	private func readTemplate() throws -> String {
@@ -19,5 +21,9 @@ struct LatexRenderer {
 		guard let contents = String(data: data, encoding: .utf8) else { throw LatexError.invalidTemplateFileEncoding(templateURL) }
 		
 		return contents
+	}
+	
+	private func applyTemplate(to formula: String) throws -> String {
+		return try readTemplate().replacingOccurrences(of: textPlaceholder, with: formula)
 	}
 }
