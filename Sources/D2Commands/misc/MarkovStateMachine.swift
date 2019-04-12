92e4c25b16ae62e394e41c3e2d46ee1774f08da5
fwcd <fwcdmail@gmail.com>
2019-04-12 22:49:00 +0200

    Fix compile-time issues in MarkovStateMachine and MarkovTransitionMatrix

--- Sources/D2Commands/misc/MarkovStateMachine.swift
+++ Sources/D2Commands/misc/MarkovStateMachine.swift
@@ -1,28 +1,26 @@
+import D2Utils
+
 struct MarkovStateMachine<T: Hashable>: Sequence, IteratorProtocol {
 	private let matrix: MarkovTransitionMatrix<T>
 	private let maxLength: Int
-	private(set) var state: [T]
-	private var circular
+	private(set) var state: CircularArray<T>
 	private(set) var index: Int
 	
 	init(matrix: MarkovTransitionMatrix<T>, startValue: T, maxLength: Int = Int.max) {
 		self.matrix = matrix
 		self.maxLength = maxLength
-		state = [startValue]
+		state = CircularArray(capacity: matrix.order)
+		state.push(startValue)
 		index = 0
 	}
 	
 	mutating func next() -> T? {
 		guard index < maxLength else { return nil }
-		let prediction = matrix.predict(state)
+		guard let prediction = matrix.predict(Array(state)) else { return nil }
 		
-		push(prediction)
+		state.push(prediction)
 		index += 1
 		
 		return prediction
-	}
-	
-	private mutating func push(_ stateElement: T) {
-		
 	}
 }
--- Sources/D2Commands/misc/MarkovTransitionMatrix.swift
+++ Sources/D2Commands/misc/MarkovTransitionMatrix.swift
@@ -1,10 +1,15 @@
 /** A stateless transition table. */
 struct MarkovTransitionMatrix<T: Hashable> {
 	private var values = [[T]: [T]]()
+	let order: Int
 	
-	init() {}
+	init() {
+		order = 1
+	}
 	
 	init(fromElements elements: [T], order: Int) {
+		self.order = order
+		
 		for i in 0..<(elements.count - order) {
 			let key = Array(elements[i..<(i + order)])
 			let next = elements[i + order]
@@ -23,12 +28,16 @@ struct MarkovTransitionMatrix<T: Hashable> {
 	}
 	
 	private func suffixDistance(between a: [T], and b: [T]) -> Int {
-		let minCount = min(a.count, b.count)
-		return (0..<minCount).filter { a[(a.count - 1) - $0] != b[(b.count - 1) - $0] }.count
+		return (0..<min(a.count, b.count))
+			.filter { (i: Int) -> Bool in a[(a.count - 1) - i] != b[(b.count - 1) - i] }
+			.count
 	}
 	
 	func predict(_ state: [T]) -> T? {
-		let next = values[state] ?? values.sorted { suffixDistance(between: $0, and: state) }.first
+		let next = values[state] ?? values
+			.sorted { suffixDistance(between: $0.key, and: state) < suffixDistance(between: $1.key, and: state) }
+			.first?
+			.value
 		return next?.randomElement()
 	}
 }
