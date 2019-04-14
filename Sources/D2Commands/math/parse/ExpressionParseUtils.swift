3e99dac3cb17e6790c6c0f34aa450f43129cf730
fwcd <fwcdmail@gmail.com>
2019-04-14 14:23:52 +0200

    Extract expression parsing constants into separate file

---
+++ Sources/D2Commands/math/parse/ExpressionParseUtils.swift
@@ -0,0 +1,12 @@
+let expressionBinaryOperators: [String: (ExpressionASTNode, ExpressionASTNode) -> ExpressionASTNode] = [
+	"+": { SumNode(lhs: $0, rhs: $1) },
+	"-": { DifferenceNode(lhs: $0, rhs: $1) },
+	"*": { ProductNode(lhs: $0, rhs: $1) },
+	"/": { QuotientNode(lhs: $0, rhs: $1) },
+	"^": { ExponentiationNode(lhs: $0, rhs: $1) }
+]
+
+let expressionConstants: [String: ExpressionASTNode] = [
+	"e": ConstantNode(value: 2.71828182845904523536),
+	"pi": ConstantNode(value: .pi)
+]
