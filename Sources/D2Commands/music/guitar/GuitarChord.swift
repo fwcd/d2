bd6e34934c5ecb32cf55bc6fd6941b12d69e99b9
fwcd <fwcdmail@gmail.com>
2019-04-19 15:17:43 +0200

    Optimize guitar chord generation through the use of a cost function

--- Sources/D2Commands/music/guitar/GuitarChord.swift
+++ Sources/D2Commands/music/guitar/GuitarChord.swift
@@ -1,16 +1,27 @@
+fileprivate func cost<C: Collection>(of dots: C) -> Int where C.Element == GuitarFretboard.Location {
+	return dots.map { ($0.fret * $0.fret) - $0.guitarString }.reduce(0, +)
+}
+
 fileprivate func findDots<C: Collection>(remainingNotes: C, usedStrings: [Int] = [], on fretboard: GuitarFretboard) -> [GuitarFretboard.Location]? where C.Element == Note {
 	guard let firstNote = remainingNotes.first else { return [] }
-	print("Remaining \(remainingNotes)")
+	var bestResult: [GuitarFretboard.Location]? = nil
+	var bestCost = Int.max
 	
 	for stringIndex in 0..<fretboard.stringCount {
 		if !usedStrings.contains(stringIndex), let pos = fretboard.find(note: firstNote, on: stringIndex) {
 			if let nextDots = findDots(remainingNotes: remainingNotes.dropFirst(), usedStrings: usedStrings + [stringIndex], on: fretboard) {
-				return [pos.location] + nextDots
+				let dots = [pos.location] + nextDots
+				let currentCost = cost(of: dots)
+				
+				if currentCost < bestCost {
+					bestResult = dots
+					bestCost = currentCost
+				}
 			}
 		}
 	}
 	
-	return nil
+	return bestResult
 }
 
 struct GuitarChord {
