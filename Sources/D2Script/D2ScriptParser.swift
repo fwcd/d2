9211b9aba058e2623af98b94e565034d148582a3
fwcd <fwcdmail@gmail.com>
2019-05-17 22:26:13 +0200

    Hide tokenization methods in D2ScriptParser

--- Sources/D2Script/D2ScriptParser.swift
+++ Sources/D2Script/D2ScriptParser.swift
@@ -1,14 +1,14 @@
 public struct D2ScriptParser {
-	func parse(_ input: String) throws -> D2ScriptTopLevelScope {
+	public func parse(_ input: String) throws -> D2ScriptTopLevelScope {
 		return try parse(tokens: try tokenize(input))
 	}
 	
-	func tokenize(_ input: String) throws -> [D2ScriptToken] {
+	private func tokenize(_ input: String) throws -> [D2ScriptToken] {
 		// TODO
 		return []
 	}
 	
-	func parse(tokens: [D2ScriptToken]) throws -> D2ScriptTopLevelScope {
+	private func parse(tokens: [D2ScriptToken]) throws -> D2ScriptTopLevelScope {
 		// TODO
 		return D2ScriptTopLevelScope()
 	}
