783c501475929ac6550f54f0076e23e5bfc2c2a4
fwcd <fwcdmail@gmail.com>
2019-05-24 16:21:09 +0200

    Implement D2ScriptCommand
    Spawn an executor when the command is invoked, configure an 'output'
    function to allow the script to interact with the environment and call
    the command.

--- Sources/D2Commands/d2script/D2ScriptCommand.swift
+++ Sources/D2Commands/d2script/D2ScriptCommand.swift
@@ -6,16 +6,41 @@ public class D2ScriptCommand: StringCommand {
 	public let description: String
 	public let sourceFile: String = #file
 	public let requiredPermissionLevel: PermissionLevel
-	private let executor: D2ScriptExecutor
+	private let script: D2Script
 	
-	public init(executor: D2ScriptExecutor) {
-		self.executor = executor
+	public init(script: D2Script) {
+		self.script = script
+		
+		let executor = D2ScriptExecutor()
+		executor.run(script)
 		description = executor.topLevelStorage[string: "description"] ?? "Anonymous D2Script"
 		requiredPermissionLevel = executor.topLevelStorage[string: "requiredPermissionLevel"].flatMap { PermissionLevel.of($0) } ?? .vip
 	}
 	
 	public func invoke(withStringInput input: String, output: CommandOutput, context: CommandContext) {
-		let commandNames = executor.topLevelStorage.commandNames
+		let executor = D2ScriptExecutor()
+		executor.run(script)
+		
+		let storage = executor.topLevelStorage
+		let commandNames = storage.commandNames
+		
+		// Add Discord commands
+		storage[function: "output"] = {
+			guard let value = $0.first else {
+				output.append("output(...) received no arguments")
+				return nil
+			}
+			switch value {
+				case let .string(str)?:
+					output.append(str)
+				case let .number(num)?:
+					output.append(String(num))
+				default:
+					output.append(String(describing: value))
+			}
+			return nil
+		}
+		
 		if commandNames.count > 1 {
 			output.append("Ambiguous invocation, there is more than one command in the executor's storage: \(commandNames)")
 		} else if let commandName = commandNames.first {
