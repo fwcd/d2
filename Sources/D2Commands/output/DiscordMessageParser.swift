99f6852cf9d39d274ec0f9163158681a7e9decad
fwcd <fwcdmail@gmail.com>
2019-07-25 13:07:51 +0300

    Implement message content parser
    Parse raw text and code from a received DiscordMessage in
    DiscordMessageParser.

--- Sources/D2Commands/output/DiscordMessageParser.swift
+++ Sources/D2Commands/output/DiscordMessageParser.swift
@@ -1,11 +1,32 @@
 import SwiftDiscord
+import D2Utils
+
+// The first group matches the language, the second group matches the code
+fileprivate let codePattern = try! Regex(from: "(?:`(?:``(?:(\\w*)\n)?)?)?([^`]+)`*")
 
 /**
  * Parses Discord messages into rich values.
  */
 public struct DiscordMessageParser {
 	public func parse(message: DiscordMessage) -> RichValue {
-		// TODO
-		return .text(message.content)
+		var values: [RichValue] = []
+		
+		// Parse message content
+		if let codeGroups = codePattern.firstGroups(in: message.content) {
+			let language = codeGroups[1].nilIfEmpty
+			let code = codeGroups[2]
+			values.append(.code(code, language: language))
+		} else {
+			values.append(.text(message.content))
+		}
+		
+		// TODO: Handle images
+		
+		// Convert values to RichValue
+		switch values.count {
+			case 0: return .none
+			case 1: return values.first!
+			default: return .compound(values)
+		}
 	}
 }
