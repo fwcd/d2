d93dcac86f22913e8a9be5730cbba0df4d0e85f5
fwcd <fwcdmail@gmail.com>
2019-09-29 23:53:28 +0200

    Remove unused CommandEvent

--- Sources/D2Commands/CommandEvent.swift
+++
@@ -1,7 +0,0 @@
-import SwiftDiscord
-
-/** A "non-invocation" event on a command. */
-public enum CommandEvent {
-    case successfullySent(message: DiscordMessage)
-    case subscriptionMessage(content: String, output: CommandOutput, context: CommandContext)
-}
