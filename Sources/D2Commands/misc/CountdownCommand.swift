d6e16f958ef95e5d4231e579fa4ed8ed7e40f564
fwcd <fwcdmail@gmail.com>
2019-11-27 00:56:22 +0100

    Implement CountdownCommand
    Store running countdowns by name, add subcommands and clear countdowns
    once completed.

--- Sources/D2Commands/misc/CountdownCommand.swift
+++ Sources/D2Commands/misc/CountdownCommand.swift
@@ -1,20 +1,106 @@
+import Foundation
 import SwiftDiscord
 import D2Permissions
+import D2Utils
+
+fileprivate func makeDateFormatter(_ format: String) -> DateFormatter {
+    let formatter = DateFormatter()
+    formatter.dateFormat = format
+    return formatter
+}
+
+fileprivate let outputDateFormatter = makeDateFormatter("dd.MM.yyyy - HH:mm")
+fileprivate let inputDateFormatters = [
+    makeDateFormatter("dd.MM.yyyy"),
+    makeDateFormatter("dd.MM.yyyy HH:mm"),
+    outputDateFormatter
+]
+fileprivate let namedDatePattern = try! Regex(from: "(\\w+)\\s*(.+)")
 
 public class CountdownCommand {
-    public let info = CommandInfo(
+    public private(set) var info = CommandInfo(
         category: .misc,
         shortDescription: "A date/time countdown manager",
         longDescription: "Stores a collection of dates that it counts down to",
         requiredPermissionLevel: .basic
     )
     private var goals: [String: CountdownGoal]
+    private var subcommands: [String: (String, CommandOutput) -> Void] = [:]
     
     public init(goals: [String: CountdownGoal] = [:]) {
         self.goals = goals
+
+        subcommands = [
+            "add": { [unowned self] input, output in
+                guard let parsedInput = namedDatePattern.firstGroups(in: input) else {
+                    output.append("Please use the format: [name] [dd.MM.yyyy] [HH:mm]?")
+                    return
+                }
+
+                let name = parsedInput[1]
+                let rawDate = parsedInput[2]
+
+                guard let date = self.parseDate(from: rawDate) else {
+                    output.append("Could not parse date. Please use one of these formats: \(inputDateFormatters.map { $0.dateFormat })")
+                    return
+                }
+
+                let goal = FixedCountdownGoal(date: date)
+                self.goals[name] = goal
+                self.show(name, as: goal, to: output)
+            }
+        ]
+        info.helpText = makeHelpText()
     }
     
     public func invoke(withStringInput input: String, output: CommandOutput, context: CommandContext) {
-        // TODO
+        if let date = parseDate(from: input) {
+            show("Anonymous Event", as: FixedCountdownGoal(date: date), to: output)
+        } else if let subcommand = input.split(separator: " ", maxSplits: 2).first.flatMap({ subcommands[String($0)] }) {
+            subcommand(input, output)
+        } else {
+            removeCompletedGoals() // Clean up
+            showRunningGoals(to: output)
+        }
+    }
+    
+    private func plural(of str: String, ifOne value: Int) -> String {
+        return (value == 1) ? str : "\(str)s"
+    }
+    
+    private func parseDate(from input: String) -> Date? {
+        return inputDateFormatters.compactMap { $0.date(from: input) }.first
+    }
+    
+    private func show(_ name: String, as goal: CountdownGoal, to output: CommandOutput) {
+        output.append(.embed(DiscordEmbed(
+            title: ":hourglass: \(name) Countdown",
+            description: "The next \(name) will take place in **\(describeRemainingTimeUntil(goal: goal))**",
+            footer: DiscordEmbed.Footer(text: outputDateFormatter.string(from: goal.date))
+        )))
+    }
+    
+    private func showRunningGoals(to output: CommandOutput) {
+        output.append(.embed(DiscordEmbed(
+            title: ":hourglass: Running Countdowns",
+            fields: goals.map { DiscordEmbed.Field(name: $0.key, value: "will take place in **\(describeRemainingTimeUntil(goal: $0.value))** (on \(outputDateFormatter.string(from: $0.value.date)))") }
+        )))
+    }
+    
+    private func describeRemainingTimeUntil(goal: CountdownGoal) -> String {
+        let date = Calendar.current.dateComponents([.day, .hour, .minute], from: Date(), to: goal.date)
+        let days = date.day!
+        let hours = date.hour!
+        let minutes = date.minute!
+
+        return "\(days) \(plural(of: "day", ifOne: days)), \(hours) \(plural(of: "hour", ifOne: hours)) and \(minutes) \(plural(of: "minute", ifOne: minutes))"
+    }
+    
+    private func removeCompletedGoals() {
+        goals = goals.filter { !$0.value.hasCompleted }
+    }
+    
+    private func makeHelpText() -> String {
+        return "Available subcommands: \(subcommands.keys)"
     }
 }
--- Sources/D2Commands/misc/CountdownGoal.swift
+++ Sources/D2Commands/misc/CountdownGoal.swift
@@ -3,4 +3,9 @@ import Foundation
 public protocol CountdownGoal {
     var date: Date { get }
     var removeAfterCompletion: Bool { get }
+    var hasCompleted: Bool { get }
+}
+
+public extension CountdownGoal {
+    var hasCompleted: Bool { return date.timeIntervalSinceNow <= 0 }
 }
