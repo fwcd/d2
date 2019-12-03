cb62f2fafbfe01a12585de1c5782a5a57bac428a
fwcd <fwcdmail@gmail.com>
2019-12-03 22:32:59 +0100

    Fix typo

--- Sources/D2Commands/misc/CountdownCommand.swift
+++ Sources/D2Commands/misc/CountdownCommand.swift
@@ -25,7 +25,7 @@ public class CountdownCommand: StringCommand {
         longDescription: "Stores a collection of dates that it counts down to",
         requiredPermissionLevel: .basic
     )
-    public let outputValueType: RichValue = .embed
+    public let outputValueType: RichValueType = .embed
     private var goals: [String: CountdownGoal]
     private var subcommands: [String: (String, CommandOutput) -> Void] = [:]
     
