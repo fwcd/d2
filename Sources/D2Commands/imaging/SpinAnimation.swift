7b0f5c7c74bccfb7299148d6fbf4f0179e5283f0
fwcd <fwcdmail@gmail.com>
2020-01-13 17:36:22 +0100

    Add public initializer to SpinAnimation

--- Sources/D2Commands/imaging/SpinAnimation.swift
+++ Sources/D2Commands/imaging/SpinAnimation.swift
@@ -2,6 +2,8 @@ import D2Graphics
 import D2Utils
 
 public struct SpinAnimation: Animation {
+    public init() {}
+
     public func renderFrame(from image: Image, to graphics: inout Graphics, percent: Double, args: String) {
         let angle = percent * 2 * Double.pi
         graphics.draw(image, at: Vec2(x: 0, y: 0), rotation: angle)
