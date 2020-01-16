55cd4838e2d8dea73425b854dc273f4f24c5ec80
fwcd <fwcdmail@gmail.com>
2020-01-16 05:04:08 +0100

    Format constant expression decimal nodes using NumberFormatter

--- Sources/D2Commands/math/ast/ConstantNode.swift
+++ Sources/D2Commands/math/ast/ConstantNode.swift
@@ -1,9 +1,16 @@
+import Foundation
 import D2Utils
 
 struct ConstantNode: ExpressionASTNode {
 	let value: Double
 	let occurringVariables: Set<String> = []
-	var label: String { return String(value) }
+	var label: String {
+		let formatter = NumberFormatter()
+		formatter.minimumFractionDigits = 0
+		formatter.maximumFractionDigits = 4
+		formatter.numberStyle = .decimal
+		return formatter.string(from: value as NSNumber) ?? String(value)
+	}
 	var prefixFunctionNotation: String { return label }
 	var infixICNotation: String { return label }
 	
