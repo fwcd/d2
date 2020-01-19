995fd38b7f85a15e4132d6fc910dc0ee426059f9
fwcd <fwcdmail@gmail.com>
2020-01-19 20:07:33 +0100

    WIP: Statefully remember last code in LzwDecoder

--- Sources/D2Graphics/gif/LzwDecoder.swift
+++ Sources/D2Graphics/gif/LzwDecoder.swift
@@ -3,19 +3,19 @@ struct LzwDecoder {
     private(set) var table: LzwDecoderTable
     
     public private(set) var decoded: [Int] = [] // The decoded indices
+    private var lastCode: UInt8? = nil
     
     public init(colorCount: Int) {
         table = LzwDecoderTable(colorCount: colorCount)
     }
     
-    public mutating func decodeAndAppend(lastCode: UInt8, code: UInt8) throws {
-        // The main LZW decoding algorithm
-        if code == table.meta.clearCode {
-            table.reset()
-        } else {
-            let lastCodeInt = Int(lastCode)
-            let codeInt = Int(code)
-            if let indices = table[codeInt] {
+    public mutating func decodeAndAppend(code: UInt8) throws {
+        let codeInt = Int(code)
+        if let lastCodeInt = lastCode.map(Int.init) {
+            // The main LZW decoding algorithm
+            if code == table.meta.clearCode {
+                table.reset()
+            } else if let indices = table[codeInt] {
                 guard var nextIndices = table[lastCodeInt] else { throw LzwCodingError.tableTooSmall }
                 guard let k = indices.first else { throw LzwCodingError.decodedIndicesEmpty }
                 decoded += indices
@@ -29,5 +29,6 @@ struct LzwDecoder {
                 table.append(indices: indices)
             }
         }
+        lastCode = code
     }
 }
