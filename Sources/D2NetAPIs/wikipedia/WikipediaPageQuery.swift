8b5d66c588c5d11cdfe5f0aacc7af7fd679496e4
fwcd <fwcdmail@gmail.com>
2020-01-20 16:57:21 +0100

    Format page name before querying Wikipedia's REST API

--- Sources/D2NetAPIs/wikipedia/WikipediaPageQuery.swift
+++ Sources/D2NetAPIs/wikipedia/WikipediaPageQuery.swift
@@ -1,17 +1,20 @@
 import D2Utils
 
 public struct WikipediaPageQuery {
-    private let pageName: String
+    private let page: String
     
     public init(pageName: String) {
-        self.pageName = pageName
+        page = pageName
+            .withFirstUppercased
+            .replacingOccurrences(of: "/", with: "")
+            .replacingOccurrences(of: " ", with: "_")
     }
     
     public func perform(then: @escaping (Result<WikipediaPage, Error>) -> Void) {
         do {
             try HTTPRequest(
                 host: "en.wikipedia.org",
-                path: "/api/rest_v1/page/summary/\(pageName.replacingOccurrences(of: "/", with: ""))"
+                path: "/api/rest_v1/page/summary/\(page)"
             ).fetchJSONAsync(as: WikipediaPage.self, then: then)
         } catch {
             then(.failure(error))
