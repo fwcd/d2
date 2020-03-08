3c84f9183183f182a5e2b8ad2c455a36ebbde619
fwcd <fwcdmail@gmail.com>
2020-03-08 01:14:05 +0100

    Add Hoogle JSON API

---
+++ Sources/D2NetAPIs/hoogle/HoogleQuery.swift
@@ -0,0 +1,24 @@
+import D2Utils
+
+public struct HoogleQuery {
+    private let term: String
+    
+    public init(term: String) {
+        self.term = term
+    }
+    
+    public func perform(then: @escaping (Result<[HoogleResult], Error>) -> Void) {
+        do {
+            let request = try HTTPRequest(host: "hoogle.haskell.org", query: [
+                "mode": "json",
+                "format": "text",
+                "hoogle": term,
+                "start": "1",
+                "count": "5"
+            ])
+            request.fetchJSONAsync(as: [HoogleResult].self, then: then)
+        } catch {
+            then(.failure(error))
+        }
+    }
+}
---
+++ Sources/D2NetAPIs/hoogle/HoogleResult.swift
@@ -0,0 +1,13 @@
+public struct HoogleResult: Codable {
+    public let url: String?
+    public let module: NamedURL?
+    public let package: NamedURL?
+    public let item: String
+    public let type: String?
+    public let docs: String?
+    
+    public struct NamedURL: Codable {
+        public let url: String
+        public let name: String
+    }
+}
