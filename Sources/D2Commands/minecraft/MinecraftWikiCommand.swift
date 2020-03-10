5dfb48aacaf6ca9180836f31c506e0933b2a545a
fwcd <fwcdmail@gmail.com>
2020-03-10 16:27:38 +0100

    Format MC wiki documents as embeds

--- Sources/D2Commands/minecraft/MinecraftWikiCommand.swift
+++ Sources/D2Commands/minecraft/MinecraftWikiCommand.swift
@@ -1,3 +1,4 @@
+import Foundation
 import SwiftDiscord
 import D2NetAPIs
 import D2Utils
@@ -20,21 +21,38 @@ public class MinecraftWikiCommand: StringCommand {
                     output.append(errorText: "No wikitext got")
                     return
                 }
-                print(raw)
                 let doc = try MinecraftWikitextParser().parse(raw: raw)
-                print(doc)
-                // output.append(DiscordEmbed(
-                //     title: sectionsParse.parse.title,
-                //     fields: Array(wikitextParses.prefix(5).map {
-                //         DiscordEmbed.Field(
-                //             name: "Section", // TODO: Extract section title from sectionsParse using index
-                //             value: $0.parse.wikitext?.truncate(200, appending: "...") ?? "_no text_" // TODO: Parse/format
-                //         )
-                //     })
-                // ))
+                output.append(DiscordEmbed(
+                    title: wikitextParse.title,
+                    url: wikitextParse.title.flatMap(self.wikiLink(page:)),
+                    fields: Array(doc.sections.prefix(5).map {
+                        DiscordEmbed.Field(
+                            name: $0.title ?? "Section",
+                            value: self.markdownFrom(nodes: $0.content).truncate(200, appending: "...").nilIfEmpty ?? "_no text_"
+                        )
+                    })
+                ))
             } catch {
                 output.append(error, errorText: "Could not fetch/parse wikitext from Minecraft wiki")
             }
         }
+    }
+    
+    private func markdownFrom(nodes: [MinecraftWikitextDocument.Section.Node]) -> String {
+        nodes.map {
+            switch $0 {
+                case .text(let text): return text
+                case .link(let page): return "[\(page)](\(wikiLink(page: page)?.absoluteString ?? page))"
+                case .template(let name, let params): return name // TODO
+            }
+        }.joined(separator: " ")
+    }
+    
+    private func wikiLink(page: String) -> URL? {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "minecraft.gamepedia.com"
+        components.path = "/\(page)"
+        return components.url
     }
 }
