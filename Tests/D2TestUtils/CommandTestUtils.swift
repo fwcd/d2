b15d781f0d86486e578063baf73b5a968d1188f1
fwcd <fwcdmail@gmail.com>
2020-03-24 18:03:52 +0100

    Fix command test extension

--- Tests/D2TestUtils/CommandTestUtils.swift
+++ Tests/D2TestUtils/CommandTestUtils.swift
@@ -5,7 +5,7 @@ extension Command {
 	public func testInvoke(
 		input: RichValue = .none,
 		output: CommandOutput,
-		context: CommandContext = CommandContext(guild: nil, registry: CommandRegistry(), message: DiscordMessage(content: ""), commandPrefix: "")
+		context: CommandContext = CommandContext(guild: nil, registry: CommandRegistry(), message: DiscordMessage(content: ""), commandPrefix: "", subscriptions: SubscriptionSet())
 	) {
 		invoke(input: input, output: output, context: context)
 	}
@@ -13,8 +13,8 @@ extension Command {
 	public func testSubscriptionMessage(
 		withContent content: String,
 		output: CommandOutput,
-		context: CommandContext = CommandContext(guild: nil, registry: CommandRegistry(), message: DiscordMessage(content: ""), commandPrefix: "")
-	) -> SubscriptionAction {
-		return onSubscriptionMessage(withContent: content, output: output, context: context)
+		context: CommandContext = CommandContext(guild: nil, registry: CommandRegistry(), message: DiscordMessage(content: ""), commandPrefix: "", subscriptions: SubscriptionSet())
+	) {
+		onSubscriptionMessage(withContent: content, output: output, context: context)
 	}
 }
