2b50b06b65be00c8cc0ab8458c0964a2c1d81b65
fwcd <fwcdmail@gmail.com>
2020-03-24 01:22:57 +0100

    Output the actual color in ColorCommand

--- Sources/D2Commands/imaging/ColorCommand.swift
+++ Sources/D2Commands/imaging/ColorCommand.swift
@@ -13,11 +13,17 @@ public class ColorCommand: StringCommand {
     
     public func invoke(withStringInput input: String, output: CommandOutput, context: CommandContext) {
         do {
+            guard let hexCode = UInt32(input, radix: 16) else {
+                output.append(errorText: "Please enter a hex code as argument!")
+                return
+            }
+            let isRGB = ((hexCode >> 24) & 0xFF) == 0
+            let color = isRGB ? Color(rgb: hexCode) : Color(argb: hexCode)
             let width = 100
             let height = 50
             let image = try Image(width: width, height: height)
             var graphics = CairoGraphics(fromImage: image)
-            graphics.draw(Rectangle(fromX: 0, y: 0, width: Double(width), height: Double(height), isFilled: true))
+            graphics.draw(Rectangle(fromX: 0, y: 0, width: Double(width), height: Double(height), color: color, isFilled: true))
             try output.append(image)
         } catch {
             output.append(error, errorText: "Could not create image")
