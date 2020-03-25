cf409db7a47fa9c6196078056e087030e93b6667
fwcd <fwcdmail@gmail.com>
2020-03-25 22:44:33 +0100

    Use actual logger in TelegramBot

--- Sources/D2TelegramIO/TelegramRun.swift
+++ Sources/D2TelegramIO/TelegramRun.swift
@@ -1,9 +1,14 @@
 import D2MessageIO
+import Logging
 import TelegramBotSDK
+
+fileprivate let log = Logger(label: "TelegramRun")
 
 /** Runs the Telegram-based backend and blocks the thread. */
 public func runTelegramIO(with delegate: MessageDelegate, combinedClient: CombinedMessageClient, token: String) {
     let bot = TelegramBot(token: token)
+    bot.logger = { log.debug("\($0)") }
+
     let overlayClient = combinedClient.register(client: TelegramMessageClient(bot: bot))
 
     while let update = bot.nextUpdateSync() {
