075ff63931d61bc2fa59effb544d30f13c7923f2
fwcd <fwcdmail@gmail.com>
2020-04-13 03:06:45 +0200

    Allow users to fetch other's inventories

--- Sources/D2Commands/misc/InventoryCommand.swift
+++ Sources/D2Commands/misc/InventoryCommand.swift
@@ -13,13 +13,13 @@ public class InventoryCommand: StringCommand {
     }
     
     public func invoke(withStringInput input: String, output: CommandOutput, context: CommandContext) {
-        guard let author = context.author else {
+        guard let user = context.message.mentions.first ?? context.author else {
             output.append(errorText: "No author available")
             return
         }
-        let inventory = inventoryManager[author]
+        let inventory = inventoryManager[user]
         output.append(Embed(
-            title: "Inventory for `\(author.username)`",
+            title: "Inventory for `\(user.username)`",
             fields: inventory.items.map {
                 Embed.Field(name: $0.0, value: Dictionary(grouping: $0.1, by: { $0.id })
                     .map { "\($0.1.count)x \($0.1.first?.name ?? "?")" }
