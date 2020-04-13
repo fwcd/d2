fb0f98f2d4836695fa8c103b29ba17cfcb0694fc
fwcd <fwcdmail@gmail.com>
2020-04-13 02:29:58 +0200

    Cache the pokedex

--- Sources/D2NetAPIs/pokemon/PokedexQuery.swift
+++ Sources/D2NetAPIs/pokemon/PokedexQuery.swift
@@ -1,14 +1,28 @@
 import D2Utils
 
 public struct PokedexQuery {
+    private static var cached: [PokedexEntry]? = nil
+
     public init() {}
 
     public func perform(then: @escaping (Result<[PokedexEntry], Error>) -> Void) {
-        do {
-            let request = try HTTPRequest(host: "randompokemon.com", path: "/dex/all.json")
-            request.fetchJSONAsync(as: [PokedexEntry].self, then: then)
-        } catch {
-            then(.failure(error))
+        if let pokedex = PokedexQuery.cached {
+            then(.success(pokedex))
+        } else {
+            do {
+                let request = try HTTPRequest(host: "randompokemon.com", path: "/dex/all.json")
+                request.fetchJSONAsync(as: [PokedexEntry].self) {
+                    switch $0 {
+                        case .success(let pokedex):
+                            PokedexQuery.cached = pokedex
+                            then(.success(pokedex))
+                        case .failure(let error):
+                            then(.failure(error))
+                    }
+                }
+            } catch {
+                then(.failure(error))
+            }
         }
     }
 }
