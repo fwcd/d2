de62deb992775c5f30507ff5acb1e296775aca0a
fwcd <fwcdmail@gmail.com>
2020-04-27 21:25:26 +0200

    Add note about swiftplot

--- Package.swift
+++ Package.swift
@@ -24,6 +24,8 @@ let package = Package(
         .package(url: "https://github.com/swift-server/swift-backtrace.git", from: "1.1.1"),
         .package(url: "https://github.com/safx/Emoji-Swift.git", .revision("b3a49f4a9fbee3c7320591dbc7263c192244063e")),
         .package(url: "https://github.com/stephencelis/SQLite.swift", from: "0.12.2")
+        // TODO: Integrate swiftplot, this is however blocked on https://bugs.swift.org/browse/SR-679
+        //       due to a dependency target collision (CFreeType)
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
