27ba5b1e2b32152862839e929d0f1af41c5b3cf8
fwcd <fwcdmail@gmail.com>
2020-05-20 04:07:05 +0200

    Simplify ConcatCommand implementation

--- Sources/D2Commands/scripting/ConcatCommand.swift
+++ Sources/D2Commands/scripting/ConcatCommand.swift
@@ -1,19 +1,18 @@
 import D2Permissions
 
-public class ConcatCommand: Command {
+public class ConcatCommand: StringCommand {
 	public let info = CommandInfo(
 		category: .scripting,
 		shortDescription: "Concatenates the input values",
 		longDescription: "Concatenates a compound input as text",
 		requiredPermissionLevel: .basic
 	)
-	public let inputValueType: RichValueType = .compound([.text])
 	public let outputValueType: RichValueType = .text
 	private let separator = " "
 	
 	public init() {}
 	
-	public func invoke(input: RichValue, output: CommandOutput, context: CommandContext) {
-		output.append(input.values.compactMap { $0.asText }.joined(separator: separator))
+	public func invoke(withStringInput input: String, output: CommandOutput, context: CommandContext) {
+		output.append(input)
 	}
 }
