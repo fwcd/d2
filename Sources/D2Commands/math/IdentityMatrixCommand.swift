8b5728f13fbfb05517d3e57e1db7808b4f107b07
fwcd <fwcdmail@gmail.com>
2020-05-26 02:33:58 +0200

    Validate input size in matrix generators

--- Sources/D2Commands/math/IdentityMatrixCommand.swift
+++ Sources/D2Commands/math/IdentityMatrixCommand.swift
@@ -7,13 +7,24 @@ public class IdentityMatrixCommand: Command {
         helpText: "Syntax: [size]",
         requiredPermissionLevel: .basic
     )
+    private let sizeLimit: Int
 
-    public init() {}
+    public init(sizeLimit: Int = 8) {
+        self.sizeLimit = sizeLimit
+    }
 
     public func invoke(input: RichValue, output: CommandOutput, context: CommandContext) {
         guard let size = input.asText.flatMap({ Int($0) }) else {
             output.append(errorText: "Please specify a single integer as size!")
             return
+        }
+        guard size <= sizeLimit else {
+            output.append(errorText: "Please use a smaller size!")
+            return
+        }
+        guard size >= 0 else {
+            output.append(errorText: "Please use a non-negative size!")
+            return 
         }
 
         output.append(.ndArrays([Matrix.identity(width: size).asNDArray]))
