853675514895e19e2ecdf6bb0c6aa033a3908c6d
fwcd <fwcdmail@gmail.com>
2020-05-26 02:43:00 +0200

    Add IdentityCommand

---
+++ Sources/D2Commands/scripting/IdentityCommand.swift
@@ -0,0 +1,13 @@
+public class IdentityCommand: Command {
+    public let info = CommandInfo(
+        category: .scripting,
+        shortDescription: "Appends the input to the output",
+        requiredPermissionLevel: .basic
+    )
+
+    public init() {}
+
+	public func invoke(input: RichValue, output: CommandOutput, context: CommandContext) {
+        output.append(input)
+    }
+}
