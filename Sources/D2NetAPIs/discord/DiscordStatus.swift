5f66ac226df2fa69888767b2aef7f4fa24c97946
fwcd <fwcdmail@gmail.com>
2020-06-16 14:47:53 +0200

    Add Discord status API

---
+++ Sources/D2NetAPIs/discord/DiscordStatus.swift
@@ -0,0 +1,60 @@
+public struct DiscordStatus: Codable {
+    public let page: Page?
+    public let components: [Component]?
+    public let incidents: [Incident]?
+    public let status: Status?
+
+    public struct Page: Codable {
+        public enum CodingKeys: String, CodingKey {
+            case id
+            case name
+            case url
+            case timeZone = "time_zone"
+            case updatedAt = "updated_at"
+        }
+
+        public let id: String
+        public let name: String
+        public let url: String?
+        public let timeZone: String?
+        public let updatedAt: String?
+    }
+
+    public struct Component: Codable {
+        public enum CodingKeys: String, CodingKey {
+            case id
+            case name
+            case createdAt = "created_at"
+            case updatedAt = "updated_at"
+            case position
+            case description
+            case showcase
+            case groupId = "group_id"
+            case pageId = "page_id"
+            case group
+            case onlyShowIfDegraded = "only_show_if_degraded"
+        }
+
+        public let id: String
+        public let name: String
+        public let createdAt: String?
+        public let updatedAt: String?
+        public let position: Int?
+        public let description: String?
+        public let showcase: Bool?
+        public let groupId: String?
+        public let pageId: String?
+        public let group: Bool?
+        public let onlyShowIfDegraded: Bool?
+    }
+
+    public struct Incident: Codable {
+        public let id: String
+        public let name: String
+    }
+
+    public struct Status: Codable {
+        public let indicator: String
+        public let description: String
+    }
+}
---
+++ Sources/D2NetAPIs/discord/DiscordStatusQuery.swift
@@ -0,0 +1,12 @@
+import D2Utils
+
+public struct DiscordStatusQuery {
+    public func perform(then: @escaping (Result<DiscordStatus, Error>) -> Void) {
+        do {
+            let request = try HTTPRequest(host: "status.discord.com", path: "/api/v2/summary.json")
+            request.fetchJSONAsync(as: DiscordStatus.self, then: then)
+        } catch {
+            then(.failure(error))
+        }
+    }
+}
