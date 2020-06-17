22e687a62174012af014aae4d3cf74d89e036d83
fwcd <fwcdmail@gmail.com>
2020-06-17 18:11:06 +0200

    Add timer subcommands

--- Sources/D2Commands/misc/TimerCommand.swift
+++ Sources/D2Commands/misc/TimerCommand.swift
@@ -3,7 +3,7 @@ import D2Utils
 import D2MessageIO
 import Dispatch
 
-fileprivate let argsPattern = try! Regex(from: "(?:(\\w+)\\s+)?(.+)")
+fileprivate let argsPattern = try! Regex(from: "(?:([a-zA-Z]+)\\s*)?(.*)")
 fileprivate let durationPattern = try! Regex(from: "(\\d+)\\s*([a-zA-Z]+)")
 fileprivate let flagPattern = try! Regex(from: "--([a-z]+)")
 fileprivate let timeUnits: [String: (Int) -> Int] = [
@@ -24,15 +24,40 @@ fileprivate struct NamedTimer {
     let name: String?
     let guildId: GuildID?
     let timer: DispatchSourceTimer
+    let elapseDate: Date
+
+    var remainingTime: TimeInterval { elapseDate.timeIntervalSinceNow }
 }
 
 public class TimerCommand: StringCommand {
-    public let info = CommandInfo(
+    public private(set) var info = CommandInfo(
         category: .misc,
         shortDescription: "Pings a group of users after the timer elapses",
-        helpText: """
+        requiredPermissionLevel: .vip
+    )
+    private var subcommands: [String: (String, CommandOutput, CommandContext) -> Void] = [:]
+    private var timers: [Int: NamedTimer] = [:]
+    private var nextTimerId: Int = 0
+
+    public init() {
+        subcommands = [
+            "list": { [unowned self] input, output, context in
+                output.append(Embed(
+                    title: ":timer: Running Timers",
+                    description: self.timers.values
+                        .filter { $0.guildId == context.guild?.id }
+                        .sorted(by: descendingComparator { $0.remainingTime })
+                        .map { "`\($0.name ?? "<unnamed>")` elapses in \($0.remainingTime.displayString)" }
+                        .joined(separator: "\n")
+                ))
+            }
+        ]
+        info.helpText = """
             Syntax: `[name]? [[number] [s|m|h]]+ [--here]? [--everyone]?`
             ...or alternatively: `[subcommand] [name]`
+
+            Available Subcommands:
+            \(subcommands.keys.map { "`\($0)`" }.joined(separator: ", "))
             
             Examples:
             - Creation: `timer 4s`, `timer 1m 1s`, `timer myTimer 5m`
@@ -40,55 +65,59 @@ public class TimerCommand: StringCommand {
             - Showing all running timers on this guild: `timer list`
 
             Note that only named timers can be cancelled!
-            """,
-        requiredPermissionLevel: .vip
-    )
-    private var timers: [Int: NamedTimer] = [:]
-    private var nextTimerId: Int = 0
-
-    public init() {}
+            """
+    }
 
     public func invoke(withStringInput input: String, output: CommandOutput, context: CommandContext) {
         guard let parsedArgs = argsPattern.firstGroups(in: input) else {
             output.append(errorText: info.helpText!)
             return
         }
-
+        print(parsedArgs)
         let name = parsedArgs[1].nilIfEmpty
-        let rawDurations = parsedArgs[2]
-        let allParsedDurations = durationPattern.allGroups(in: rawDurations)
-        let flags = flagPattern.allGroups(in: input).map { $0[1] }
-        let durations = allParsedDurations.compactMap { timeUnits[timeUnitAliases[$0[2]] ?? $0[2]]?(Int($0[1])!) }
 
-        guard !durations.isEmpty else {
-            output.append(errorText: info.helpText!)
-            return
-        }
+        if let n = name, let subcommand = subcommands[n] {
+            // Invoke the subcommand
 
-        let authorId = context.author?.id
-        let guildId = context.guild?.id
-        let duration = durations.reduce(0, +)
+            subcommand(parsedArgs[2], output, context)
+        } else {
+            // Create a new timer
 
-        let timerId = nextTimerId
-        nextTimerId += 1
+            let rawDurations = parsedArgs[2]
+            let allParsedDurations = durationPattern.allGroups(in: rawDurations)
+            let flags = flagPattern.allGroups(in: input).map { $0[1] }
+            let durations = allParsedDurations.compactMap { timeUnits[timeUnitAliases[$0[2]] ?? $0[2]]?(Int($0[1])!) }
 
-        let timer = DispatchSource.makeTimerSource()
-        timer.schedule(deadline: .now() + .seconds(duration))
-        timer.setEventHandler {
-            let mention: String
-            if flags.contains("everyone") {
-                mention = "@everyone"
-            } else if flags.contains("here") {
-                mention = "@here"
-            } else {
-                mention = authorId.map { "<@\($0)>" } ?? ""
+            guard !durations.isEmpty else {
+                output.append(errorText: info.helpText!)
+                return
             }
-            output.append("\(mention), the timer\(name.map { " `\($0)`" } ?? "") has elapsed!")
-            self.timers[timerId] = nil
-        }
-        timers[timerId] = NamedTimer(name: name, guildId: guildId, timer: timer)
 
-        output.append("Created a timer\(name.map { " named `\($0)`" } ?? "") that runs for \(duration) \("second".pluralize(with: duration))!")
-        timer.resume()
+            let authorId = context.author?.id
+            let guildId = context.guild?.id
+            let duration = durations.reduce(0, +)
+
+            let timerId = nextTimerId
+            nextTimerId += 1
+
+            let timer = DispatchSource.makeTimerSource()
+            timer.schedule(deadline: .now() + .seconds(duration))
+            timer.setEventHandler {
+                let mention: String
+                if flags.contains("everyone") {
+                    mention = "@everyone"
+                } else if flags.contains("here") {
+                    mention = "@here"
+                } else {
+                    mention = authorId.map { "<@\($0)>" } ?? ""
+                }
+                output.append("\(mention), the timer\(name.map { " `\($0)`" } ?? "") has elapsed!")
+                self.timers[timerId] = nil
+            }
+            timers[timerId] = NamedTimer(name: name, guildId: guildId, timer: timer, elapseDate: Date() + TimeInterval(duration))
+
+            output.append("Created a timer\(name.map { " named `\($0)`" } ?? "") that runs for \(duration) \("second".pluralize(with: duration))!")
+            timer.resume()
+        }
     }
 }
