4e0724ab2262a0b1cf0eb6832ddc18285be996a6
fwcd <fwcdmail@gmail.com>
2020-07-06 14:46:35 +0200

    Make DiceRollCommand's output prettier

--- Sources/D2Commands/misc/DiceRollCommand.swift
+++ Sources/D2Commands/misc/DiceRollCommand.swift
@@ -1,3 +1,14 @@
+import D2MessageIO
+
+fileprivate let diceFaces = [
+    1: [[false, false, false], [false, true, false], [false, false, false]],
+    2: [[false, false, true], [false, false, false], [true, false, false]],
+    3: [[false, false, true], [false, true, false], [true, false, false]],
+    4: [[true, false, true], [false, false, false], [true, false, true]],
+    5: [[true, false, true], [false, true, false], [true, false, true]],
+    6: [[true, true, true], [false, false, false], [true, true, true]]
+]
+
 public class DiceRollCommand: StringCommand {
     public let info = CommandInfo(
         category: .misc,
@@ -6,17 +17,27 @@ public class DiceRollCommand: StringCommand {
         requiredPermissionLevel: .basic
     )
     private let range: Range<Int>
+    private let fancy: Bool
 
-    public convenience init(_ closedRange: ClosedRange<Int>) {
-        self.init(Range(closedRange))
+    public convenience init(_ closedRange: ClosedRange<Int>, fancy: Bool = true) {
+        self.init(Range(closedRange), fancy: fancy)
     }
 
-    public init(_ range: Range<Int>) {
+    public init(_ range: Range<Int>, fancy: Bool = true) {
         self.range = range
+        self.fancy = fancy
     }
 
     public func invoke(withStringInput input: String, output: CommandOutput, context: CommandContext) {
         let result = Int.random(in: range)
-        output.append("\(result)")
+
+        if fancy {
+            output.append(Embed(
+                title: "Result: \(result)",
+                description: diceFaces[result].map { $0.map { $0.map { $0 ? ":white_square_button:" : ":white_large_square:" }.joined() }.joined(separator: "\n") }
+            ))
+        } else {
+            output.append("\(result)")
+        }
     }
 }
