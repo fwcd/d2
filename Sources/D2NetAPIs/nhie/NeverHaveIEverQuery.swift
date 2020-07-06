3af0ee92cf109c1dafac3aec4d34824254f4f672
fwcd <fwcdmail@gmail.com>
2020-07-06 16:24:00 +0200

    Recursively query nhie pages

--- Sources/D2NetAPIs/nhie/NeverHaveIEverQuery.swift
+++ Sources/D2NetAPIs/nhie/NeverHaveIEverQuery.swift
@@ -8,17 +8,26 @@ public struct NeverHaveIEverQuery {
         self.maxPages = maxPages
     }
 
-    public func perform(page: Int? = nil, then: @escaping (Result<[NeverHaveIEverStatement], Error>) -> Void) {
+    public func perform(page: Int? = nil, prepending: [NeverHaveIEverStatement] = [], then: @escaping (Result<[NeverHaveIEverStatement], Error>) -> Void) {
         do {
             let request = try HTTPRequest(host: "nnnever.com", path: "/\(page.map { "\($0)" } ?? "")")
             request.fetchHTMLAsync { result in
-                then(Result {
+                do {
                     let document = try result.get()
                     let rawStatements = try document.select(".question .title").array()
                     let statements = try rawStatements.map { NeverHaveIEverStatement(statement: try $0.text()) }
 
-                    return statements
-                })
+                    let nextPage = (page ?? 0) + 1
+                    let nextLinks = try document.select("pagination .page-link:contains(Next)")
+
+                    if nextPage < self.maxPages && !nextLinks.isEmpty() {
+                        perform(page: nextPage, prepending: prepending + statements, then: then)
+                    } else {
+                        then(.success(prepending + statements))
+                    }
+                } catch {
+                    then(.failure(error))
+                }
             }
         } catch {
             then(.failure(error))
