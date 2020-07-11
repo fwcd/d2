ab725b9a031ee1bb5891c6b923dcc759be70265a
fwcd <fwcdmail@gmail.com>
2020-07-11 18:39:36 +0200

    Split messages as needed in MessageIOOutput

--- Sources/D2Commands/output/MessageIOOutput.swift
+++ Sources/D2Commands/output/MessageIOOutput.swift
@@ -7,6 +7,7 @@ import FoundationNetworking
 import Logging
 
 fileprivate let log = Logger(label: "D2Commands.MessageIOOutput")
+fileprivate let contentLimit = 2000
 
 public class MessageIOOutput: CommandOutput {
 	private var context: CommandContext
@@ -31,38 +32,58 @@ public class MessageIOOutput: CommandOutput {
 		}
 
 		messageWriter.write(value: value).listen {
-			var message: Message
+			var messages: [Message]
 			do {
-				message = try $0.get()
+				messages = self.splitUp(message: try $0.get())
 			} catch {
 				log.error("Error while encoding message: \(error)")
-				message = Message(content: """
+				messages = [Message(content: """
 					An error occurred while encoding the message:
 					```
 					\(error)
 					```
-					""")
+					""")]
 			}
 
-			switch channel {
-				case .guildChannel(let id):
-					client.sendMessage(message, to: id)
-				case .dmChannel(let id):
-					client.createDM(with: id) { channelId, _ in
-						guard let id = channelId else {
-							log.error("Could not send direct message, since no channel ID could be fetched")
-							return
+			for message in messages {
+				switch channel {
+					case .guildChannel(let id):
+						client.sendMessage(message, to: id)
+					case .dmChannel(let id):
+						client.createDM(with: id) { channelId, _ in
+							guard let id = channelId else {
+								log.error("Could not send direct message, since no channel ID could be fetched")
+								return
+							}
+							client.sendMessage(message, to: id, then: self.onSent)
 						}
-						client.sendMessage(message, to: id, then: self.onSent)
-					}
-				case .defaultChannel:
-					if let textChannelId = self.context.channel?.id {
-						client.sendMessage(message, to: textChannelId, then: self.onSent)
-					} else {
-						log.warning("No default text channel available")
-					}
+					case .defaultChannel:
+						if let textChannelId = self.context.channel?.id {
+							client.sendMessage(message, to: textChannelId, then: self.onSent)
+						} else {
+							log.warning("No default text channel available")
+						}
+				}
 			}
 		}
+	}
+
+	private func splitUp(message: Message) -> [Message] {
+		var remaining = message
+		var results = [Message]()
+
+		while remaining.content.count > contentLimit {
+			results.append(Message(content: String(remaining.content.prefix(contentLimit))))
+			remaining.content.removeFirst(contentLimit)
+		}
+
+		while remaining.embeds.count > 1, let embed = remaining.embeds.first {
+			results.append(Message(embed: embed))
+			remaining.embeds.removeFirst()
+		}
+
+		results.append(remaining)
+		return results
 	}
 
 	public func update(context: CommandContext) {
