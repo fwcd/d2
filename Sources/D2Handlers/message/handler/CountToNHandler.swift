a65b1a6468a554e54f8176d2462accee31bc858b
fwcd <fwcdmail@gmail.com>
2020-07-14 21:33:23 +0200

    Add CountToNHandler

---
+++ Sources/D2Handlers/message/handler/CountToNHandler.swift
@@ -0,0 +1,15 @@
+import D2Commands
+import D2MessageIO
+import D2Utils
+
+fileprivate let countToNPattern = try! Regex(from: "count\\s+to\\s+(\\d+)")
+
+public struct CountToNHandler: MessageHandler {
+    public func handle(message: Message, from client: MessageClient) -> Bool {
+        if let parsed = countToNPattern.firstGroups(in: message.content.lowercased()), let n = UInt(parsed[1]), n <= 300, let channelId = message.channelId {
+            client.sendMessage(Message(content: "Here you go: \((1...n).map(String.init).joined(separator: ", "))"), to: channelId)
+            return true
+        }
+        return false
+    }
+}
