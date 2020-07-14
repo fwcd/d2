090bd7b12c860006458983b8136ee562c8ce64dc
fwcd <fwcdmail@gmail.com>
2020-07-15 01:39:41 +0200

    Move Direction into D2Utils

--- Sources/D2Commands/imaging/animation/ScrollTransform.swift
+++ Sources/D2Commands/imaging/animation/ScrollTransform.swift
@@ -6,25 +6,6 @@ public struct ScrollTransform: ImageTransform {
         case direction
     }
 
-    public enum Direction: String {
-        case up
-        case down
-        case left
-        case right
-
-        public var asUnitVector: Vec2<Int> {
-            switch self {
-                case .up: return Vec2(x: 0, y: -1)
-                case .down: return Vec2(x: 0, y: 1)
-                case .left: return Vec2(x: 1, y: 0)
-                case .right: return Vec2(x: -1, y: 0)
-            }
-        }
-        public var horizontal: Bool {
-            self == .left || self == .right
-        }
-    }
-
     private let pos: Vec2<Int>?
     private let speed: Int
     private let direction: Direction
