bb84b7ff5cb3aafe31a15265906b0b6a3b4b4f06
fwcd <fwcdmail@gmail.com>
2020-07-15 18:24:08 +0200

    Make sure that the black keys are rendered on top
    Also tweak the parameters of the key sizes on the rendered piano
    board.

--- Sources/D2Commands/music/piano/PianoRenderer.swift
+++ Sources/D2Commands/music/piano/PianoRenderer.swift
@@ -11,10 +11,10 @@ struct PianoRenderer: ScaleRenderer {
 
     init(
         whiteKeyWidth: Int = 20,
-        blackKeyWidth: Int = 20,
+        blackKeyWidth: Int = 15,
         whiteKeyPadding: Int = 2,
-        whiteKeyHeight: Int = 100,
-        blackKeyHeight: Int = 80,
+        whiteKeyHeight: Int = 80,
+        blackKeyHeight: Int = 60,
         range: Range<Note>
     ) {
         self.whiteKeyWidth = whiteKeyWidth
@@ -30,15 +30,27 @@ struct PianoRenderer: ScaleRenderer {
         let width = whiteKeyWidth * whiteKeyCount + whiteKeyPadding * (whiteKeyCount - 1)
         let image = try Image(width: width, height: whiteKeyHeight)
         var graphics = CairoGraphics(fromImage: image)
+        var whiteKeys = [Rectangle<Double>]()
+        var blackKeys = [Rectangle<Double>]()
         var x = 0
 
         for note in range {
             if note.accidental == .none {
-                graphics.draw(Rectangle(fromX: Double(x), y: 0, width: Double(whiteKeyWidth), height: Double(whiteKeyHeight), color: Colors.white, isFilled: true))
+                whiteKeys.append(Rectangle(fromX: Double(x), y: 0, width: Double(whiteKeyWidth), height: Double(whiteKeyHeight), color: Colors.white, isFilled: true))
                 x += whiteKeyWidth + whiteKeyPadding
             } else {
-                graphics.draw(Rectangle(fromX: Double(x - (blackKeyWidth / 2)), y: 0, width: Double(blackKeyWidth), height: Double(blackKeyHeight), color: Colors.black, isFilled: true))
+                blackKeys.append(Rectangle(fromX: Double(x - (blackKeyWidth / 2)), y: 0, width: Double(blackKeyWidth), height: Double(blackKeyHeight), color: Colors.black, isFilled: true))
             }
+        }
+
+        // Make sure that the black keys are visually "on top" of the white keys
+
+        for whiteKey in whiteKeys {
+            graphics.draw(whiteKey)
+        }
+
+        for blackKey in blackKeys {
+            graphics.draw(blackKey)
         }
 
         return image
