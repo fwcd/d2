84eb4067af6aeef6462ae6217cff9498c21c97ce
fwcd <fwcdmail@gmail.com>
2020-07-15 18:21:43 +0200

    Add padding to the white keys

--- Sources/D2Commands/music/piano/PianoRenderer.swift
+++ Sources/D2Commands/music/piano/PianoRenderer.swift
@@ -4,6 +4,7 @@ import D2Utils
 struct PianoRenderer: ScaleRenderer {
     private let whiteKeyWidth: Int
     private let blackKeyWidth: Int
+    private let whiteKeyPadding: Int
     private let whiteKeyHeight: Int
     private let blackKeyHeight: Int
     private let range: Range<Note>
@@ -11,19 +12,22 @@ struct PianoRenderer: ScaleRenderer {
     init(
         whiteKeyWidth: Int = 20,
         blackKeyWidth: Int = 20,
+        whiteKeyPadding: Int = 2,
         whiteKeyHeight: Int = 100,
         blackKeyHeight: Int = 80,
         range: Range<Note>
     ) {
         self.whiteKeyWidth = whiteKeyWidth
         self.blackKeyWidth = blackKeyWidth
+        self.whiteKeyPadding = whiteKeyPadding
         self.whiteKeyHeight = whiteKeyHeight
         self.blackKeyHeight = blackKeyHeight
         self.range = range
     }
 
 	func render(scale: Scale) throws -> Image {
-        let width = whiteKeyWidth * range.count(forWhich: { $0.accidental == .none })
+        let whiteKeyCount = range.count(forWhich: { $0.accidental == .none })
+        let width = whiteKeyWidth * whiteKeyCount + whiteKeyPadding * (whiteKeyCount - 1)
         let image = try Image(width: width, height: whiteKeyHeight)
         var graphics = CairoGraphics(fromImage: image)
         var x = 0
@@ -31,7 +35,7 @@ struct PianoRenderer: ScaleRenderer {
         for note in range {
             if note.accidental == .none {
                 graphics.draw(Rectangle(fromX: Double(x), y: 0, width: Double(whiteKeyWidth), height: Double(whiteKeyHeight), color: Colors.white, isFilled: true))
-                x += whiteKeyWidth
+                x += whiteKeyWidth + whiteKeyPadding
             } else {
                 graphics.draw(Rectangle(fromX: Double(x - (blackKeyWidth / 2)), y: 0, width: Double(blackKeyWidth), height: Double(blackKeyHeight), color: Colors.black, isFilled: true))
             }
