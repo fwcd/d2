bb69826cd2f2943fbc1f4a944e4f59fd2c634558
fwcd <fwcdmail@gmail.com>
2020-07-16 18:10:53 +0200

    Add option to query the streamer role

--- Sources/D2Commands/misc/TwitchRoleCommand.swift
+++ Sources/D2Commands/misc/TwitchRoleCommand.swift
@@ -13,16 +13,18 @@ public class TwitchRoleCommand: Command {
     }
 
     public func invoke(input: RichValue, output: CommandOutput, context: CommandContext) {
-        guard let role = input.asRoleMentions?.first else {
-            output.append(errorText: "Please mention a role to use!")
-            return
-        }
         guard let guild = context.guild else {
             output.append(errorText: "Not on a guild")
             return
         }
 
-        twitchRoleConfiguration.wrappedValue.twitchRoles[guild.id] = role
-        output.append("Successfully set the Twitch streamer role!")
+        if let role = input.asRoleMentions?.first {
+            twitchRoleConfiguration.wrappedValue.twitchRoles[guild.id] = role
+            output.append("Successfully set the Twitch streamer role!")
+        } else {
+            let roleId = twitchRoleConfiguration.wrappedValue.twitchRoles[guild.id]
+            let roleName = roleId.flatMap { guild.roles[$0] }?.name ?? "none"
+            output.append("The Twitch streamer role on this guild is currently `\(roleName)`")
+        }
     }
 }
