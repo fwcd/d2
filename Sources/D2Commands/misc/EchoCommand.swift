eb15139744f5bdc5e0e7a46b56e073f445130a54
fwcd <fwcdmail@gmail.com>
2020-07-21 03:32:39 +0200

    Simplify echo and lower its permission level

--- Sources/D2Commands/misc/EchoCommand.swift
+++ Sources/D2Commands/misc/EchoCommand.swift
@@ -3,52 +3,17 @@ import Foundation
 import Dispatch
 import D2Utils
 
-fileprivate let argPattern = try! Regex(from: "(?:(\\d+)\\s*)?([\\s\\S]*)")
-
-public class EchoCommand: Command {
+public class EchoCommand: StringCommand {
 	public let info = CommandInfo(
 		category: .misc,
-		shortDescription: "Outputs something n times",
-		longDescription: "Outputs the given input n times with an interval of 1 second by default",
-		requiredPermissionLevel: .vip
+		shortDescription: "Outputs the string input",
+		requiredPermissionLevel: .basic
 	)
-	public let inputValueType: RichValueType = .any
-	public let outputValueType: RichValueType = .any
-	private let timer: RepeatingTimer
+	public let outputValueType: RichValueType = .text
 	
-	public init(intervalSeconds: Int = 1) {
-		timer = RepeatingTimer(interval: .seconds(intervalSeconds))
-	}
+	public init() {}
 	
-	public func invoke(input: RichValue, output: CommandOutput, context: CommandContext) {
-		if let groups = argPattern.firstGroups(in: input.asText ?? "") {
-			// Extract parsed values
-			let n = groups[safely: 1].flatMap { Int($0) } ?? 1
-			let value = groups[safely: 2]?.nilIfEmpty.map { .text($0) } ?? input
-			
-			if n == 1 {
-				// Output synchronously
-				append(value, to: output)
-			} else {
-				guard !timer.isRunning else {
-					output.append(errorText: "Cannot run multiple asynchronous `echo`s concurrently")
-					return
-				}
-				
-				timer.schedule(nTimes: n) { _, _ in
-					self.append(value, to: output)
-				}
-			}
-		}
-	}
-	
-	private func append(_ value: RichValue, to output: CommandOutput) {
-		let lengthLimit = output.messageLengthLimit ?? Int.max
-		
-		if (value.asText?.count ?? 0) > lengthLimit {
-			output.append(errorText: "Cannot echo value that is longer than \(lengthLimit) characters")
-		} else {
-			output.append(value)
-		}
+	public func invoke(withStringInput input: String, output: CommandOutput, context: CommandContext) {
+		output.append(input)
 	}
 }
