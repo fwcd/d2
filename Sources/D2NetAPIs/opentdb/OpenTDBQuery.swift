6dae1247b8643f3e0768fd8dabfcc40dd6bd8ce1
fwcd <fwcdmail@gmail.com>
2020-07-22 00:06:24 +0200

    Fix encode parameter in OpenTDBQuery

--- Sources/D2NetAPIs/opentdb/OpenTDBQuery.swift
+++ Sources/D2NetAPIs/opentdb/OpenTDBQuery.swift
@@ -9,10 +9,11 @@ public struct OpenTDBQuery {
 
     public func perform(then: @escaping (Result<OpenTDBResponse, Error>) -> Void) {
         do {
-            let request = try HTTPRequest(host: "opentdb.com", path: "/api.php", query: ["amount": String(amount), "encoding": "url3986"])
+            let request = try HTTPRequest(host: "opentdb.com", path: "/api.php", query: ["amount": String(amount), "encode": "url3986"])
             request.fetchJSONAsync(as: OpenTDBResponse.self) {
                 do {
                     var response = try $0.get()
+                    print(response)
                     response.results = response.results.map {
                         var r = $0
                         r.category = r.category.removingPercentEncoding ?? r.category
