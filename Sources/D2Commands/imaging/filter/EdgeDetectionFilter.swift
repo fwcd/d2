db013320c2351e44fbff9741f3126071283e9574
fwcd <fwcdmail@gmail.com>
2020-07-27 02:11:20 +0200

    Implement edge detection filter

---
+++ Sources/D2Commands/imaging/filter/EdgeDetectionFilter.swift
@@ -0,0 +1,11 @@
+import D2Utils
+
+public struct EdgeDetectionFilter: ImageFilter {
+    public let matrix: Matrix<Double>
+
+    public init(size: Int) {
+        var matrix = Matrix<Double>(repeating: -1, width: size, height: size)
+        matrix[size / 2, size / 2] = Double((size * size) - 1)
+        self.matrix = matrix
+    }
+}
