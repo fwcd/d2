60bbd73939d9852c6d2f365b2ab7fb7888069618
fwcd <fwcdmail@gmail.com>
2020-07-28 00:59:03 +0200

    WIP: Update doc comments in CommandInfo

--- Sources/D2Commands/CommandInfo.swift
+++ Sources/D2Commands/CommandInfo.swift
@@ -1,30 +1,30 @@
 import D2Permissions
 
-/** A collection of metadata about a command. */
+/// A collection of metadata about a command.
 public struct CommandInfo {
-    /** The command's category in the help framework. */
+    /// The command's category in the help framework.
     public var category: CommandCategory
-    /** A short/one-lined description of the command. */
+    /// A short/one-lined description of the command.
     public var shortDescription: String
-    /** A long description/help text for the command. */
+    /// A long description/help text for the command.
     public var longDescription: String
-    /** Additional help/usage information about a command. */
+    /// Additional help/usage information about a command.
     public var helpText: String?
-    /** The command's permission level. */
+    /// The command's permission level.
     public var requiredPermissionLevel: PermissionLevel
-    /** Whether the command is should use a simulated permission level, if available. Should be true for almost every command. */
+    /// Whether the command is should use a simulated permission level, if available. Should be true for almost every command.
     public var usesSimulatedPermissionLevel: Bool
-    /** Whether the commands should be hidden in lists. */
+    /// Whether the commands should be hidden in lists.
     public var hidden: Bool
-    /** Whether the command will set up/handle subscriptions. Only for documentary purposes.. */
+    /// Whether the command will set up/handle subscriptions. Only for documentary purposes..
     public var subscribesToNextMessages: Bool
-    /** Whether the command can only be invoked by users (not bots). */
+    /// Whether the command can only be invoked by users (not bots).
     public var userOnly: Bool
-    /** Whether the command can receive subscription messages only by users (not bots). Overrides userOnly. */
+    /// Whether the command can receive subscription messages only by users (not bots). Overrides userOnly.
     public var subscriptionsUserOnly: Bool
-    /** The source file in which the command is located. */
+    /// The source file in which the command is located.
     public var sourceFile: String
-    /** If present, the only platforms on which this command is allowed to run. */
+    /// If present, the only platforms on which this command is allowed to run.
     public var platformAvailability: Set<String>?
     
     public init(
