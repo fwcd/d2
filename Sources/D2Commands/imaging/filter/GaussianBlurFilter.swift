0da2e70bc681cc0dc61013c9bea1cdbf9c742fa0
fwcd <fwcdmail@gmail.com>
2020-07-27 22:50:13 +0200

    Add (approximated) Gaussian blur

---
+++ Sources/D2Commands/imaging/filter/GaussianBlurFilter.swift
@@ -0,0 +1,12 @@
+import D2Utils
+
+fileprivate let accuracy: Int = 4
+
+public struct GaussianBlurFilter: ImageFilter {
+    public let matrices: [Matrix<Double>]
+
+    public init(size: Int) {
+        // Approximate a Gaussian blur using repeated box blurs
+        matrices = Array(repeating: BoxBlurFilter(size: size).matrices, count: accuracy).flatMap { $0 }
+    }
+}
