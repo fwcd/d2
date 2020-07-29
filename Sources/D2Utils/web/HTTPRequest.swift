d71c84e6f107d3701ac4a4ce118aa34042121900
fwcd <fwcdmail@gmail.com>
2020-07-29 18:23:44 +0200

    WIP: Migrate HTTPRequest to promises

--- Sources/D2Utils/web/HTTPRequest.swift
+++ Sources/D2Utils/web/HTTPRequest.swift
@@ -9,7 +9,7 @@ import SwiftSoup
 
 public struct HTTPRequest {
 	private var request: URLRequest
-	
+
 	public init(
 		scheme: String = "https",
 		host: String,
@@ -31,21 +31,21 @@ public struct HTTPRequest {
         if let p = port {
             components.port = p
         }
-		
+
 		let body: Data
-		
+
 		if isPost && !query.isEmpty {
 			body = components.percentEncodedQuery?.data(using: .utf8) ?? .init()
 			components.queryItems = []
 		} else {
 			body = customBody?.data(using: .utf8) ?? .init()
 		}
-		
+
 		guard let url = components.url else { throw NetworkError.couldNotCreateURL(components) }
-		
+
 		request = URLRequest(url: url)
 		request.httpMethod = method
-		
+
 		if isPost {
             if !query.isEmpty {
                 request.setValue("application/x-www-form-urlencoded;charset=UTF-8", forHTTPHeaderField: "Content-Type")
@@ -53,73 +53,65 @@ public struct HTTPRequest {
 			request.setValue("\(body.count)", forHTTPHeaderField: "Content-Length")
 			request.httpBody = body
 		}
-		
+
 		for (key, value) in headers {
 			request.setValue(value, forHTTPHeaderField: key)
 		}
 	}
-	
-	public func runAsync(then: @escaping (Result<Data, Error>) -> Void) {
-		URLSession.shared.dataTask(with: request) { data, response, error in
-			guard error == nil else {
-				then(.failure(NetworkError.ioError(error!)))
-				return
-			}
-			guard let data = data else {
-				then(.failure(NetworkError.missingData))
-				return
-			}
-			
-			then(.success(data))
-		}.resume()
+
+	public func runAsync() -> Promise<Data, Error> {
+        Promise { then in
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                guard error == nil else {
+                    then(.failure(NetworkError.ioError(error!)))
+                    return
+                }
+                guard let data = data else {
+                    then(.failure(NetworkError.missingData))
+                    return
+                }
+
+                then(.success(data))
+            }.resume()
+        }
 	}
-	
-	public func fetchUTF8Async(then: @escaping (Result<String, Error>) -> Void) {
-		runAsync {
-			guard case let .success(data) = $0 else {
-				guard case let .failure(error) = $0 else { fatalError("'Result' should always be either successful or insuccessful") }
-				then(.failure(error))
-				return
-			}
-			guard let utf8 = String(data: data, encoding: .utf8) else {
-				then(.failure(NetworkError.notUTF8(data)))
-				return
-			}
-			then(.success(utf8))
+
+	public func fetchUTF8Async() -> Promise<String, Error> {
+		runAsync().then {
+			if let utf8 = String(data: $0, encoding: .utf8) {
+                return Promise(.success(utf8))
+			} else {
+				return Promise(.failure(NetworkError.notUTF8($0)))
+            }
 		}
 	}
-	
-	public func fetchJSONAsync<T>(as type: T.Type, then: @escaping (Result<T, Error>) -> Void) where T: Decodable {
-		runAsync {
-			guard case let .success(data) = $0 else {
-				guard case let .failure(error) = $0 else { fatalError("'Result' should always be either successful or insuccessful") }
-				then(.failure(error))
-				return
+
+	public func fetchJSONAsync<T>(as type: T.Type) -> Promise<T, Error> where T: Decodable {
+		runAsync().then {
+			if let deserialized = try? JSONDecoder().decode(type, from: $0) {
+                return Promise(.success(deserialized))
+            } else {
+				return Promise(.failure(NetworkError.jsonDecodingError(String(data: $0, encoding: .utf8) ?? "<non-UTF-8-encoded data: \($0)>")))
 			}
-			guard let deserialized = try? JSONDecoder().decode(type, from: data) else {
-				then(.failure(NetworkError.jsonDecodingError(String(data: data, encoding: .utf8) ?? "<non-UTF-8-encoded data: \(data)>")))
-				return
-			}
-			then(.success(deserialized))
 		}
 	}
-	
-	public func fetchXMLAsync<T>(using delegate: XMLParserDelegate, then: @escaping (Result<T, Error>) -> Void) {
-		runAsync {
+
+	public func fetchXMLAsync(using delegate: XMLParserDelegate) {
+		runAsync().listen {
 			switch $0 {
 				case .success(let data):
 					let parser = XMLParser(data: data)
 					parser.delegate = delegate
 					_ = parser.parse()
 				case .failure(let error):
-					then(.failure(error))
+					delegate.parser(XMLParser(data: Data()), parseErrorOccurred: error)
 			}
 		}
 	}
 
-	public func fetchHTMLAsync(then: @escaping (Result<Document, Error>) -> Void) {
-		fetchUTF8Async {
-			then($0.flatMap { html in Result { try SwiftSoup.parse(html) } })
+	public func fetchHTMLAsync() -> Promise<Document, Error> {
+		fetchUTF8Async().then { html in
+			Promise(Result { try SwiftSoup.parse(html) })
 		}
 	}
 }
