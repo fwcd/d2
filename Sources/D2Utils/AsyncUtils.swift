55b2705a0b03dba90c3cd77edb4afd26b2f54e86
fwcd <fwcdmail@gmail.com>
2020-07-30 21:52:03 +0200

    Remove collect(thenables:) from AsyncUtils
    ...in favor of promises

--- Sources/D2Utils/AsyncUtils.swift
+++ Sources/D2Utils/AsyncUtils.swift
@@ -1,23 +1,5 @@
 import Dispatch
 
-/**
- * Turns a list of thenables into a thenable of lists.
- */
-public func collect<T>(prepending previous: [T] = [], thenables: [(@escaping (Result<T, Error>) -> Void) -> Void], then: @escaping (Result<[T], Error>) -> Void) {
-    guard let nextThenable = thenables.first else {
-        then(.success(previous))
-        return
-    }
-
-    nextThenable {
-        switch $0 {
-            case .success(let value): collect(prepending: previous + [value], thenables: Array(thenables.dropFirst()), then: then)
-            case .failure(let error): then(.failure(error))
-        }
-    }
-}
-
-/// Aggregates the result of multiple promises asynchronously.
 @discardableResult
 public func all<T, E>(promises: [Promise<T, E>]) -> Promise<[T], E> where E: Error {
     Promise { then in
