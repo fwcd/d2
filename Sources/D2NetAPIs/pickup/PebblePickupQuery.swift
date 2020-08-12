052b77ecaacb9d32e29a11c13fc7eb5612216908
fwcd <fwcdmail@gmail.com>
2020-08-12 16:45:18 +0200

    Rewrite PebblePickupQuery slightly more concisely

--- Sources/D2NetAPIs/pickup/PebblePickupQuery.swift
+++ Sources/D2NetAPIs/pickup/PebblePickupQuery.swift
@@ -4,9 +4,7 @@ public struct PebblePickupQuery: PickupLineQuery {
     public init() {}
 
     public func perform() -> Promise<PickupLine, Error> {
-        Promise.catchingThen {
-            let request = try HTTPRequest(host: "pebble-pickup.herokuapp.com", path: "/tweets/random")
-            return request.fetchJSONAsync(as: PickupLine.self)
-        }
+        Promise.catching { try HTTPRequest(host: "pebble-pickup.herokuapp.com", path: "/tweets/random") }
+            .then { $0.fetchJSONAsync(as: PickupLine.self) }
     }
 }
