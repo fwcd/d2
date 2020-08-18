6404c422046d35cd06be65d9f816ed8b7cd17471
fwcd <fwcdmail@gmail.com>
2020-08-18 14:50:19 +0200

    Add ThisForThatCommand

---
+++ Sources/D2Commands/fun/ThisForThatCommand.swift
@@ -0,0 +1,22 @@
+import D2NetAPIs
+
+public class ThisForThatCommand: StringCommand {
+    public let info = CommandInfo(
+        category: .fun,
+        shortDescription: "Fetches an idea about what to create",
+        requiredPermissionLevel: .basic
+    )
+
+    public init() {}
+
+    public func invoke(withStringInput input: String, output: CommandOutput, context: CommandContext) {
+        ThisForThatQuery().perform().listen {
+            do {
+                let tft = try $0.get()
+                output.append("**\(tft.this)** for **\(tft.that)**")
+            } catch {
+                output.append(error, errorText: "Could not fetch this for that")
+            }
+        }
+    }
+}
