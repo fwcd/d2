a84d436c63fd338c79824e8c6e230753e8cc1534
fwcd <fwcdmail@gmail.com>
2020-08-19 00:33:17 +0200

    Add complimentr API

---
+++ Sources/D2NetAPIs/complimentr/Compliment.swift
@@ -0,0 +1,3 @@
+public struct Compliment: Codable {
+    public let compliment: String
+}
---
+++ Sources/D2NetAPIs/complimentr/ComplimentrQuery.swift
@@ -0,0 +1,10 @@
+import D2Utils
+
+public struct ComplimentrQuery {
+    public init() {}
+
+    public func perform() -> Promise<Compliment, Error> {
+        Promise.catching { try HTTPRequest(host: "complimentr.com", path: "/api") }
+            .then { $0.fetchJSONAsync(as: Compliment.self) }
+    }
+}
