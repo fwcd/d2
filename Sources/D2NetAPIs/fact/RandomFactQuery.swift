dd49885bfb5e2318f8dd72ba4b07c4beaeb30896
fwcd <fwcdmail@gmail.com>
2020-08-18 14:29:52 +0200

    Make fact language customizable

--- Sources/D2NetAPIs/fact/RandomFactQuery.swift
+++ Sources/D2NetAPIs/fact/RandomFactQuery.swift
@@ -1,10 +1,14 @@
 import D2Utils
 
 public struct RandomFactQuery {
-    public init() {}
+    private let language: String?
+
+    public init(language: String? = nil) {
+        self.language = language
+    }
 
     public func perform() -> Promise<Fact, Error> {
-        Promise.catching { try HTTPRequest(host: "uselessfacts.jsph.pl", path: "/random.json") }
+        Promise.catching { try HTTPRequest(host: "uselessfacts.jsph.pl", path: "/random.json", query: language.map { ["language": $0] } ?? [:]) }
             .then { $0.fetchJSONAsync(as: Fact.self) }
     }
 }
