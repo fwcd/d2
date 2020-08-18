297fbd11027de0e8817bdf3d162dc6deb3c4a5a3
fwcd <fwcdmail@gmail.com>
2020-08-18 21:41:06 +0200

    Refactor MapQuestGeocoder to promises

--- Sources/D2NetAPIs/mapquest/MapQuestGeocoder.swift
+++ Sources/D2NetAPIs/mapquest/MapQuestGeocoder.swift
@@ -9,49 +9,39 @@ public struct MapQuestGeocoder {
         //       use Codable structs rather than manually extracting
         //       values from JSON objects.
 
-        Promise { then in
+        Promise.catching { () throws -> HTTPRequest in
             let encodedLocation = location.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) ?? ""
             guard let mapQuestKey = storedNetApiKeys?.mapQuest else {
-                then(.failure(NetApiError.missingApiKey("No API key for MapQuest found")))
-                return
+                throw NetApiError.missingApiKey("No API key for MapQuest found")
             }
-
             guard let url = URL(string: "https://www.mapquestapi.com/geocoding/v1/address?key=\(mapQuestKey)&location=\(encodedLocation)") else {
-                then(.failure(NetApiError.urlStringError("Error while constructing url from location '\(encodedLocation)'")))
-                return
+                throw NetApiError.urlStringError("Error while constructing url from location '\(encodedLocation)'")
             }
 
-            // Source: https://stackoverflow.com/questions/39939143/parse-json-response-with-swift-3
-
-            HTTPRequest(url: url).runAsync().listen {
-                do {
-                    let data = try $0.get()
-                    let json = try JSONSerialization.jsonObject(with: data)
-                    let latLng = (json as? [String: Any])
-                        .flatMap { $0["results"] }
-                        .flatMap { $0 as? [Any] }
-                        .flatMap { $0.first }
-                        .flatMap { $0 as? [String: Any] }
-                        .flatMap { $0["locations"] }
-                        .flatMap { $0 as? [Any] }
-                        .flatMap { $0.first }
-                        .flatMap { $0 as? [String: Any] }
-                        .flatMap { $0["latLng"] }
-                        .flatMap { $0 as? [String: Double] }
-
-                    guard let location = latLng else {
-                        then(.failure(NetApiError.jsonParseError(String(describing: json), "Could not locate results -> locations -> latLng")))
-                        return
-                    }
-                    guard let lat = location["lat"], let lng = location["lng"] else {
-                        then(.failure(NetApiError.jsonParseError(String(describing: location), "No 'lat'/'lng' keys found")))
-                        return
-                    }
-                    then(.success(GeoCoordinates(latitude: lat, longitude: lng)))
-                } catch {
-                    then(.failure(NetApiError.jsonIOError(error)))
-                }
-            }
+            return HTTPRequest(url: url)
         }
+            .then { $0.runAsync() }
+            .mapCatching { try JSONSerialization.jsonObject(with: $0) }
+            .mapCatching { json in
+                let latLng = (json as? [String: Any])
+                    .flatMap { $0["results"] }
+                    .flatMap { $0 as? [Any] }
+                    .flatMap { $0.first }
+                    .flatMap { $0 as? [String: Any] }
+                    .flatMap { $0["locations"] }
+                    .flatMap { $0 as? [Any] }
+                    .flatMap { $0.first }
+                    .flatMap { $0 as? [String: Any] }
+                    .flatMap { $0["latLng"] }
+                    .flatMap { $0 as? [String: Double] }
+
+                guard let location = latLng else {
+                    throw NetApiError.jsonParseError(String(describing: json), "Could not locate results -> locations -> latLng")
+                }
+                guard let lat = location["lat"], let lng = location["lng"] else {
+                    throw NetApiError.jsonParseError(String(describing: location), "No 'lat'/'lng' keys found")
+                }
+                return GeoCoordinates(latitude: lat, longitude: lng)
+            }
 	}
 }
