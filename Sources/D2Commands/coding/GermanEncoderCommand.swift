196ddbb524b66ec8bf419a24b4a4285af3a1cf63
fwcd <fwcdmail@gmail.com>
2020-08-25 18:18:57 +0200

    Limit the germanness

--- Sources/D2Commands/coding/GermanEncoderCommand.swift
+++ Sources/D2Commands/coding/GermanEncoderCommand.swift
@@ -4,10 +4,17 @@ public class GermanEncoderCommand: StringCommand {
         shortDescription: "Encodes a string into 'german'",
         requiredPermissionLevel: .basic
     )
+    private let maxLength: Int
 
-    public init() {}
+    public init(maxLength: Int = 35) {
+        self.maxLength = maxLength
+    }
 
     public func invoke(withStringInput input: String, output: CommandOutput, context: CommandContext) {
+        guard input.count <= maxLength else {
+            output.append(errorText: "Please input a text of length <= \(maxLength)!")
+            return
+        }
         guard let data = input.data(using: .utf8) else {
             output.append(errorText: "Could not encode input as UTF-8")
             return
