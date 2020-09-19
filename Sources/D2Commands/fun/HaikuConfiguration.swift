b2797870ec967424ab4a1f22a745028ee068ef32
fwcd <fwcdmail@gmail.com>
2020-09-20 00:59:24 +0200

    Add Haiku configuration and command

---
+++ Sources/D2Commands/fun/HaikuConfiguration.swift
@@ -0,0 +1,11 @@
+import D2MessageIO
+
+// TODO: Temporary solution until a more unified channel/guild
+//       management system is in place, that also configures things
+//       like message previews, ...
+
+public struct HaikuConfiguration: Codable {
+    public var enabledChannelIds: Set<ChannelID> = []
+
+    public init() {}
+}
---
+++ Sources/D2Commands/fun/HaikusCommand.swift
@@ -0,0 +1,40 @@
+import D2Utils
+import D2MessageIO
+
+public class HaikusCommand: StringCommand {
+    public let info = CommandInfo(
+        category: .fun,
+        shortDescription: "Enables/disables Haikus in a channel",
+        requiredPermissionLevel: .admin
+    )
+    @AutoSerializing private var haikuConfiguration: HaikuConfiguration
+    private var subcommands: [String: (CommandOutput, ChannelID) -> Void] = [:]
+
+    public init(haikuConfiguration: AutoSerializing<HaikuConfiguration>) {
+        self._haikuConfiguration = haikuConfiguration
+
+        subcommands = [
+            "enable": { [unowned self] output, channelId in
+                self.haikuConfiguration.enabledChannelIds.insert(channelId)
+                output.append("Enabled Haikus on this channel")
+            },
+            "disable": { [unowned self] output, channelId in
+                self.haikuConfiguration.enabledChannelIds.remove(channelId)
+                output.append("Disabled Haikus on this channel")
+            }
+        ]
+    }
+
+    public func invoke(with input: String, output: CommandOutput, context: CommandContext) {
+        guard let subcommand = subcommands[input] else {
+            output.append(errorText: "Unrecognized subcommand, try one of these: `\(subcommands.keys.joined(separator: ", "))`")
+            return
+        }
+        guard let channelId = context.channel?.id else {
+            output.append(errorText: "Not in a channel")
+            return
+        }
+
+        subcommand(output, channelId)
+    }
+}
