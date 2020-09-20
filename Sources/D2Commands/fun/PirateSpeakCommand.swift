0d584e4248da1598cb6ee833076cc82fa5c28d4e
fwcd <fwcdmail@gmail.com>
2020-09-20 20:52:24 +0200

    Add interjections to pirate speak

--- Sources/D2Commands/fun/PirateSpeakCommand.swift
+++ Sources/D2Commands/fun/PirateSpeakCommand.swift
@@ -1,3 +1,7 @@
+import D2Utils
+
+fileprivate let punctuationPattern = try! Regex(from: "[\\.,!\\?]")
+
 public class PirateSpeakCommand: StringCommand {
     public let info = CommandInfo(
         category: .fun,
@@ -6,6 +10,7 @@ public class PirateSpeakCommand: StringCommand {
     )
     public let outputValueType: RichValueType = .text
     private let substitutions: [String: String]
+    private let interjections: [String]
 
     public init(substitutions: [String: String] = [
         "hello": "ahoy",
@@ -19,14 +24,28 @@ public class PirateSpeakCommand: StringCommand {
         "\\bthose\\b": "'ose",
         "\\bwhoa\\b": "avast",
         "\\bvery\\b": "mighty",
+        "\\bocean\\b": "briney deep",
+        "\\beggs\\b": "cackle fruit",
+        "\\bdaredevil\\b": "swashbuckler",
+        "\\btreasure\\b": "booty",
+        "\\breward\\b": "bounty",
+        "\\chest\\b": "coffer",
         "ng\\b": "n'",
         "ngs\\b": "n's",
         "st\\b": "s'",
         "you": "ye",
         "\\bto": "t'",
         "\\bis": "be"
+    ], interjections: [String] = [
+        "Arr!",
+        "C'mere, me beauty.",
+        "Savvy?",
+        "Shiver me timbers!",
+        "Sink me!",
+        "Yo ho ho."
     ]) {
         self.substitutions = substitutions
+        self.interjections = interjections
     }
 
     public func invoke(with input: String, output: CommandOutput, context: CommandContext) {
@@ -35,7 +54,8 @@ public class PirateSpeakCommand: StringCommand {
             return
         }
 
-        let result = substitutions.reduce(input) { $0.replacingOccurrences(of: $1.key, with: $1.value, options: [.caseInsensitive, .regularExpression]) }
-        output.append(result.replacingOccurrences(of: ".", with: ". Arr!"))
+        let preprocessed = punctuationPattern.replace(in: input) { "\($0[0])\(interjections.randomElement().filter { _ in Bool.random() }.map { " \($0)" } ?? "")" }
+        let result = substitutions.reduce(preprocessed) { $0.replacingOccurrences(of: $1.key, with: $1.value, options: [.caseInsensitive, .regularExpression]) }
+        output.append(result)
     }
 }
