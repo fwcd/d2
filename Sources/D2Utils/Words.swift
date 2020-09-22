4ca3b5c022360c542253511cda31950e754206c9
fwcd <fwcdmail@gmail.com>
2020-09-23 00:22:48 +0200

    Represent word lists and lists for efficiency

--- Sources/D2Utils/Words.swift
+++ Sources/D2Utils/Words.swift
@@ -3,7 +3,7 @@ public struct Words {
     public static let german = loadWords(name: "german")
     public static let nouns = loadWords(name: "nouns")
 
-    private static func loadWords(name: String) -> Set<String> {
-        Set((try? String(contentsOfFile: "Resources/words/\(name).txt", encoding: .utf8))?.split(separator: "\n").map(String.init) ?? [])
+    private static func loadWords(name: String) -> [String] {
+        (try? String(contentsOfFile: "Resources/words/\(name).txt", encoding: .utf8))?.split(separator: "\n").map(String.init) ?? []
     }
 }
