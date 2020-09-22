ccac1a05d15d329e54e34a75c47ec787d3e4ff7d
fwcd <fwcdmail@gmail.com>
2020-09-23 00:02:09 +0200

    Use CodenamesBoardView in CodenamesBoard

--- Sources/D2Commands/game/codenames/CodenamesBoard.swift
+++ Sources/D2Commands/game/codenames/CodenamesBoard.swift
@@ -1,8 +1,16 @@
 import D2Utils
+import Logging
+
+fileprivate let log = Logger(label: "D2Commands.CodenamesBoard")
 
 public struct CodenamesBoard: RichValueConvertible {
-    // TODO
-
     public var model = CodenamesBoardModel()
-    public var asRichValue: RichValue { .none }
+    public var asRichValue: RichValue {
+        do {
+            return try .image(CodenamesBoardView(model: model).image)
+        } catch {
+            log.warning("Could not create CodenamesBoardView: \(error)")
+            return .none
+        }
+    }
 }
