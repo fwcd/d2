0180e8ee83c8297f7730e721b0b7647061711d0d
fwcd <fwcdmail@gmail.com>
2020-09-26 05:23:16 +0200

    Trim RSS descriptions

--- Sources/D2Commands/feed/FeedListPresenter.swift
+++ Sources/D2Commands/feed/FeedListPresenter.swift
@@ -26,7 +26,11 @@ public struct FeedListPresenter: FeedPresenter {
                 guard let title = $0.title, let link = $0.link else { return nil }
                 return """
                     **[\(title)](\(link))**
-                    \(try $0.description.map { try converter.convert(htmlFragment: $0) }?.truncate(200, appending: "...") ?? "_no description_")
+                    \(try $0.description
+                        .map { try converter.convert(htmlFragment: $0) }?
+                        .trimmingCharacters(in: .whitespacesAndNewlines)
+                        .truncate(200, appending: "...")
+                        ?? "_no description_")
                     """
             }.joined(separator: "\n"),
             thumbnail: rss.image?.url.flatMap(URL.init(string:)).map(Embed.Thumbnail.init(url:))
