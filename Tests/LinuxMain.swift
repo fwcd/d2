e033de948d73e7285989c60394d5f3bd519901d2
fwcd <fwcdmail@gmail.com>
2020-09-30 16:32:29 +0200

    Remove invalid GraphicsTests import

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,13 +1,11 @@
 import XCTest
 
 import D2CommandTests
-import GraphicsTests
 import D2NetAPITests
 import D2ScriptTests
 
 var tests = [XCTestCaseEntry]()
 tests += D2CommandTests.allTests()
-tests += GraphicsTests.allTests()
 tests += D2NetAPITests.allTests()
 tests += D2ScriptTests.allTests()
 XCTMain(tests)
