2306e8b565fdee48123cec6953dabd7e7c684c2d
fwcd <fwcdmail@gmail.com>
2020-09-30 15:58:35 +0200

    Remove invalid UtilsTests import

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,6 +1,5 @@
 import XCTest
 
-import UtilsTests
 import D2CommandTests
 import GraphicsTests
 import D2NetAPITests
@@ -8,7 +7,6 @@ import D2ScriptTests
 
 var tests = [XCTestCaseEntry]()
 tests += D2CommandTests.allTests()
-tests += UtilsTests.allTests()
 tests += GraphicsTests.allTests()
 tests += D2NetAPITests.allTests()
 tests += D2ScriptTests.allTests()
