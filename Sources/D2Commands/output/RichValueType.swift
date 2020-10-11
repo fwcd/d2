e097582c56656b67f181bb73ebae9559eac15e96
fwcd <fwcdmail@gmail.com>
2020-10-11 22:16:00 +0200

    Implement CustomStringConvertible for RichValueType

--- Sources/D2Commands/output/RichValueType.swift
+++ Sources/D2Commands/output/RichValueType.swift
@@ -1,4 +1,4 @@
-public enum RichValueType {
+public enum RichValueType: CustomStringConvertible {
     case none
     case text
     case image
@@ -17,4 +17,27 @@ public enum RichValueType {
     case either([RichValueType])
     case unknown
     case any
+
+    public var description: String {
+        switch self {
+            case .none: return "none"
+            case .text: return "text"
+            case .image: return "image"
+            case .table: return "table"
+            case .gif: return "gif"
+            case .domNode: return "domNode"
+            case .urls: return "urls"
+            case .code: return "code"
+            case .embed: return "embed"
+            case .mentions: return "mentions"
+            case .ndArrays: return "ndArrays"
+            case .error: return "error"
+            case .files: return "files"
+            case .attachments: return "attachments"
+            case .compound(let values): return values.map(\.description).joined(separator: " & ")
+            case .either(let values): return values.map(\.description).joined(separator: " | ")
+            case .unknown: return "?"
+            case .any: return "any"
+        }
+    }
 }
