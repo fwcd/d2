d443501aff25d698e2b62a6afaa043bfb0ed9603
fwcd <fwcdmail@gmail.com>
2020-10-11 20:07:36 +0200

    Abstract CropImageCommand to CropImageMapping

---
+++ Sources/D2Commands/imaging/mapping/CropImageMapping.swift
@@ -0,0 +1,54 @@
+import Utils
+import D2MessageIO
+import Graphics
+
+fileprivate let argsPattern = try! Regex(from: "(\\d+)\\s+(\\d+)\\s+(\\d+)\\s+(\\d+)")
+
+public struct CropImageMapping: ImageMapping {
+    private let topLeftX: Int
+    private let topLeftY: Int
+    private let bottomRightX: Int
+    private let bottomRightY: Int
+
+    private enum CropError: Error {
+        case invalidArgs(String)
+        case invalidDimensions(String)
+        case outOfBounds(String)
+    }
+
+    public init(args: String?) throws {
+        guard
+            let rawBounds = args,
+            let parsedBounds = argsPattern.firstGroups(in: rawBounds)?[1...].compactMap(Int.init) else {
+            throw CropError.invalidArgs("Syntax: [top left x] [top left y] [bottom right x] [bottom right y]")
+        }
+
+        (topLeftX, topLeftY, bottomRightX, bottomRightY) = (parsedBounds[0], parsedBounds[1], parsedBounds[2], parsedBounds[3])
+    }
+
+    public func apply(to image: Image) throws -> Image {
+        let inputWidth = image.width
+        let inputHeight = image.height
+
+        guard (0..<inputHeight).contains(topLeftY), (0..<inputWidth).contains(topLeftX), (0..<inputHeight).contains(bottomRightY), (0..<inputWidth).contains(bottomRightX) else {
+            throw CropError.outOfBounds("Make sure that all cropped bounds are in the image's bounds!")
+        }
+
+        let width = bottomRightX - topLeftX
+        let height = bottomRightY - topLeftY
+
+        guard width > 0, height > 0 else {
+            throw CropError.invalidDimensions("Make sure that the width/height of the cropped dimensions are positive!")
+        }
+
+        var cropped = try Image(width: width, height: height)
+
+        for y in 0..<height {
+            for x in 0..<width {
+                cropped[y, x] = image[y + topLeftY, x + topLeftX]
+            }
+        }
+
+        return cropped
+    }
+}
