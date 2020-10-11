dc6981a214561037f682e0f8470429085116e217
fwcd <fwcdmail@gmail.com>
2020-10-11 21:06:10 +0200

    Use pos and add direction to SpinTransform

--- Sources/D2Commands/imaging/animation/transform/SpinTransform.swift
+++ Sources/D2Commands/imaging/animation/transform/SpinTransform.swift
@@ -3,17 +3,31 @@ import Utils
 
 public struct SpinTransform: ImageTransform {
     public enum Key: String, StringEnum {
-        case speed
+        case direction
+    }
+
+    private enum Direction: String {
+        case clockwise
+        case counterclockwise
+
+        var sign: Int {
+            switch self {
+                case .clockwise: return -1
+                case .counterclockwise: return 1
+            }
+        }
     }
 
     private let pos: Vec2<Int>?
+    private let direction: Direction
 
     public init(at pos: Vec2<Int>?, kvArgs: [Key: String]) {
         self.pos = pos
+        direction = kvArgs[.direction].flatMap(Direction.init(rawValue:)) ?? .counterclockwise
     }
 
     public func sourcePos(from destPos: Vec2<Int>, imageSize: Vec2<Int>, percent: Double) -> Vec2<Int> {
-        let centerPos = imageSize / 2
-        return (Mat2<Double>.rotation(by: percent * 2.0 * .pi) * (destPos - centerPos).asDouble).floored + centerPos
+        let centerPos = pos ?? (imageSize / 2)
+        return (Mat2<Double>.rotation(by: Double(direction.sign) * percent * 2.0 * .pi) * (destPos - centerPos).asDouble).floored + centerPos
     }
 }
