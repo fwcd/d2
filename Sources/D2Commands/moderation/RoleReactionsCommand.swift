04a3fec06972ee733338ada6b1ee3a2ab7cdccc3
fwcd <fwcdmail@gmail.com>
2020-11-09 18:57:11 +0100

    Scaffold out RoleReactionsCommand

--- Sources/D2Commands/moderation/RoleReactionsCommand.swift
+++ Sources/D2Commands/moderation/RoleReactionsCommand.swift
@@ -1,18 +1,40 @@
+import D2MessageIO
 import Utils
 
+fileprivate let argsPattern = try! Regex(from: "(\\w+)\\s+(\\d+)\\s*(.*)")
+
 public class RoleReactionsCommand: StringCommand {
-    public let info = CommandInfo(
+    public private(set) var info = CommandInfo(
         category: .moderation,
         shortDescription: "Adds reactions to a message that automatically assign roles",
         requiredPermissionLevel: .vip
     )
     @AutoSerializing private var configuration: RoleReactionsConfiguration
+    private var subcommands: [String: (CommandOutput, MessageID, String) -> Void] = [:]
 
     public init(configuration: AutoSerializing<RoleReactionsConfiguration>) {
         self._configuration = configuration
+        subcommands = [
+            "attach": { [unowned self] output, messageId, args in
+                // TODO
+            },
+            "detach": { [unowned self] output, messageId, _ in
+                // TODO
+            }
+        ]
+        info.helpText = """
+            Syntax: `[subcommand] [message id] [args...]`
+
+            For example:
+            `attach 123456789012345678 😁=Role a, 👍=Role b`
+            `detach 123456789012345678`
+            """
     }
 
     public func invoke(with input: String, output: CommandOutput, context: CommandContext) {
-        // TODO
+        guard let parsedArgs = argsPattern.firstGroups(in: input) else {
+            output.append(errorText: info.helpText!)
+            return
+        }
     }
 }
