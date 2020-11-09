a455089f077b51bda95c242fe03813da49773f5b
fwcd <fwcdmail@gmail.com>
2020-11-09 23:11:40 +0100

    WIP: Update PokedexCommand

--- Sources/D2Commands/fun/PokedexCommand.swift
+++ Sources/D2Commands/fun/PokedexCommand.swift
@@ -19,22 +19,26 @@ public class PokedexCommand: StringCommand {
             return
         }
         PokedexQuery().perform().listen {
-            switch $0 {
-                case .success(let pokedex):
-                    guard let pokemon = pokedex.min(by: ascendingComparator(comparing: { $0.name?.levenshteinDistance(to: input, caseSensitive: false) ?? Int.max })) else {
-                        output.append(errorText: "No such Pokémon could be found.")
-                        return
-                    }
-                    output.append(Embed(
-                        title: pokemon.name ?? "Unnamed Pokémon",
-                        thumbnail: Embed.Thumbnail(url: pokemon.gifUrl),
-                        fields: [
-                            Embed.Field(name: "Types", value: pokemon.types?.joined(separator: ", ").nilIfEmpty ?? "_none_"),
-                            Embed.Field(name: "Forms", value: pokemon.forms?.compactMap { $0.name }.joined(separator: ", ").nilIfEmpty ?? "_none_")
-                        ]
-                    ))
-                case .failure(let error):
-                    output.append(error, errorText: "Could not fetch Pokédex.")
+            do {
+                let pokedex = try $0.get()
+                guard let pokemon = pokedex.min(by: ascendingComparator(comparing: { $0.name.levenshteinDistance(to: input, caseSensitive: false) ?? Int.max })) else {
+                    output.append(errorText: "No such Pokémon could be found.")
+                    return
+                }
+                output.append(Embed(
+                    title: pokemon.name,
+                    thumbnail: (pokemon.sprites?.url).map(Embed.Thumbnail.init(url:)),
+                    fields: [
+                        ("Base Experience", pokemon.baseExperience.map { String($0) }),
+                        ("Weight", pokemon.weight.map { String($0) }),
+                        ("Order", pokemon.order.map { String($0) }),
+                        ("Abilities", pokemon.abilities.flatMap { $0.map(\.abilitiy.name).joined(separator: ", ").nilIfEmpty })
+                        ("Forms", pokemon.forms.flatMap { $0.map(\.name).joined(separator: ", ").nilIfEmpty }),
+                        ("Stats", pokemon.stats.flatMap { $0.map { "\($0.stat.name): Base \($0.stat.baseStat), effort \($0.stat.effort)" }.joined(separator: ", ").nilIfEmpty })
+                    ].compactMap { (k, v) in v.map { Embed.Field(name: k, value: $0) } }
+                ))
+            } catch {
+                output.append(error, errorText: "Could not fetch Pokédex.")
             }
         }
     }
