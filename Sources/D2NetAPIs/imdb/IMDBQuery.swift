b46f531d39a0cbab76189cdf6953009ac546ee6a
fwcd <fwcdmail@gmail.com>
2020-11-14 23:20:45 +0100

    Add IMDB API

---
+++ Sources/D2NetAPIs/imdb/IMDBQuery.swift
@@ -0,0 +1,22 @@
+import Foundation
+import Utils
+
+public struct IMDBQuery {
+    private let query: String
+    private var formattedQuery: String {
+        query
+            .lowercased()
+            .replacingOccurrences(of: "\t", with: " ")
+            .replacingOccurrences(of: "\n", with: " ")
+            .replacingOccurrences(of: " ", with: "_")
+    }
+
+    public init(query: String) {
+        self.query = query
+    }
+
+    public func perform() -> Promise<IMDBResults, Error> {
+        Promise.catching { try HTTPRequest(host: "v2.sg.media-imdb.com", path: "/suggestion/h/\(formattedQuery).json") }
+            .then { $0.fetchJSONAsync(as: IMDBResults.self) }
+    }
+}
---
+++ Sources/D2NetAPIs/imdb/IMDBResults.swift
@@ -0,0 +1,57 @@
+public struct IMDBResults: Codable {
+    public enum CodingKeys: String, CodingKey {
+        case data = "d"
+        case query = "q"
+        case v
+    }
+
+    public let data: [Entry]
+    public let query: String?
+    public let v: Int?
+
+    public struct Entry: Codable {
+        public enum CodingKeys: String, CodingKey {
+            case info = "i"
+            case id
+            case title = "l"
+            case type = "q"
+            case rank
+            case starring = "s"
+            case videos = "v"
+            case vt
+            case year = "y"
+            case years = "yr"
+        }
+
+        public let info: Info
+        public let id: String
+        public let title: String
+        public let type: String
+        public let rank: Int?
+        public let starring: String?
+        public let videos: [Video]?
+        public let vt: Int?
+        public let year: Int?
+        public let years: String?
+
+        public struct Info: Codable {
+            public let width: Int?
+            public let height: Int?
+            public let imageUrl: String?
+        }
+
+        public struct Video: Codable {
+            public enum CodingKeys: String, CodingKey {
+                case info = "i"
+                case id
+                case name = "l"
+                case length = "s"
+            }
+
+            public let info: Info?
+            public let id: String
+            public let name: String
+            public let length: String?
+        }
+    }
+}
