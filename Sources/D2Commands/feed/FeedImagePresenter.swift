3d1f458a80abd32354d185b86e59b22c5a6d3b85
fwcd <fwcdmail@gmail.com>
2020-11-15 00:31:13 +0100

    Extract enclosure in FeedImagePresenter

--- Sources/D2Commands/feed/FeedImagePresenter.swift
+++ Sources/D2Commands/feed/FeedImagePresenter.swift
@@ -20,13 +20,30 @@ public struct FeedImagePresenter: FeedPresenter {
 
     private func present(rss: RSSFeed) throws -> Embed? {
         // TODO: Produce proper error messages instead of just returning nil
-        guard let item = rss.items?.first, let description = item.description else { return nil }
-        let document = try SwiftSoup.parseBodyFragment(description)
-        guard let imgSrc = try document.getElementsByTag("img").array().first?.attr("src") else { return nil }
+        guard let item = rss.items?.first else { return nil }
         return Embed(
             title: item.title,
             url: item.link.flatMap(URL.init(string:)),
-            image: URL(string: imgSrc).map(Embed.Image.init(url:))
+            image: try extractImageUrl(item: item).map(Embed.Image.init(url:))
         )
+    }
+
+    private func extractImageUrl(item: RSSFeedItem) throws -> URL? {
+        try extractImageUrlFromEnclosure(item: item) ?? extractImageUrlFromHtml(item: item)
+    }
+
+    private func extractImageUrlFromEnclosure(item: RSSFeedItem) -> URL? {
+        item.enclosure
+            .flatMap(\.attributes)
+            .filter { $0.type?.starts(with: "image/") ?? false }
+            .flatMap(\.url)
+            .flatMap(URL.init(string:))
+    }
+
+    private func extractImageUrlFromHtml(item: RSSFeedItem) throws -> URL? {
+        guard let description = item.description else { return nil }
+        let document = try SwiftSoup.parseBodyFragment(description)
+        guard let imgSrc = try document.getElementsByTag("img").array().first?.attr("src") else { return nil }
+        return URL(string: imgSrc)
     }
 }
