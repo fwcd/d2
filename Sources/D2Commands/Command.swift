8ebd202fa5602110847b88f21a04f16350877da7
fwcd <fwcdmail@gmail.com>
2020-11-15 03:22:12 +0100

    Add more doc comments to Command

--- Sources/D2Commands/Command.swift
+++ Sources/D2Commands/Command.swift
@@ -1,20 +1,34 @@
 import D2MessageIO
 import D2Permissions
 
-/** Encapsulates functionality that can conveniently be invoked using inputs and arguments. */
+/// Encapsulates functionality that can conveniently be invoked using rich-valued inputs.
 public protocol Command: AnyObject {
+    /// The input that this command expects to be invoked with. Note that this is purely
+    /// an annotation for documentary purposes and not enforced at runtime.
     var inputValueType: RichValueType { get }
+    /// The output that this command is expected to emit. Note that this is purely
+    /// an annotation for documentary purposes and not enforced at runtime.
     var outputValueType: RichValueType { get }
+    /// Metadata about the command.
     var info: CommandInfo { get }
 
+    /// Invokes the command.
+    ///
+    /// Command invocations are inherently effectful and often asynchronous. This means
+    /// that the passed output may be invoked on any thread, zero or (arbitrary) more times.
     func invoke(with input: RichValue, output: CommandOutput, context: CommandContext)
 
+    /// Notifies the command that a message sent via CommandOutput has been
+    /// successfully transmitted.
     func onSuccessfullySent(context: CommandContext)
 
+    /// Notifies the command that a message on a subscribed channel has arrived.
     func onSubscriptionMessage(with content: String, output: CommandOutput, context: CommandContext)
 
+    /// Notifies the command that a reaction on a subscribed channel has arrived.
     func onSubscriptionReaction(emoji: Emoji, by user: User, output: CommandOutput, context: CommandContext)
 
+    /// Notifies the command that the bot's presence has updated.
     func onReceivedUpdated(presence: Presence)
 }
 
