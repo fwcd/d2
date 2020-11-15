b5476361340a02e76471ad4386e33e429593ae16
fwcd <fwcdmail@gmail.com>
2020-11-15 19:42:58 +0100

    Add GuildChannelsCommand

---
+++ Sources/D2Commands/moderation/GuildChannelsCommand.swift
@@ -0,0 +1,50 @@
+import D2MessageIO
+import Utils
+
+public class GuildChannelsCommand: StringCommand {
+    public let info = CommandInfo(
+        category: .moderation,
+        shortDescription: "Fetches the channel list of a guild",
+        requiredPermissionLevel: .admin
+    )
+
+    public init() {}
+
+    public func invoke(with input: String, output: CommandOutput, context: CommandContext) {
+        guard let client = context.client else {
+            output.append(errorText: "No client available!")
+            return
+        }
+
+        let guildId: GuildID
+
+        if input.isEmpty {
+            guard let guild = context.guild else {
+                output.append(errorText: "Not on a guild!")
+                return
+            }
+            guildId = guild.id
+        } else {
+            guildId = GuildID(input, clientName: client.name)
+        }
+
+        guard let guild = client.guild(for: guildId) else {
+            output.append(errorText: "No guild with this id found!")
+            return
+        }
+
+        output.append(Embed(
+            title: ":accordion: Channels on `\(guild.name)`",
+            description: guild.channels.values
+                .sorted(by: ascendingComparator(comparing: \.position))
+                .map {
+                    switch $0.type {
+                        case .text: return ":hash: \($0.name)"
+                        case .voice: return ":speaker: \($0.name)"
+                        case .category: return ":arrow_down_small: **\($0.name)**"
+                    }
+                }
+                .joined(separator: "\n")
+        ))
+    }
+}
--- Sources/D2Handlers/D2Delegate.swift
+++ Sources/D2Handlers/D2Delegate.swift
@@ -327,6 +327,7 @@ public class D2Delegate: MessageDelegate {
         registry["parsedom", aka: ["parsehtml"]] = ParseDOMCommand()
         registry["cssselect", aka: ["cssselector", "selector", "select"]] = CSSSelectorCommand()
         registry["guildinfo", aka: ["stats", "server", "serverstats", "serverinfo", "guild", "guildstats"]] = GuildInfoCommand(messageDB: messageDB)
+        registry["guildchannels", aka: ["channels", "serverchannels"]] = GuildChannelsCommand()
         registry["guilds"] = GuildsCommand()
         registry["whatsup"] = WhatsUpCommand()
         registry["songcharts"] = SongChartsCommand()
