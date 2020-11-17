d1a7585a1bb077321c50af7f7ccf1d8961839339
fwcd <fwcdmail@gmail.com>
2020-11-17 10:14:47 +0100

    Add universities search api

---
+++ Sources/D2NetAPIs/universities/UniversitiesQuery.swift
@@ -0,0 +1,14 @@
+import Utils
+
+public struct UniversitiesQuery {
+    public let name: String
+
+    public init(name: String) {
+        self.name = name
+    }
+
+    public func perform() -> Promise<[University], Error> {
+        Promise.catching { try HTTPRequest(scheme: "http", host: "universities.hipolabs.com", path: "/search", query: ["name": name]) }
+            .then { $0.fetchJSONAsync(as: [University].self) }
+    }
+}
---
+++ Sources/D2NetAPIs/universities/University.swift
@@ -0,0 +1,17 @@
+public struct University: Codable {
+    public enum CodingKeys: String, CodingKey {
+        case domains
+        case webPages = "web_pages"
+        case alphaTwoCode = "alpha_two_code"
+        case country
+        case name
+        case stateProvince = "state-province"
+    }
+
+    public let domains: [String]?
+    public let webPages: [String]?
+    public let alphaTwoCode: String?
+    public let country: String?
+    public let name: String?
+    public let stateProvince: String?
+}
