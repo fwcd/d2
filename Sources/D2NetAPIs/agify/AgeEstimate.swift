3f7ae8b50d14674294f622c27b05176af37bda9d
fwcd <fwcdmail@gmail.com>
2020-11-17 10:25:09 +0100

    Add agify.io API

---
+++ Sources/D2NetAPIs/agify/AgeEstimate.swift
@@ -0,0 +1,5 @@
+public struct AgeEstimate: Codable {
+    public let name: String
+    public let age: Int
+    public let count: Int
+}
---
+++ Sources/D2NetAPIs/agify/AgifyQuery.swift
@@ -0,0 +1,14 @@
+import Utils
+
+public struct AgifyQuery {
+    public let name: String
+
+    public init(name: String) {
+        self.name = name
+    }
+
+    public func perform() -> Promise<AgeEstimate, Error> {
+        Promise.catching { try HTTPRequest(host: "api.agify.io", path: "/", query: ["name": name]) }
+            .then { $0.fetchJSONAsync(as: AgeEstimate.self) }
+    }
+}
