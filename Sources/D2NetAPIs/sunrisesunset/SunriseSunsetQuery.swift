fd513af5bba17b8702ae2ea504ffca344f662047
fwcd <fwcdmail@gmail.com>
2020-11-17 13:40:53 +0100

    Add sunrise/sunset API

---
+++ Sources/D2NetAPIs/sunrisesunset/SunriseSunsetQuery.swift
@@ -0,0 +1,14 @@
+import Utils
+
+public struct SunriseSunsetQuery {
+    public let coords: GeoCoordinates
+
+    public init(coords: GeoCoordinates) {
+        self.coords = coords
+    }
+
+    public func perform() -> Promise<SunriseSunsetResults, Error> {
+        Promise.catching { try HTTPRequest(host: "api.sunrise-sunset.org", path: "/json", query: ["lat": String(coords.latitude), "lon": String(coords.longitude)]) }
+            .then { $0.fetchJSONAsync(as: SunriseSunsetResults.self) }
+    }
+}
---
+++ Sources/D2NetAPIs/sunrisesunset/SunriseSunsetResults.swift
@@ -0,0 +1,32 @@
+public struct SunriseSunsetResults: Codable {
+    public let status: String
+    public let results: Results
+
+    public struct Results: Codable {
+        public enum CodingKeys: String, CodingKey {
+            case sunrise
+            case sunset
+            case solarNoon = "solar_noon"
+            case dayLength = "day_length"
+            case civilTwilightBegin = "civil_twilight_begin"
+            case civilTwilightEnd = "civil_twilight_end"
+            case nauticalTwilightBegin = "nautical_twilight_begin"
+            case nauticalTwilightEnd = "nautical_twilight_end"
+            case astronomialTwilightBegin = "astronomical_twilight_begin"
+            case astronomialTwilightEnd = "astronomical_twilight_end"
+        }
+
+        // TODO: Parse to Foundation's Date (the format is "hh:mm:ss AM/PM")
+
+        public let sunrise: String
+        public let sunset: String
+        public let solarNoon: String
+        public let dayLength: String
+        public let civilTwilightBegin: String
+        public let civilTwilightEnd: String
+        public let nauticalTwilightBegin: String
+        public let nauticalTwilightEnd: String
+        public let astronomialTwilightBegin: String
+        public let astronomialTwilightEnd: String
+    }
+}
