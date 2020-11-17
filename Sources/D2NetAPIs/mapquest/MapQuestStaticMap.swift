0f624aa2834f47085e981fb8e290ab3d15d9bf6f
fwcd <fwcdmail@gmail.com>
2020-11-17 12:24:44 +0100

    Add center parameter in MapQuestStaticMap

--- Sources/D2NetAPIs/mapquest/MapQuestStaticMap.swift
+++ Sources/D2NetAPIs/mapquest/MapQuestStaticMap.swift
@@ -5,7 +5,7 @@ public struct MapQuestStaticMap {
     public let url: URL
 
     public init(
-        center: GeoCoordinates,
+        center: GeoCoordinates? = nil,
         locations: [GeoCoordinates] = [],
         width: Int = 300,
         height: Int = 300,
@@ -18,12 +18,19 @@ public struct MapQuestStaticMap {
         components.scheme = "https"
         components.host = "www.mapquestapi.com"
         components.path = "/staticmap/v5/map"
-        components.queryItems = [
+
+        var query = [
             URLQueryItem(name: "key", value: mapQuestKey),
             URLQueryItem(name: "size", value: "\(width),\(height)"),
             URLQueryItem(name: "zoom", value: String(zoom)),
             URLQueryItem(name: "locations", value: locations.map { "\($0.latitude),\($0.longitude)" }.joined(separator: "|"))
         ]
+
+        if let center = center {
+            query.append(URLQueryItem(name: "center", value: "\(center.latitude),\(center.longitude)"))
+        }
+
+        components.queryItems = query
 
         guard let url = components.url else { throw NetApiError.urlError(components) }
         self.url = url
