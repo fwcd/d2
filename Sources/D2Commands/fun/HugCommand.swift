3ac55bcdae7a59cbe0edb5a7ae8d3cf800626410
fwcd <fwcdmail@gmail.com>
2020-11-23 17:43:51 +0100

    Put hugs into the user's inventory

--- Sources/D2Commands/fun/HugCommand.swift
+++ Sources/D2Commands/fun/HugCommand.swift
@@ -9,10 +9,17 @@ public class HugCommand: Command {
     )
     public let inputValueType: RichValueType = .mentions
     public let outputValueType: RichValueType = .image
+    private let inventoryManager: InventoryManager
 
-    public init() {}
+    public init(inventoryManager: InventoryManager) {
+        self.inventoryManager = inventoryManager
+    }
 
     public func invoke(with input: RichValue, output: CommandOutput, context: CommandContext) {
+        guard let author = context.author else {
+            output.append(errorText: "No author found")
+            return
+        }
         guard let user = input.asMentions?.first else {
             output.append(errorText: "Please mention someone!")
             return
@@ -33,6 +40,9 @@ public class HugCommand: Command {
                     let template = try colorToAlpha(in: rawTemplate, color: green, squaredThreshold: 0.4)
                     let composition = try composeImage(from: template, with: image, between: topLeft, and: bottomRight)
                     try output.append(composition)
+
+                    // Place the hug in the recipient's inventory
+                    self.inventoryManager[user].append(item: .init(id: "hug-\(author.username)", name: "Hug by \(author.username)"), to: "Hugs")
                 } catch {
                     output.append(error, errorText: "Could not create hug image. :(")
                 }
