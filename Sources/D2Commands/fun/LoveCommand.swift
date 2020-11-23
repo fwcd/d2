3836d81ebad9049c40c97bb6ced67f5772fadc49
fwcd <fwcdmail@gmail.com>
2020-11-23 15:49:20 +0100

    Use custom hashing function for stable results

--- Sources/D2Commands/fun/LoveCommand.swift
+++ Sources/D2Commands/fun/LoveCommand.swift
@@ -21,17 +21,19 @@ public class LoveCommand: Command {
             output.append(errorText: info.helpText!)
             return
         }
+        guard let authorId = Int(author.id.value), let otherId = Int(other.id.value) else {
+            output.append(errorText: "Numerical IDs needed!")
+            return
+        }
         guard author.id != other.id else {
             output.append(errorText: "Please mention someone other than yourself!")
             return
         }
 
-        var hasher = Hasher()
-        hasher.combine(Set([author.id, other.id]))
-
         let basePrecision = 1000
+        let baseHash = (authorId % basePrecision) ^ (otherId % basePrecision)
         let components: [(weight: Double, value: Double)] = [
-            (weight: 8, value: Double(abs(hasher.finalize() % basePrecision)) / Double(basePrecision)),
+            (weight: 8, value: Double(abs(baseHash % basePrecision)) / Double(basePrecision)),
             (weight: 1, value: 1 - min(1, Double(author.username.levenshteinDistance(to: other.username)) / 40)),
             (weight: 2, value: author.bot == other.bot ? 0.8 : 0.1)
         ]
