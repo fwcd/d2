b6f5ce2fcf6c0850f4efae7326494589c9f819ab
fwcd <fwcdmail@gmail.com>
2020-12-01 14:34:55 +0100

    Cache the AoC leaderboard for 15 minutes

--- Sources/D2NetAPIs/adventofcode/AdventOfCodeLeaderboardQuery.swift
+++ Sources/D2NetAPIs/adventofcode/AdventOfCodeLeaderboardQuery.swift
@@ -1,6 +1,14 @@
+import Foundation
+import Logging
 import Utils
 
+fileprivate let log = Logger(label: "D2NetAPIs.AdventOfCodeLeaderboardQuery")
+
 public struct AdventOfCodeLeaderboardQuery {
+    private static var cachedBoard: AdventOfCodeLeaderboard? = nil
+    private static var cachedTimestamp: Date? = nil
+    private static let cacheRefreshDelay: TimeInterval = 15 * 60
+
     public let event: String
     public let ownerId: Int
 
@@ -10,16 +18,30 @@ public struct AdventOfCodeLeaderboardQuery {
     }
 
     public func perform() -> Promise<AdventOfCodeLeaderboard, Error> {
-        guard let key = storedNetApiKeys?.adventOfCode?[event] else {
-            return Promise(.failure(NetApiError.missingApiKey("for Advent of Code \(event)")))
-        }
+        if let ts = Self.cachedTimestamp, -ts.timeIntervalSinceNow <= Self.cacheRefreshDelay, let board = Self.cachedBoard {
+            log.info("Using cached AoC leaderboard")
 
-        return Promise
-            .catching { try HTTPRequest(
-                host: "adventofcode.com",
-                path: "/\(event)/leaderboard/private/view/\(ownerId).json",
-                headers: ["Cookie": "session=\(key)"]
-            ) }
-            .then { $0.fetchJSONAsync(as: AdventOfCodeLeaderboard.self) }
+            return Promise(.success(board))
+        } else {
+            log.info("Refreshing AoC leaderboard cache...")
+
+            guard let key = storedNetApiKeys?.adventOfCode?[event] else {
+                return Promise(.failure(NetApiError.missingApiKey("for Advent of Code \(event)")))
+            }
+
+            return Promise
+                .catching { try HTTPRequest(
+                    host: "adventofcode.com",
+                    path: "/\(event)/leaderboard/private/view/\(ownerId).json",
+                    headers: ["Cookie": "session=\(key)"]
+                ) }
+                .then { $0.fetchJSONAsync(as: AdventOfCodeLeaderboard.self) }
+                .peekListen {
+                    if case let .success(board) = $0 {
+                        Self.cachedBoard = board
+                        Self.cachedTimestamp = Date()
+                    }
+                }
+        }
     }
 }
