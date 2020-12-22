c194931c75539f74d6e9034e9415280472d9a777
fwcd <fwcdmail@gmail.com>
2020-12-22 21:29:13 +0100

    Request all intents from Discord

--- Sources/D2DiscordIO/DiscordPlatform.swift
+++ Sources/D2DiscordIO/DiscordPlatform.swift
@@ -17,7 +17,10 @@ public struct DiscordPlatform: MessagePlatform {
 
         let queue = DispatchQueue(label: "Discord handle queue")
         self.delegate = MessageIOClientDelegate(inner: delegate, sinkClient: combinedClient)
-        discordClient = DiscordClient(token: DiscordToken(stringLiteral: "Bot \(token)"), delegate: self.delegate, configuration: [.handleQueue(queue)])
+        discordClient = DiscordClient(token: DiscordToken(stringLiteral: "Bot \(token)"), delegate: self.delegate, configuration: [
+            .handleQueue(queue),
+            .intents(.allIntents)
+        ])
 
         combinedClient.register(client: DiscordMessageClient(client: discordClient))
     }
