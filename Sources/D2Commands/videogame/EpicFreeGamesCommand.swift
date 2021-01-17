6f8fde778824ee54ec69cca5f5983b7c996f38c5
fwcd <fwcdmail@gmail.com>
2021-01-17 18:42:18 +0100

    Update EpicFreeGamesCommand

--- Sources/D2Commands/videogame/EpicFreeGamesCommand.swift
+++ Sources/D2Commands/videogame/EpicFreeGamesCommand.swift
@@ -13,14 +13,24 @@ public class EpicFreeGamesCommand: StringCommand {
     public func invoke(with input: String, output: CommandOutput, context: CommandContext) {
         EpicFreeGamesQuery().perform().listen {
             do {
-                let games = try $0.get()
+                let games = try $0.get().data.catalog.searchStore.elements
                 output.append(Embed(
                     title: "Free Games in the Epic Store",
-                    fields: games.prefix(5).map { Embed.Field(name: $0.title, value: $0.subtitle.nilIfEmpty ?? "_no info_") }
+                    fields: games
+                        .prefix(5)
+                        .map { Embed.Field(name: $0.title, value: $0.price.flatMap(self.format(price:)) ?? "_no info_") }
                 ))
             } catch {
                 output.append(error, errorText: "Could not query games")
             }
         }
+    }
+
+    private func format(price: EpicFreeGames.ResponseData.Catalog.SearchStore.Element.Price) -> String? {
+        let fmtPrice = price.totalPrice?.fmtPrice
+        return [
+            (fmtPrice?.originalPrice).map { "~~\($0)~~" },
+            fmtPrice?.discountPrice
+        ].compactMap { $0 }.joined(separator: " ").nilIfEmpty
     }
 }
