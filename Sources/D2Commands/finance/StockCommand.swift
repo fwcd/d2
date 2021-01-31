cf2f725fa83615d1e1bb182e8aee804ddc764241
fwcd <fwcdmail@gmail.com>
2021-01-31 22:00:47 +0100

    Plot stock

--- Sources/D2Commands/finance/StockCommand.swift
+++ Sources/D2Commands/finance/StockCommand.swift
@@ -1,6 +1,10 @@
 import Foundation
 import D2NetAPIs
 import D2MessageIO
+import Utils
+import Graphics
+import SwiftPlot
+import AGGRenderer
 
 public class StockCommand: StringCommand {
     public let info = CommandInfo(
@@ -17,15 +21,39 @@ public class StockCommand: StringCommand {
             return
         }
 
+        let name = input.uppercased()
         let end = Date()
         let start = Calendar.current.date(byAdding: .day, value: -30, to: end)!
-        YahooFinanceQuery(stock: input.uppercased(), from: start, to: end).perform().listen {
+        YahooFinanceQuery(stock: name, from: start, to: end).perform().listen {
             do {
                 let values = try $0.get()
-                output.append(.table(Array(values.map { [$0.date, "\($0.high)", "\($0.low)", "\($0.close)"] }.prefix(10))))
+                let image = try self.presentStock(name: name, values: values)
+                try output.append(image)
             } catch {
-                output.append(error, errorText: "Could not query stock")
+                output.append(error, errorText: "Could not query/present stock")
             }
         }
+    }
+
+    private func presentStock(name: String, values: [YahooFinanceStockDataPoint]) throws -> Image {
+        var graph = LineGraph<Double, Double>(enablePrimaryAxisGrid: true)
+        graph.addSeries(
+            (0..<values.count).map(Double.init),
+            values.map(\.close),
+            label: name
+        )
+        graph.plotLineThickness = 3
+        return try render(plot: graph)
+    }
+
+    private func render<P>(plot: P) throws -> Image where P: Plot {
+        let renderer = AGGRenderer()
+        plot.drawGraph(renderer: renderer)
+
+        guard let pngData = Data(base64Encoded: renderer.base64Png()) else {
+            throw AdventOfCodeError.noPlotImageData
+        }
+
+        return try Image(fromPng: pngData)
     }
 }
