5d7fc75de9e8983e144e1e9278d14e982c15161c
fwcd <fwcdmail@gmail.com>
2021-02-15 17:22:59 +0100

    Implement GiphyCommand

--- Sources/D2Commands/fun/GiphyCommand.swift
+++ Sources/D2Commands/fun/GiphyCommand.swift
@@ -1,7 +1,8 @@
 import D2NetAPIs
 import D2MessageIO
+import GIF
 
-public struct GiphyCommand: StringCommand {
+public class GiphyCommand: StringCommand {
     public let info = CommandInfo(
         category: .fun,
         shortDescription: "Fetches a GIF from Giphy",
@@ -17,6 +18,22 @@ public struct GiphyCommand: StringCommand {
         }
 
         GiphySearchQuery(term: input)
+            .perform()
+            .thenCatching {
+                guard let gif = $0.data.first else { throw GiphyError.noGIFsFound }
+                return gif.download()
+            }
+            .mapCatching { try GIF(data: $0) }
+            .listen {
+                do {
+                    output.append(.gif(try $0.get()))
+                } catch {
+                    output.append(error, errorText: "Could not query GIF")
+                }
+            }
+    }
 
+    private enum GiphyError: Error {
+        case noGIFsFound
     }
 }
