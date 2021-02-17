d299ea61b3ffc4b789e8551467d6b1c54127c862
fwcd <fwcdmail@gmail.com>
2021-02-17 16:18:47 +0100

    Add truth or dare API

---
+++ Sources/D2NetAPIs/truthdare/TruthOrDare.swift
@@ -0,0 +1,6 @@
+public struct TruthOrDare: Codable {
+    public let id: Int
+    public let category: String
+    public let type: String
+    public let text: String
+}
---
+++ Sources/D2NetAPIs/truthdare/TruthOrDareQuery.swift
@@ -0,0 +1,29 @@
+import Utils
+
+public struct TruthOrDareQuery {
+    public let category: Category
+    public let type: TDType
+
+    public init(category: Category, type: TDType) {
+        self.category = category
+        self.type = type
+    }
+
+    public enum Category: String {
+        case friendly
+        case dirty
+    }
+
+    public enum TDType: String {
+        case truth
+        case dare
+    }
+
+    public func perform() -> Promise<TruthOrDare, Error> {
+        Promise.catching { try HTTPRequest(host: "randommer.io", path: "/truth-dare-generator", method: "POST", query: [
+            "category": category.rawValue,
+            "type": type.rawValue
+        ]) }
+            .then { $0.fetchJSONAsync(as: TruthOrDare.self) }
+    }
+}
