33b17c392ab2806f958564147c94ce48287f004f
fwcd <fwcdmail@gmail.com>
2021-02-17 22:22:47 +0100

    Add custom headers in AkinatorSession

--- Sources/D2NetAPIs/akinator/AkinatorSession.swift
+++ Sources/D2NetAPIs/akinator/AkinatorSession.swift
@@ -1,11 +1,27 @@
 import Foundation
 import Utils
+import Logging
 
 // Ported from https://github.com/janniksam/Akinator.Api.Net/blob/master/Akinator.Api.Net/AkinatorClient.cs
 // MIT-licensed, Copyright (c) 2019 Jannik
 
+fileprivate let jQuerySignature = "jQuery331023608747682107778"
+fileprivate let headers = [
+    "Accept": "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01",
+    "Accept-Language": "en-US,en;q=0.9,ar;q=0.8",
+    "X-Requested-With": "XMLHttpRequest",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
+    "Connection": "keep-alive",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36",
+    "Referer": "https://en.akinator.com/game"
+]
+
 fileprivate let sessionPattern = try! Regex(from: "var uid_ext_session = '([^']*)'\\;\\s*.*var frontaddr = '([^']*)'\\;")
-fileprivate let startGamePattern = try! Regex(from: "^jQuery3410014644797238627216_\\d+\\((.+)\\)$")
+fileprivate let startGamePattern = try! Regex(from: "^\(jQuerySignature)_\\d+\\((.+)\\)$")
+
+fileprivate let log = Logger(label: "D2NetAPIs.AkinatorSession")
 
 public struct AkinatorSession {
     private let session: String
@@ -26,7 +42,7 @@ public struct AkinatorSession {
     }
 
     public static func create() -> Promise<(AkinatorSession, AkinatorQuestion), Error> {
-        let time = Int64(Date().timeIntervalSince1970 * 1000)
+        let time = Int64(Date().timeIntervalSince1970 * 1000) + 1
         return AkinatorServersQuery().perform()
             .thenCatching {
                 guard let url = $0.parameters.instance.first?.urlBaseWs else { throw AkinatorError.noServersFound }
@@ -37,7 +53,7 @@ public struct AkinatorSession {
                     host: "en.akinator.com",
                     path: "/new_session",
                     query: [
-                        "callback": "jQuery3410014644797238627216_\(time)",
+                        "callback": "\(jQuerySignature)_\(time)",
                         "urlApiWs": "\(url)",
                         "player": "website-desktop",
                         "partner": "1",
@@ -47,8 +63,9 @@ public struct AkinatorSession {
                         "constraint": "ETAT<>'AV'",
                         "soft_constraint": "",
                         "question_filter": "",
-                        "_": "\(time)"
-                    ]
+                        "_": "\(time + 1)"
+                    ],
+                    headers: headers
                 ).fetchUTF8Async().map { ($0, url) }
             }
             .mapCatching { (raw: String, url: URL) in
@@ -75,7 +92,7 @@ public struct AkinatorSession {
             "signature": signature,
             "step": "\(step)",
             "answer": "\(answer.value)"
-        ]) }
+        ], headers: headers) }
             .then { $0.fetchJSONAsync(as: AkinatorResponse.StepInformation.self) }
             .map {
                 step = $0.parameters.step
@@ -85,11 +102,13 @@ public struct AkinatorSession {
     }
 
     private static func getApiKey() -> Promise<ApiKey, Error> {
-        Promise.catching { try HTTPRequest(host: "en.akinator.com", path: "/game") }
+        Promise.catching { try HTTPRequest(host: "en.akinator.com", path: "/game", headers: headers) }
             .then { $0.fetchUTF8Async() }
             .mapCatching {
                 guard let parsed = sessionPattern.firstGroups(in: $0) else { throw AkinatorError.sessionPatternNotFound }
-                return ApiKey(uidExtSession: parsed[1], frontaddr: parsed[2])
+                let key = ApiKey(uidExtSession: parsed[1], frontaddr: parsed[2])
+                log.info("Got \(key)")
+                return key
             }
     }
 }
