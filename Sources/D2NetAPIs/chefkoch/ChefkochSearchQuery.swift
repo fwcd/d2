f79b89cf92f11dea748888e6ef075ce35c424915
fwcd <fwcdmail@gmail.com>
2021-02-18 02:12:17 +0100

    Add more parameters to ChefkochSearchQuery

--- Sources/D2NetAPIs/chefkoch/ChefkochSearchQuery.swift
+++ Sources/D2NetAPIs/chefkoch/ChefkochSearchQuery.swift
@@ -1,16 +1,52 @@
 import Utils
 
 public struct ChefkochSearchQuery {
-    public let query: String
-    public let limit: Int
+    private let query: String
+    private let limit: Int
+    private let orderBy: OrderBy
+    private let descendCategories: Bool
+    private let minimumRating: Double
+    private let maximumTime: Double
 
-    public init(query: String, limit: Int = 5) {
+    public init(
+        query: String,
+        limit: Int = 5,
+        orderBy: OrderBy = .relevance,
+        descendCategories: Bool = true,
+        minimumRating: Double = 0,
+        maximumTime: Double = 0
+    ) {
         self.query = query
         self.limit = limit
+        self.orderBy = orderBy
+        self.descendCategories = descendCategories
+        self.minimumRating = minimumRating
+        self.maximumTime = maximumTime
+    }
+
+    public enum OrderBy: Int {
+        case relevance = 2
+        case rating = 3
+        case difficulty = 4
+        case maxTimeNeeded = 5
+        case date = 6
+        case random = 7
+        case dailyShuffle = 8
     }
 
     public func perform() -> Promise<ChefkochSearchResults, Error> {
-        Promise.catching { try HTTPRequest(host: "api.chefkoch.de", path: "/v2/recipes", query: ["query": query, "limit": String(limit)]) }
+        Promise.catching { try HTTPRequest(
+            host: "api.chefkoch.de",
+            path: "/v2/recipes",
+            query: [
+                "query": query,
+                "limit": String(limit),
+                "orderBy": String(orderBy.rawValue),
+                "descendCategories": descendCategories ? "1" : "0",
+                "minimumRating": String(minimumRating),
+                "maximumTime": String(maximumTime)
+            ]
+        ) }
             .then { $0.fetchJSONAsync(as: ChefkochSearchResults.self) }
     }
 }
