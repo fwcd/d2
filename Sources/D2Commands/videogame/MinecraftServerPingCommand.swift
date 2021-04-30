de7556aff1c6555e610dde5be59d8140d728c0f4
fwcd <fwcdmail@gmail.com>
2021-04-30 12:10:30 +0200

    Render favicon in mc server ping embeds

--- Sources/D2Commands/videogame/MinecraftServerPingCommand.swift
+++ Sources/D2Commands/videogame/MinecraftServerPingCommand.swift
@@ -1,3 +1,4 @@
+import Foundation
 import Logging
 import Socket
 import D2MessageIO
@@ -23,20 +24,25 @@ public class MinecraftServerPingCommand: StringCommand {
             if let (host, port) = parseHostPort(from: input) {
                 let serverInfo = try MinecraftServerPing(host: host, port: port ?? 25565, timeoutMs: 1000).perform()
                 let modCount = serverInfo.forgeData?.mods?.count ?? serverInfo.modinfo?.modList?.count
+                let faviconImage = serverInfo.faviconImage
 
-                // TODO: Display server favicon in embed
-
-                output.append(Embed(
-                    title: "Minecraft Server at `\(host)\(port.map { ":\($0)" } ?? "")`",
-                    description: "\(serverInfo.description)",
-                    footer: modCount.map { _ in Embed.Footer(text: "Use \(context.commandPrefix)mcmods to get a detailed mod list") },
-                    fields: [
-                        Embed.Field(name: "Online", value: "\(serverInfo.players.online) of \(serverInfo.players.max)"),
-                        Embed.Field(name: "Players", value: serverInfo.players.sample?.map { $0.name }.joined(separator: "\n") ?? "_no information_"),
-                        Embed.Field(name: "Version", value: serverInfo.version.name),
-                        Embed.Field(name: "Mods", value: modCount.map { "\($0) \("mod".pluralized(with: $0))" } ?? "_vanilla_")
-                    ]
-                ))
+                output.append(.compound([
+                    .embed(Embed(
+                        title: "Minecraft Server at `\(host)\(port.map { ":\($0)" } ?? "")`",
+                        description: "\(serverInfo.description)",
+                        thumbnail: URL(string: "attachment://favicon.png").filter { _ in faviconImage != nil }.map { Embed.Thumbnail(url: $0) },
+                        footer: modCount.map { _ in Embed.Footer(text: "Use \(context.commandPrefix)mcmods to get a detailed mod list") },
+                        fields: [
+                            Embed.Field(name: "Online", value: "\(serverInfo.players.online) of \(serverInfo.players.max)"),
+                            Embed.Field(name: "Players", value: serverInfo.players.sample?.map { $0.name }.joined(separator: "\n") ?? "_no information_"),
+                            Embed.Field(name: "Version", value: serverInfo.version.name),
+                            Embed.Field(name: "Mods", value: modCount.map { "\($0) \("mod".pluralized(with: $0))" } ?? "_vanilla_")
+                        ]
+                    )),
+                    .files([
+                        faviconImage.flatMap { try? $0.pngEncoded() }.map { Message.FileUpload(data: $0, filename: "favicon.png", mimeType: "image/png") }
+                    ].compactMap { $0 })
+                ]))
             } else {
                 output.append(errorText: "Could not parse host/port, please specify it using the following format: `localhost:25565`")
             }
