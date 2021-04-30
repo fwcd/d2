b867ecdc72193a87c51b95ec68d1ab0f008caaf0
fwcd <fwcdmail@gmail.com>
2021-04-30 16:29:42 +0200

    WIP: Remove FinitePossibleMoves constraint in GameIntelligence

--- Sources/D2Commands/game/GameIntelligence.swift
+++ Sources/D2Commands/game/GameIntelligence.swift
@@ -1,7 +1,6 @@
 /// A strategy for picking a move.
 public protocol GameIntelligence {
     associatedtype State: GameState
-        where State: FinitePossibleMoves
 
     func pickMove(from state: State) throws -> State.Move
 }
