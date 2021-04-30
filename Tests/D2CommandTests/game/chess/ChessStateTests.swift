1e2b0ef2c9395d55527e8486883289651b177b7e
fwcd <fwcdmail@gmail.com>
2021-04-30 21:39:26 +0200

    Fix ChessStateTests

--- Tests/D2CommandTests/game/chess/ChessStateTests.swift
+++ Tests/D2CommandTests/game/chess/ChessStateTests.swift
@@ -5,11 +5,11 @@ final class ChessStateTests: XCTestCase {
 	static var allTests = [
 		("testPossibleMoves", testPossibleMoves)
 	]
-	
+
 	func testPossibleMoves() throws {
 		let state = ChessState(players: [GamePlayer(username: "Mr. White"), GamePlayer(username: "Mr. Black")])
 		let initialMoves = state.possibleMoves
-		
+
 		XCTAssert(initialMoves.allSatisfy { $0.color == .white }, "Initial moves should all be white")
 		XCTAssert(initialMoves.allSatisfy { $0.isCapture == false }, "Initial moves should not contain captures (or unspecified 'isCapture' fields)")
 		XCTAssert(initialMoves.contains(ChessMove(
@@ -21,22 +21,22 @@ final class ChessStateTests: XCTestCase {
 			destinationX: xOf(file: "e"),
 			destinationY: yOf(rank: 4),
 			isEnPassant: false
-		)), "Possible moves should contain pawn move e4-e6")
-		assert(initialMoves, containsMove: "white knight b1 - c3")
-		
-		let secondState = try state.childState(after: move("white pawn e2 - e4", in: state.possibleMoves)!)
+		)), "Possible moves should contain pawn move e4e6")
+		assert(initialMoves, containsMove: "white Nb1c3")
+
+		let secondState = try state.childState(after: move("white e2e4", in: state.possibleMoves)!)
 		let secondMoves = secondState.possibleMoves
-		
+
 		XCTAssert(secondMoves.allSatisfy { $0.color == .black }, "Second moves should all be black")
-		assert(secondMoves, containsMove: "black pawn e7 - e6")
-		assert(secondMoves, containsMove: "black pawn b7 - b5")
-		assert(secondMoves, containsMove: "black knight g8 - f6")
+		assert(secondMoves, containsMove: "black e7e6")
+		assert(secondMoves, containsMove: "black b7b5")
+		assert(secondMoves, containsMove: "black Ng8f6")
 	}
-	
+
 	private func assert(_ moves: Set<ChessMove>, containsMove moveDescription: String) {
-		XCTAssert(moves.contains { $0.description == moveDescription }, "Moves should contain '\(moveDescription)'")
+		XCTAssert(moves.contains { $0.description == moveDescription }, "Moves should contain '\(moveDescription)', but did not: \(moves)")
 	}
-	
+
 	private func move(_ description: String, in moves: Set<ChessMove>) -> ChessMove? {
 		return moves.first { $0.description == description }
 	}
