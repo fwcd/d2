a99b98ee0d4ae7be8b10d555ad3699c401bbedb7
fwcd <fwcdmail@gmail.com>
2021-05-21 18:02:24 +0200

    Reimplement LaTeX renderer using MathJax 3

--- Node/latex-renderer/src/index.js
+++ Node/latex-renderer/src/index.js
@@ -1,31 +1,34 @@
-const mathjax = require("mathjax-node-svg2png");
 const fs = require("fs");
+const util = require("util");
+const svg2img = util.promisify(require("svg2img"));
 
 (async () => {
+    const mathjax = await require("mathjax").init({
+        loader: {
+            load: ["input/tex", "output/svg"]
+        }
+    });
+
     try {
         const args = process.argv;
-        if (args.length < 6) {
-            console.log("Usage: node " + args[1] + " [tex math] [color] [output file] [scale]");
+        if (args.length < 5) {
+            console.log(`Usage: node ${args[1]} [tex math] [color] [scale]`);
             return;
         }
         const math = args[2];
         const color = args[3];
-        const outputFile = args[4];
-        const scale = args[5];
+        const scale = args[4];
 
-        mathjax.start();
-        const res = await mathjax.typeset({
-            math: "\\color{" + color + "}{" + math + "}",
-            png: true,
-            scale: scale,
-            linebreaks: true,
-            width: 80
+        const adaptor = mathjax.startup.adaptor;
+        const svgNode = await mathjax.tex2svgPromise(`\\color{${color}}{${math}}`, {
+            display: true,
+            containerWidth: 80
         });
-        const base64PngData = res.png.replace(/^data:image\/png;base64,/, "");
 
-        fs.writeFile(outputFile, base64PngData, "base64", e => {
-            if (e) console.log("Error while writing to file: " + e);
-        });
+        const svgString = adaptor.innerHTML(svgNode);
+
+        const pngBuffer = await svg2img(svgString);
+        process.stdout.write(pngBuffer);
     } catch (e) {
         console.log("Error: " + e);
     }
