55d9e9802555d47c35d7bc7a2172d64527b32cb1
fwcd <fwcdmail@gmail.com>
2021-09-27 15:57:47 +0200

    Use switch over joke type

--- Sources/D2Commands/fun/JokeCommand.swift
+++ Sources/D2Commands/fun/JokeCommand.swift
@@ -15,23 +15,24 @@ public class JokeCommand: VoidCommand {
         JokeAPIQuery().perform().listen {
             do {
                 let joke = try $0.get()
-                if joke.type == .single {
-                    guard let line = joke.joke else {
-                        output.append(errorText: "The joke did not contain a joke.")
-                        return
-                    }
-                    output.append(Embed(
-                        description: "**\(line)**"
-                    ))
-                } else if joke.type == .twopart {
-                    guard let setup = joke.setup, let delivery = joke.delivery else {
-                        output.append(errorText: "The joke either did not contain a setup or a delivery.")
-                        return
-                    }
-                    output.append(Embed(
-                        title: setup,
-                        description: delivery
-                    ))
+                switch joke.type {
+                    case .single:
+                        guard let line = joke.joke else {
+                            output.append(errorText: "The joke did not contain a joke.")
+                            return
+                        }
+                        output.append(Embed(
+                            description: "**\(line)**"
+                        ))
+                    case .twopart:
+                        guard let setup = joke.setup, let delivery = joke.delivery else {
+                            output.append(errorText: "The joke either did not contain a setup or a delivery.")
+                            return
+                        }
+                        output.append(Embed(
+                            title: setup,
+                            description: delivery
+                        ))
                 }
             } catch {
                 output.append(error, errorText: "Could not fetch joke")
