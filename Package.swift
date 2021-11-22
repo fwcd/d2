466ba5366e2df208556c53671b22bc900022bb6d
fwcd <fwcdmail@gmail.com>
2021-11-22 12:22:59 +0100

    Add dependency on NIOCronScheduler

--- Package.swift
+++ Package.swift
@@ -34,6 +34,7 @@ let package = Package(
         .package(url: "https://github.com/nmdias/FeedKit.git", from: "9.1.2"),
         .package(url: "https://github.com/SwiftyTesseract/SwiftyTesseract.git", from: "4.0.0"),
         .package(url: "https://github.com/dehesa/CodableCSV.git", from: "0.6.5"),
+        .package(url: "https://github.com/MihaelIsaev/NIOCronScheduler.git", from: "2.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
