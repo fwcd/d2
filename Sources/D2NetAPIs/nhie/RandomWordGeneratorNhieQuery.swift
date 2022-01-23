d5b5af5d1e651561a1ba2b795ca2b58645ccf64a
fwcd <fwcdmail@gmail.com>
2022-01-23 13:04:52 +0100

    Add RandomWordGeneratorNhieQuery

---
+++ Sources/D2NetAPIs/nhie/RandomWordGeneratorNhieQuery.swift
@@ -0,0 +1,20 @@
+import Utils
+
+public struct RandomWordGeneratorNhieQuery {
+    private struct Response: Codable {
+        let data: [String: [String]]
+    }
+
+    public func perform() -> Promise<[NeverHaveIEverStatement], Error> {
+        Promise.catching { try HTTPRequest(host: "randomwordgenerator.com", path: "/json/question-never-have-i-ever.json") }
+            .then { $0.fetchJSONAsync(as: Response.self) }
+            .map {
+                $0.data.flatMap { (category, statements) in statements.map {
+                    NeverHaveIEverStatement(
+                        statement: $0,
+                        category: category
+                    )
+                } }
+            }
+    }
+}
