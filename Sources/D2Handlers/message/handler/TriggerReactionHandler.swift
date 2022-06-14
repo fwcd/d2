8cd08de78f3e1913dc597d4b5446ba0b8dd6f93e
fwcd <fwcdmail@gmail.com>
2022-06-14 14:48:07 +0200

    Make TriggerReactionHandler slightly more flexible

--- Sources/D2Handlers/message/handler/TriggerReactionHandler.swift
+++ Sources/D2Handlers/message/handler/TriggerReactionHandler.swift
@@ -3,21 +3,27 @@ import D2MessageIO
 import Utils
 
 public struct TriggerReactionHandler: MessageHandler {
-    private let keywords: [String: String]
+    private let keywords: [[String]: String]
 
-    public init(keywords: [String: String] = [
-        "hello": "👋",
-        "hmmm": "🤔",
-        "hai": "🦈",
-        "spooky": "🎃",
-        "ghost": "👻"
+    public init(keywords: [[String]: String] = [
+        ["hello"]: "👋",
+        ["hmmm"]: "🤔",
+        ["hai"]: "🦈",
+        ["spooky"]: "🎃",
+        ["ghost"]: "👻",
+        ["good morning", "guten morgen"]: "☀️"
     ]) {
         self.keywords = keywords
     }
 
     public func handle(message: Message, from client: any MessageClient) -> Bool {
-        if let emoji = keywords[message.content.lowercased()], let messageId = message.id, let channelId = message.channelId {
-            client.createReaction(for: messageId, on: channelId, emoji: emoji)
+        if let messageId = message.id, let channelId = message.channelId {
+            let lowerContent = message.content.lowercased()
+            for (words, emoji) in keywords {
+                if words.contains(where: lowerContent.contains) {
+                    client.createReaction(for: messageId, on: channelId, emoji: emoji)
+                }
+            }
             return true
         }
         return false
