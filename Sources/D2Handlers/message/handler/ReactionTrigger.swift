659c642559532a34165712793eb3a03c28110183
fwcd <fwcdmail@gmail.com>
2022-06-16 01:12:55 +0200

    Make ReactionTrigger more flexible

--- Sources/D2Handlers/message/handler/ReactionTrigger.swift
+++ Sources/D2Handlers/message/handler/ReactionTrigger.swift
@@ -1,17 +1,31 @@
+import D2MessageIO
+
 public struct ReactionTrigger {
-    private let keywords: [String]
+    private let keywords: Set<String>
+    private let authorNames: Set<String>?
+    private let messageTypes: Set<Message.MessageType>?
     private let probability: Double
     let emoji: String
 
-    public init(keywords: [String], probability: Double = 1, emoji: String) {
+    public init(
+        keywords: Set<String>,
+        authorNames: Set<String>? = nil,
+        messageTypes: Set<Message.MessageType>? = nil,
+        probability: Double = 1,
+        emoji: String
+    ) {
         self.keywords = keywords
+        self.authorNames = authorNames
+        self.messageTypes = messageTypes
         self.probability = probability
         self.emoji = emoji
     }
 
-    func matches(content: String) -> Bool {
-        guard Double.random(in: 0..<1) < probability else { return false }
-        let lowered = content.lowercased()
+    func matches(message: Message) -> Bool {
+        guard Double.random(in: 0..<1) < probability,
+              authorNames.map({ $0.contains(message.authorDisplayName.lowercased()) }) ?? true,
+              messageTypes.flatMap({ message.type.map($0.contains) }) ?? true else { return false }
+        let lowered = message.content.lowercased()
         return keywords.contains(where: lowered.contains)
     }
 }
