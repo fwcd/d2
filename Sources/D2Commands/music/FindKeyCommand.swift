a2a5ebed9dfed5de1a7dc4bd533e04addc8096f6
fwcd <fwcdmail@gmail.com>
2022-08-27 17:43:44 +0200

    Remove debug message

--- Sources/D2Commands/music/FindKeyCommand.swift
+++ Sources/D2Commands/music/FindKeyCommand.swift
@@ -19,10 +19,7 @@ public class FindKeyCommand: StringCommand {
 
         let scales = twelveToneOctave
             .flatMap { key -> [Scale] in [DiatonicMajorScale(key: key), DiatonicMinorScale(key: key)] }
-            .filter {
-                print("\(notes) vs \($0.notes) -> \(notes.isSubset(of: $0.notes))")
-                return notes.isSubset(of: $0.notes)
-            }
+            .filter { notes.isSubset(of: $0.notes) }
         output.append("Possible keys: \(scales.map(String.init(describing:)).joined(separator: " "))")
     }
 }
