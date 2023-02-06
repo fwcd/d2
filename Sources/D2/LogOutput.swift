9aef120f24e0dd93cbf4de3a5043320c4de015df
fwcd <fwcdmail@gmail.com>
2023-02-06 18:53:00 +0100

    Make LogOutput reentrant

--- Sources/D2/LogOutput.swift
+++ Sources/D2/LogOutput.swift
@@ -4,6 +4,7 @@ import Dispatch
 class LogOutput {
     private static let dispatchQueue = DispatchQueue(label: "D2.LogOutput")
 
+    private var isSilenced: Bool = false
     private var consumers: [(String) -> Void] = []
 
     /// Registers a consumer for log messages.
@@ -13,9 +14,11 @@ class LogOutput {
 
     /// Publishes a log message.
     private func publish(_ message: String) {
+        isSilenced = true
         for consumer in consumers {
             consumer(message)
         }
+        isSilenced = false
     }
 
     /// Registers a consumer for log messages asynchronously.
@@ -27,6 +30,7 @@ class LogOutput {
 
     /// Publishes a log message asynchronously.
     func publishAsync(_ message: String) {
+        guard !isSilenced else { return }
         Self.dispatchQueue.async {
             self.publish(message)
         }
