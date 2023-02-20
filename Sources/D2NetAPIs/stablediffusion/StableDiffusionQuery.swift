b10a7e31b7617407f76e6b3ed40bd6ef5a661cc8
fwcd <fwcdmail@gmail.com>
2023-02-20 02:31:08 +0100

    Add todo note

--- Sources/D2NetAPIs/stablediffusion/StableDiffusionQuery.swift
+++ Sources/D2NetAPIs/stablediffusion/StableDiffusionQuery.swift
@@ -8,5 +8,5 @@ import FoundationNetworking
 public struct StableDiffusionQuery {
     private let input: StableDiffusionInput
 
-    // TODO
+    // TODO: Obtain API key for production D2 and add stable diffusion support using https://replicate.com/stability-ai/stable-diffusion/versions/f178fa7a1ae43a9a9af01b833b9d2ecf97b1bcb0acfd2dc5dd04895e042863f1/api#run
 }
