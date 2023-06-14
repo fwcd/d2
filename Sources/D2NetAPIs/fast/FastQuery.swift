89d8ca5613573b76884f1419fcd49b607e287270
fwcd <fwcdmail@gmail.com>
2023-06-14 17:15:58 +0200

    Update utils and fix FastQuery

--- Sources/D2NetAPIs/fast/FastQuery.swift
+++ Sources/D2NetAPIs/fast/FastQuery.swift
@@ -1,23 +1,23 @@
 import Foundation
 import Utils
 
-private let tokenPattern = try! Regex(from: #"token:\\s*"([^"]+)""#)
+private let tokenPattern = try! Regex(from: #"token:\s*"([^"]+)""#)
 
 public struct FastQuery {
     public init() {}
 
     public func perform() -> Promise<FastResult, any Error> {
-        Promise.catching { try HTTPRequest(host: "fast.com", path: "/") }
+        Promise.catching { try HTTPRequest(scheme: "https", host: "fast.com", path: "/") }
             .then { $0.fetchHTMLAsync() }
-            .thenCatching {
-                guard let script = try $0.getElementsByTag("script").last() else {
+            .thenCatching { document in
+                guard let script = try document.getElementsByTag("script").last() else {
                     throw NetApiError.apiError("Could not find script on fast homepage")
                 }
                 let src = try script.attr("src")
                 return try HTTPRequest(host: "fast.com", path: src).fetchUTF8Async()
             }
-            .thenCatching {
-                guard let parsedToken = tokenPattern.firstGroups(in: $0) else {
+            .thenCatching { raw in
+                guard let parsedToken = tokenPattern.firstGroups(in: raw) else {
                     throw NetApiError.apiError("Could not find fast token")
                 }
                 let startTime = Date()
@@ -28,9 +28,9 @@ public struct FastQuery {
                         "https": "true",
                         "token": parsedToken[1]
                     ]
-                ).runAsync().map {
+                ).runAsync().map { data in
                     let seconds = -startTime.timeIntervalSinceNow
-                    let megabits = Double($0.count) * 8_000_000.0
+                    let megabits = Double(data.count) / 125_000.0
                     return FastResult(megabitsPerSecond: megabits / seconds)
                 }
             }
