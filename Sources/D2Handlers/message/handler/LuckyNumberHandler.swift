d9cc0371885ad896d0723eda8d4568fb4161b225
fwcd <fwcdmail@gmail.com>
2023-07-14 00:36:07 +0200

    Add minimum number count

--- Sources/D2Handlers/message/handler/LuckyNumberHandler.swift
+++ Sources/D2Handlers/message/handler/LuckyNumberHandler.swift
@@ -5,20 +5,21 @@ fileprivate let numberPattern = try! Regex(from: "\\d+")
 
 public struct LuckyNumberHandler: MessageHandler {
     private let luckyNumber: Int
+    private let minimumNumberCount: Int
 
-    public init(luckyNumber: Int) {
+    public init(luckyNumber: Int, minimumNumberCount: Int = 1) {
         self.luckyNumber = luckyNumber
+        self.minimumNumberCount = minimumNumberCount
     }
 
     public func handle(message: Message, from client: any MessageClient) -> Bool {
         if let channelId = message.channelId {
             let numbers = numberPattern.allGroups(in: message.content).compactMap { Int($0[0]) }
             let sum = numbers.reduce(0, +)
-            if sum == luckyNumber {
+            if sum == luckyNumber && numbers.count >= minimumNumberCount {
                 client.sendMessage(
                     """
                     All the numbers in your message added up to \(luckyNumber). Congrats!
-
                     ```
                     \(numbers.map { "\($0)" }.reduce1 { "\($0) + \($1)" } ?? "_empty sum_") = \(sum)
                     ```
