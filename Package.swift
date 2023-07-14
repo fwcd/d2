0ef6c3008a02e1f3925620a077a5a5753707b078
fwcd <fwcdmail@gmail.com>
2023-07-14 15:32:43 +0200

    Bump swift-mensa to 0.1.10

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         .package(url: "https://github.com/fwcd/swift-utils.git", from: "2.0.1"),
         .package(url: "https://github.com/fwcd/swift-graphics.git", from: "2.1.0"),
         .package(url: "https://github.com/fwcd/swift-gif.git", from: "2.1.0"),
-        .package(url: "https://github.com/fwcd/swift-mensa.git", from: "0.1.9"),
+        .package(url: "https://github.com/fwcd/swift-mensa.git", from: "0.1.10"),
         .package(url: "https://github.com/fwcd/swift-music-theory.git", from: "0.1.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.44.0"),
         .package(url: "https://github.com/Kitura/BlueSocket.git", .upToNextMinor(from: "2.0.4")),
