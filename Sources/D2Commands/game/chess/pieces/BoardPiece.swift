142a0ada7993c41c1f32f39ffe4113200d5081f2
fwcd <fwcdmail@gmail.com>
2023-11-16 20:13:55 +0000

    Add workaround for failing Python linking in CI
    See https://github.com/actions/runner-images/issues/8500

--- Sources/D2Commands/game/chess/pieces/BoardPiece.swift
+++ Sources/D2Commands/game/chess/pieces/BoardPiece.swift
@@ -1,12 +1,12 @@
 public struct BoardPiece {
     public let color: ChessRole
-    public var piece: ChessPiece
+    public var piece: any ChessPiece
     public var moveCount: Int
     public var moved: Bool { moveCount > 0 }
 
     public var resourcePng: String { (color == .white) ? piece.whiteResourcePng : piece.blackResourcePng }
 
-    public init(_ color: ChessRole, _ piece: ChessPiece, moveCount: Int = 0) {
+    public init(_ color: ChessRole, _ piece: any ChessPiece, moveCount: Int = 0) {
         self.color = color
         self.piece = piece
         self.moveCount = moveCount
