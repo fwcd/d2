51187747ecf99c0703f3baf31a8e2abb57fb8548
fwcd <fwcd@fwcd.dev>
2024-02-13 03:37:12 +0100

    Migrate ShortAlgebraicNotationParser to regex literals

--- Sources/D2Commands/game/chess/notation/ShortAlgebraicNotationParser.swift
+++ Sources/D2Commands/game/chess/notation/ShortAlgebraicNotationParser.swift
@@ -11,27 +11,27 @@ import Utils
 /// 7. group: The promotion piece, if present
 /// 8. group: '+' if the move results in a check, '#' if the move results in a checkmate
 /// 9. group: 'e. p.' if the move is an en-passant
-fileprivate let notationRegex = try! LegacyRegex(from: "([A-Z]?)([a-h]?)([1-8])?(x?)([a-h])([1-8])(?:=?([A-Z]))?([\\+#]?)(?:\\s+(e.\\s*p.))?")
-fileprivate let shortCastlingRegex = try! LegacyRegex(from: "(?:0-0)|(?:O-O)")
-fileprivate let longCastlingRegex = try! LegacyRegex(from: "(?:0-0-0)|(?:O-O-O)")
+fileprivate let notationRegex = #/([A-Z]?)([a-h]?)([1-8])?(x?)([a-h])([1-8])(?:=?([A-Z]))?([\+#]?)(?:\s+(e.\s*p.))?/#
+fileprivate let shortCastlingRegex = #/(?:0-0)|(?:O-O)/#
+fileprivate let longCastlingRegex = #/(?:0-0-0)|(?:O-O-O)/#
 
 struct ShortAlgebraicNotationParser: ChessNotationParser {
     func parse(_ notation: String) -> ChessMove? {
-        if longCastlingRegex.matchCount(in: notation) > 0 {
+        if !notation.matches(of: longCastlingRegex).isEmpty {
             return ChessMove(castlingType: .long)
-        } else if shortCastlingRegex.matchCount(in: notation) > 0 {
+        } else if !notation.matches(of: shortCastlingRegex).isEmpty {
             return ChessMove(castlingType: .short)
-        } else if let parsed = notationRegex.firstGroups(in: notation) {
+        } else if let parsed = try? notationRegex.firstMatch(in: notation) {
             return ChessMove(
-                pieceType: parsed[1].nilIfEmpty.flatMap { pieceOf(letter: Character($0))?.pieceType } ?? .pawn,
-                originX: parsed[2].nilIfEmpty.flatMap { xOf(file: Character($0)) },
-                originY: parsed[3].nilIfEmpty.map { yOf(rank: Int($0)!) },
-                isCapture: !parsed[4].isEmpty,
-                destinationX: parsed[5].nilIfEmpty.flatMap { xOf(file: Character($0)) },
-                destinationY: parsed[6].nilIfEmpty.map { yOf(rank: Int($0)!) },
-                promotionPieceType: parsed[7].nilIfEmpty.flatMap { pieceOf(letter: Character($0))?.pieceType },
-                checkType: parseRaw(checkType: parsed[8]),
-                isEnPassant: !parsed[9].isEmpty
+                pieceType: parsed.1.nilIfEmpty.flatMap { pieceOf(letter: Character(String($0)))?.pieceType } ?? .pawn,
+                originX: parsed.2.nilIfEmpty.flatMap { xOf(file: Character(String($0))) },
+                originY: parsed.3?.nilIfEmpty.map { yOf(rank: Int($0)!) },
+                isCapture: !parsed.4.isEmpty,
+                destinationX: parsed.5.nilIfEmpty.flatMap { xOf(file: Character(String($0))) },
+                destinationY: parsed.6.nilIfEmpty.map { yOf(rank: Int($0)!) },
+                promotionPieceType: parsed.7?.nilIfEmpty.flatMap { pieceOf(letter: Character(String($0)))?.pieceType },
+                checkType: parseRaw(checkType: String(parsed.8)),
+                isEnPassant: !(parsed.9?.isEmpty ?? true)
             )
         } else {
             return nil
