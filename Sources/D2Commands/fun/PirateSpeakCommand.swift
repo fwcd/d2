38c63a4c114784279c428d460bccdfce3f689c82
fwcd <fwcd@fwcd.dev>
2024-02-13 03:28:12 +0100

    Migrate PirateSpeakCommand's substitutions to regex literals

--- Sources/D2Commands/fun/PirateSpeakCommand.swift
+++ Sources/D2Commands/fun/PirateSpeakCommand.swift
@@ -2,6 +2,41 @@ import Utils
 
 fileprivate let punctuationPattern = #/[\.!\?]/#
 
+fileprivate let substitutions = [
+    (#/hello/#, "ahoy"),
+    (#/yes/#, "aye"),
+    (#/this/#, "'tis"),
+    (#/the/#, "th'"),
+    (#/my/#, "me"),
+    (#/and/#, "'n"),
+    (#/as/#, "'s"),
+    (#/of/#, "o'"),
+    (#/\bthose\b/#, "'ose"),
+    (#/\bwhoa\b/#, "avast"),
+    (#/\bvery\b/#, "mighty"),
+    (#/\bocean\b/#, "briney deep"),
+    (#/\beggs\b/#, "cackle fruit"),
+    (#/\bdaredevil\b/#, "swashbuckler"),
+    (#/\btreasure\b/#, "booty"),
+    (#/\breward\b/#, "bounty"),
+    (#/chest\b/#, "coffer"),
+    (#/ng\b/#, "n'"),
+    (#/ngs\b/#, "n's"),
+    (#/st\b/#, "s'"),
+    (#/you/#, "ye"),
+    (#/\bto/#, "t'"),
+    (#/\bis/#, "be"),
+].map { (pattern: $0.0.ignoresCase(), substitution: $0.1) }
+
+fileprivate let interjections = [
+    "Arr!",
+    "C'mere, me beauty.",
+    "Savvy?",
+    "Shiver me timbers!",
+    "Sink me!",
+    "Yo ho ho.",
+]
+
 public class PirateSpeakCommand: StringCommand {
     public let info = CommandInfo(
         category: .fun,
@@ -10,44 +45,8 @@ public class PirateSpeakCommand: StringCommand {
         requiredPermissionLevel: .basic
     )
     public let outputValueType: RichValueType = .text
-    private let substitutions: [String: String]
-    private let interjections: [String]
 
-    public init(substitutions: [String: String] = [
-        "hello": "ahoy",
-        "yes": "aye",
-        "this": "'tis",
-        "the": "th'",
-        "my": "me",
-        "and": "'n",
-        "as": "'s",
-        "of": "o'",
-        "\\bthose\\b": "'ose",
-        "\\bwhoa\\b": "avast",
-        "\\bvery\\b": "mighty",
-        "\\bocean\\b": "briney deep",
-        "\\beggs\\b": "cackle fruit",
-        "\\bdaredevil\\b": "swashbuckler",
-        "\\btreasure\\b": "booty",
-        "\\breward\\b": "bounty",
-        "\\chest\\b": "coffer",
-        "ng\\b": "n'",
-        "ngs\\b": "n's",
-        "st\\b": "s'",
-        "you": "ye",
-        "\\bto": "t'",
-        "\\bis": "be"
-    ], interjections: [String] = [
-        "Arr!",
-        "C'mere, me beauty.",
-        "Savvy?",
-        "Shiver me timbers!",
-        "Sink me!",
-        "Yo ho ho."
-    ]) {
-        self.substitutions = substitutions
-        self.interjections = interjections
-    }
+    public init() {}
 
     public func invoke(with input: String, output: any CommandOutput, context: CommandContext) {
         guard !input.isEmpty else {
@@ -56,7 +55,7 @@ public class PirateSpeakCommand: StringCommand {
         }
 
         let preprocessed = input.replacing(punctuationPattern) { "\($0.0)\(interjections.randomElement().filter { _ in Int.random(in: 0..<4) < 3 }.map { " \($0)" } ?? "")" }
-        let result = substitutions.reduce(preprocessed) { try! LegacyRegex(from: $1.key, caseSensitive: false).replace(in: $0, with: $1.value, casePreserving: true) }
+        let result = substitutions.reduce(preprocessed) { $0.replacing($1.pattern, with: $1.substitution) }
         output.append(result)
     }
 }
