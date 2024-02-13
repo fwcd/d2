a468632c222575840820bc6090b68b24c609a32d
fwcd <fwcd@fwcd.dev>
2024-02-13 01:08:47 +0100

    Test lucky number detection

--- Tests/D2HandlersTests/message/handler/LuckyNumberHandlerTests.swift
+++ Tests/D2HandlersTests/message/handler/LuckyNumberHandlerTests.swift
@@ -1,4 +1,6 @@
 import XCTest
+import D2MessageIO
+import D2TestUtils
 @testable import D2Handlers
 
 final class LuckyNumberHandlerTests: XCTestCase {
@@ -18,5 +20,24 @@ final class LuckyNumberHandlerTests: XCTestCase {
         XCTAssertFalse(handler.isLucky(41))
         XCTAssertTrue(handler.isLucky(42))
         XCTAssertTrue(handler.isLucky(420))
+    }
+
+    func testMessageTrigger() {
+        let handler = LuckyNumberHandler(luckyNumbers: [42])
+        let output = TestOutput()
+
+        let message = Message(
+            content: "40 is a nice number and so is 2",
+            channelId: ID("Dummy Channel")
+        )
+        output.messages.append(message)
+        _ = handler.handle(message: message, from: output)
+
+        XCTAssertEqual(output.lastContent, """
+            All the numbers in your message added up to 42. Congrats!
+            ```
+            40 + 2 = 42
+            ```
+            """)
     }
 }
