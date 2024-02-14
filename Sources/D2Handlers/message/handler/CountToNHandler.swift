62a9507c11dd5363cc6b163d15a936dc8506d617
fwcd <fwcd@fwcd.dev>
2024-02-14 18:18:50 +0100

    Migrate CountToNHandler to regex literals

--- Sources/D2Handlers/message/handler/CountToNHandler.swift
+++ Sources/D2Handlers/message/handler/CountToNHandler.swift
@@ -2,11 +2,11 @@ import D2Commands
 import D2MessageIO
 import Utils
 
-fileprivate let countToNPattern = try! LegacyRegex(from: "count\\s+to\\s+(\\d+)")
+fileprivate let countToNPattern = #/count\s+to\s+(?<n>\d+)/#
 
 public struct CountToNHandler: MessageHandler {
     public func handle(message: Message, sink: any Sink) -> Bool {
-        if let parsed = countToNPattern.firstGroups(in: message.content.lowercased()), let n = UInt(parsed[1]), n <= 300, let channelId = message.channelId {
+        if let parsed = try? countToNPattern.firstMatch(in: message.content.lowercased()), let n = UInt(parsed.n), n <= 300, let channelId = message.channelId {
             sink.sendMessage(Message(content: "Here you go: \((1...n).map(String.init).joined(separator: ", "))"), to: channelId)
             return true
         }
