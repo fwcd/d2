507902da606cb510232b0762d637e3dc6d387209
fwcd <fwcd@fwcd.dev>
2024-07-02 20:41:14 +0200

    Remove minimumRating/maximumTime
    These parameters do not seem to work reliably and are not used anyway

--- Sources/D2NetAPIs/Chefkoch/ChefkochSearchQuery.swift
+++ Sources/D2NetAPIs/Chefkoch/ChefkochSearchQuery.swift
@@ -5,8 +5,6 @@ public struct ChefkochSearchQuery {
     private let limit: Int
     private let orderBy: OrderBy
     private let descendCategories: Bool
-    private let minimumRating: Double
-    private let maximumTime: Double
     private let order: Bool
 
     public init(
@@ -14,16 +12,12 @@ public struct ChefkochSearchQuery {
         limit: Int = 5,
         orderBy: OrderBy = .relevance,
         descendCategories: Bool = true,
-        minimumRating: Double = 0,
-        maximumTime: Double = 0,
         order: Bool = false
     ) {
         self.query = query
         self.limit = limit
         self.orderBy = orderBy
         self.descendCategories = descendCategories
-        self.minimumRating = minimumRating
-        self.maximumTime = maximumTime
         self.order = order
     }
 
@@ -47,8 +41,6 @@ public struct ChefkochSearchQuery {
                 "orderBy": String(orderBy.rawValue),
                 "descendCategories": descendCategories ? "1" : "0",
                 "order": order ? "1" : "0",
-                "minimumRating": String(minimumRating),
-                "maximumTime": String(maximumTime)
             ]
         )
         return try await request.fetchJSON(as: ChefkochSearchResults.self)
