5686cf1bf717addd7ae5cad508385191b7aa5b4d
fwcd <fwcd@fwcd.dev>
2024-07-03 17:59:47 +0200

    Migrate HugCommand to async

--- Sources/D2Commands/Fun/HugCommand.swift
+++ Sources/D2Commands/Fun/HugCommand.swift
@@ -16,37 +16,34 @@ public class HugCommand: Command {
         self.inventoryManager = inventoryManager
     }
 
-    public func invoke(with input: RichValue, output: any CommandOutput, context: CommandContext) {
+    public func invoke(with input: RichValue, output: any CommandOutput, context: CommandContext) async {
         guard let author = context.author else {
-            output.append(errorText: "No author found")
+            await output.append(errorText: "No author found")
             return
         }
         guard let user = input.asMentions?.first else {
-            output.append(errorText: "Please mention someone!")
+            await output.append(errorText: "Please mention someone!")
             return
         }
         guard let avatarUrl = context.sink?.avatarUrlForUser(user.id, with: user.avatar, size: 128, preferredExtension: "png") else {
-            output.append(errorText: "Could not fetch avatar URL")
+            await output.append(errorText: "Could not fetch avatar URL")
             return
         }
 
-        Promise(.success(HTTPRequest(url: avatarUrl)))
-            .then { $0.runAsync() }
-            .listen {
-                do {
-                    let image = try CairoImage(pngData: $0.get())
-                    let rawTemplate = try CairoImage(pngFilePath: "Resources/Fun/hugTemplate.png")
-                    let green = Color(rgb: 0x00FF03)
-                    let (topLeft, bottomRight) = findBoundingBox(in: rawTemplate) { $0.squaredEuclideanDistance(to: green) < 0.01 }
-                    let template = try colorToAlpha(in: rawTemplate, color: green, squaredThreshold: 0.6)
-                    let composition = try composeImage(from: template, with: image, between: topLeft, and: bottomRight)
-                    try output.append(composition)
+        do {
+            let imageData = try await HTTPRequest(url: avatarUrl).run()
+            let image = try CairoImage(pngData: imageData)
+            let rawTemplate = try CairoImage(pngFilePath: "Resources/Fun/hugTemplate.png")
+            let green = Color(rgb: 0x00FF03)
+            let (topLeft, bottomRight) = findBoundingBox(in: rawTemplate) { $0.squaredEuclideanDistance(to: green) < 0.01 }
+            let template = try colorToAlpha(in: rawTemplate, color: green, squaredThreshold: 0.6)
+            let composition = try composeImage(from: template, with: image, between: topLeft, and: bottomRight)
+            try await output.append(composition)
 
-                    // Place the hug in the recipient's inventory
-                    self.inventoryManager[user].append(item: .init(id: "hug-\(author.username)", name: "Hug by \(author.username)"), to: "Hugs")
-                } catch {
-                    output.append(error, errorText: "Could not create hug image. :(")
-                }
-            }
+            // Place the hug in the recipient's inventory
+            self.inventoryManager[user].append(item: .init(id: "hug-\(author.username)", name: "Hug by \(author.username)"), to: "Hugs")
+        } catch {
+            await output.append(error, errorText: "Could not create hug image. :(")
+        }
     }
 }
