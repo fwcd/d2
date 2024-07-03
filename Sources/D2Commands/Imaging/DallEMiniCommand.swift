a3863846d9943e1114cdefe70fa16cfdd3cac026
fwcd <fwcd@fwcd.dev>
2024-07-03 23:28:43 +0200

    Migrate DallEMiniCommand and DallEMiniQuery to async

--- Sources/D2Commands/Imaging/DallEMiniCommand.swift
+++ Sources/D2Commands/Imaging/DallEMiniCommand.swift
@@ -10,32 +10,30 @@ public class DallEMiniCommand: StringCommand {
 
     public init() {}
 
-    public func invoke(with input: String, output: CommandOutput, context: CommandContext) {
+    public func invoke(with input: String, output: CommandOutput, context: CommandContext) async {
         guard !input.isEmpty else {
-            output.append(errorText: "Please enter a text prompt!")
+            await output.append(errorText: "Please enter a text prompt!")
             return
         }
 
-        output.append("Generating an image... (could take a minute or two)")
+        await output.append("Generating an image... (could take a minute or two)")
 
-        DallEMiniQuery(prompt: input).perform().listen {
-            do {
-                let result = try $0.get()
-                guard !result.images.isEmpty else {
-                    output.append(errorText: "DALL-E generated no images")
-                    return
-                }
-                let files = result.decodedJpegImages.map {
-                    Message.FileUpload(data: $0, filename: "dallemini-result.jpg", mimeType: "image/jpeg")
-                }
-                guard !files.isEmpty else {
-                    output.append(errorText: "DALL-E images could not be decoded")
-                    return
-                }
-                output.append(.files(files))
-            } catch {
-                output.append(error, errorText: "Could not query DALL-E result")
+        do {
+            let result = try await DallEMiniQuery(prompt: input).perform()
+            guard !result.images.isEmpty else {
+                await output.append(errorText: "DALL-E generated no images")
+                return
             }
+            let files = result.decodedJpegImages.map {
+                Message.FileUpload(data: $0, filename: "dallemini-result.jpg", mimeType: "image/jpeg")
+            }
+            guard !files.isEmpty else {
+                await output.append(errorText: "DALL-E images could not be decoded")
+                return
+            }
+            await output.append(.files(files))
+        } catch {
+            await output.append(error, errorText: "Could not query DALL-E result")
         }
     }
 }
--- Sources/D2NetAPIs/DallEMini/DallEMiniQuery.swift
+++ Sources/D2NetAPIs/DallEMini/DallEMiniQuery.swift
@@ -12,34 +12,32 @@ public struct DallEMiniQuery: Codable {
         self.prompt = prompt
     }
 
-    public func perform() -> Promise<DallEMiniResponse, Error> {
-        Promise.catching { () throws -> HTTPRequest in
-            // Encode query as JSON
-            let jsonData = try JSONEncoder().encode(self)
-            guard let json = String(data: jsonData, encoding: .utf8) else {
-                throw EncodeError.couldNotDecode(jsonData)
-            }
-
-            // We need a custom URL session with longer timeouts
-            // since long response times are to be expected
-            let configuration = URLSessionConfiguration.default
-            let timeout = TimeInterval(200)
-            configuration.timeoutIntervalForRequest = timeout
-            configuration.timeoutIntervalForResource = timeout
-            let session = URLSession(configuration: configuration)
-
-            return try HTTPRequest(
-                host: "bf.dallemini.ai",
-                path: "/generate",
-                method: "POST",
-                headers: [
-                    "Content-Type": "application/json",
-                    "Accept": "application/json",
-                ],
-                body: json,
-                session: session
-            )
+    public func perform() async throws -> DallEMiniResponse {
+        // Encode query as JSON
+        let jsonData = try JSONEncoder().encode(self)
+        guard let json = String(data: jsonData, encoding: .utf8) else {
+            throw EncodeError.couldNotDecode(jsonData)
         }
-        .then { $0.fetchJSONAsync(as: DallEMiniResponse.self) }
+
+        // We need a custom URL session with longer timeouts
+        // since long response times are to be expected
+        let configuration = URLSessionConfiguration.default
+        let timeout = TimeInterval(200)
+        configuration.timeoutIntervalForRequest = timeout
+        configuration.timeoutIntervalForResource = timeout
+        let session = URLSession(configuration: configuration)
+
+        let request = try HTTPRequest(
+            host: "bf.dallemini.ai",
+            path: "/generate",
+            method: "POST",
+            headers: [
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            ],
+            body: json,
+            session: session
+        )
+        return try await request.fetchJSON(as: DallEMiniResponse.self)
     }
 }
