e6ea4f0fe326261824566b51dd47fe5b6269a2d5
fwcd <fwcd@fwcd.dev>
2024-07-03 23:30:57 +0200

    Use fetchXML in DBLPPublicationsQuery

--- Sources/D2NetAPIs/DBLP/DBLPPublicationsQuery.swift
+++ Sources/D2NetAPIs/DBLP/DBLPPublicationsQuery.swift
@@ -10,7 +10,6 @@ public struct DBLPPublicationsQuery {
 
     public func perform() async throws -> DBLPPublicationsResult {
         let request = try HTTPRequest(host: "dblp.org", path: "/search/publ/api", query: ["q": term])
-        let data = try await request.run()
-        return try XMLDecoder().decode(DBLPPublicationsResult.self, from: data)
+        return try await request.fetchXML(as: DBLPPublicationsResult.self)
     }
 }
