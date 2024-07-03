3844bb6a9790bae12c74d7459fbacf1ec32da5ab
fwcd <fwcd@fwcd.dev>
2024-07-03 18:10:43 +0200

    Migrate FTBModpacksQuery and FTBModpacksCommand to async

--- Sources/D2Commands/Videogame/FTBModpacksCommand.swift
+++ Sources/D2Commands/Videogame/FTBModpacksCommand.swift
@@ -15,29 +15,27 @@ public class FTBModpacksCommand: StringCommand {
 
     public init() {}
 
-    public func invoke(with input: String, output: any CommandOutput, context: CommandContext) {
-        FTBModpacksQuery().perform().listen {
-            do {
-                let packs = try $0.get().prefix(5)
-                output.append(Embed(
-                    title: "Recent FTB Modpacks",
-                    image: (packs.last?.imageUrl).flatMap(URL.init(string:)).map(Embed.Image.init(url:)),
-                    fields: packs.map {
-                        Embed.Field(
-                            name: "\($0.name ?? "?")\($0.mcVersion.map { " (\($0))" } ?? "")",
-                            value: """
-                                [[Download Pack]](\($0.downloadUrl ?? "")) [[Download Server]](\($0.serverDownloadUrl ?? ""))
-                                \($0.description?
-                                    .replacingOccurrences(of: "<br>", with: "\n")
-                                    .replacingOccurrences(of: "\n\n", with: "\n")
-                                    .truncated(to: 300, appending: "...") ?? "_no description_")
-                                """
-                        )
-                    }
-                ))
-            } catch {
-                output.append(error, errorText: "Could not fetch recent FTB modpacks")
-            }
+    public func invoke(with input: String, output: any CommandOutput, context: CommandContext) async {
+        do {
+            let packs = try await FTBModpacksQuery().perform()
+            await output.append(Embed(
+                title: "Recent FTB Modpacks",
+                image: (packs.last?.imageUrl).flatMap(URL.init(string:)).map(Embed.Image.init(url:)),
+                fields: packs.map {
+                    Embed.Field(
+                        name: "\($0.name ?? "?")\($0.mcVersion.map { " (\($0))" } ?? "")",
+                        value: """
+                            [[Download Pack]](\($0.downloadUrl ?? "")) [[Download Server]](\($0.serverDownloadUrl ?? ""))
+                            \($0.description?
+                                .replacingOccurrences(of: "<br>", with: "\n")
+                                .replacingOccurrences(of: "\n\n", with: "\n")
+                                .truncated(to: 300, appending: "...") ?? "_no description_")
+                            """
+                    )
+                }
+            ))
+        } catch {
+            await output.append(error, errorText: "Could not fetch recent FTB modpacks")
         }
     }
 }
--- Sources/D2NetAPIs/Minecraft/FTB/FTBModpacksQuery.swift
+++ Sources/D2NetAPIs/Minecraft/FTB/FTBModpacksQuery.swift
@@ -3,8 +3,10 @@ import Utils
 public struct FTBModpacksQuery {
     public init() {}
 
-    public func perform() -> Promise<[FTBModpack], any Error> {
-        Promise.catching { try HTTPRequest(host: "ftb.forgecdn.net", path: "/FTB2/static/modpacks.xml") }
-            .then { req in Promise { then in req.fetchXMLAsync(using: FTBModpacksXMLParserDelegate(then: then)) } }
+    public func perform() async throws -> [FTBModpack] {
+        let request = try HTTPRequest(host: "ftb.forgecdn.net", path: "/FTB2/static/modpacks.xml")
+        return try await withCheckedThrowingContinuation { continuation in
+            request.fetchXMLAsync(using: FTBModpacksXMLParserDelegate(then: continuation.resume(with:)))
+        }
     }
 }
