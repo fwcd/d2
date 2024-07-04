dca394c99053ed11a0b6249dea81b5bfc9101e14
fwcd <fwcd@fwcd.dev>
2024-07-04 15:48:48 +0200

    Migrate GiphyTrendingQuery to async

--- Sources/D2NetAPIs/Giphy/GiphyTrendingQuery.swift
+++ Sources/D2NetAPIs/Giphy/GiphyTrendingQuery.swift
@@ -7,13 +7,12 @@ public struct GiphyTrendingQuery {
         self.limit = limit
     }
 
-    public func perform() -> Promise<GiphyResults, any Error> {
-        Promise.catching { () -> HTTPRequest in
-            guard let key = storedNetApiKeys?.giphy else { throw NetApiError.missingApiKey("No giphy key provided") }
-            return try HTTPRequest(host: "api.giphy.com", path: "/v1/gifs/trending", query: [
-                "api_key": key,
-                "limit": String(limit)
-            ])
-        }.then { $0.fetchJSONAsync(as: GiphyResults.self) }
+    public func perform() async throws -> GiphyResults {
+        guard let key = storedNetApiKeys?.giphy else { throw NetApiError.missingApiKey("No giphy key provided") }
+        let request = try HTTPRequest(host: "api.giphy.com", path: "/v1/gifs/trending", query: [
+            "api_key": key,
+            "limit": String(limit)
+        ])
+        return try await request.fetchJSON(as: GiphyResults.self)
     }
 }
