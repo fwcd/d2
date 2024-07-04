877fff1113161e517ce63042f220f27e90cfa4c8
fwcd <fwcd@fwcd.dev>
2024-07-04 14:07:51 +0200

    Migrate SolveLinearSystemCommand to async

--- Sources/D2Commands/Math/SolveLinearSystemCommand.swift
+++ Sources/D2Commands/Math/SolveLinearSystemCommand.swift
@@ -16,13 +16,13 @@ public class SolveLinearSystemCommand: Command {
         self.sizeLimit = sizeLimit
     }
 
-    public func invoke(with input: RichValue, output: any CommandOutput, context: CommandContext) {
+    public func invoke(with input: RichValue, output: any CommandOutput, context: CommandContext) async {
         guard let matrix = input.asNDArrays?.first?.asMatrix else {
-            output.append(errorText: "Please input a matrix")
+            await output.append(errorText: "Please input a matrix")
             return
         }
         guard matrix.width == matrix.height + 1 else {
-            output.append(errorText: "The matrix should have the shape of an augmented matrix (i.e. be one column wider than a square matrix).")
+            await output.append(errorText: "The matrix should have the shape of an augmented matrix (i.e. be one column wider than a square matrix).")
             return
         }
         let cols = matrix.columns
@@ -30,10 +30,10 @@ public class SolveLinearSystemCommand: Command {
         let b = Matrix(columnVector: cols.last!)
 
         guard let aInverted = a.inverse else {
-            output.append(errorText: "Linearly dependent equations are currently not supported")
+            await output.append(errorText: "Linearly dependent equations are currently not supported")
             return
         }
 
-        output.append(.ndArrays([(aInverted * b).asNDArray]))
+        await output.append(.ndArrays([(aInverted * b).asNDArray]))
     }
 }
