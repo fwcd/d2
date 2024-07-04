f96a34a666cb362aa804ebce1ad058650cccb409
fwcd <fwcd@fwcd.dev>
2024-07-04 13:59:43 +0200

    Migrate EmojiImageCommand to async

--- Sources/D2Commands/Imaging/EmojiImageCommand.swift
+++ Sources/D2Commands/Imaging/EmojiImageCommand.swift
@@ -12,42 +12,39 @@ public class EmojiImageCommand: StringCommand {
 
     public init() {}
 
-    public func invoke(with input: String, output: any CommandOutput, context: CommandContext) {
+    public func invoke(with input: String, output: any CommandOutput, context: CommandContext) async {
         guard let guild = context.guild else {
-            output.append(errorText: "Not on a guild")
+            await output.append(errorText: "Not on a guild")
             return
         }
 
         guard !input.isEmpty else {
-            output.append(errorText: "Please enter the name of an emoji!")
+            await output.append(errorText: "Please enter the name of an emoji!")
             return
         }
 
         let emojis = guild.emojis.values.filter { $0.id != nil }
         guard let emoji = emojis.first(where: { $0.name == input }) else {
             let closest = emojis.min(by: ascendingComparator { $0.name.levenshteinDistance(to: input) })
-            output.append(errorText: "Could not find such an emoji!\(closest.map { " Did you mean `\($0.name)`?" } ?? "")")
+            await output.append(errorText: "Could not find such an emoji!\(closest.map { " Did you mean `\($0.name)`?" } ?? "")")
             return
         }
 
-        Promise.catching { try HTTPRequest(
-            scheme: "https",
-            host: "cdn.discordapp.com",
-            path: "/emojis/\(emoji.id!).png"
-        ) }
-            .then { $0.runAsync() }
-            .listen {
-                do {
-                    let data = try $0.get()
-                    guard !data.isEmpty else {
-                        output.append(errorText: "No emoji image available")
-                        return
-                    }
-
-                    output.append(.image(try CairoImage(pngData: data)))
-                } catch {
-                    output.append(error, errorText: "Could not fetch emoji image")
-                }
+        do {
+            let request = try HTTPRequest(
+                scheme: "https",
+                host: "cdn.discordapp.com",
+                path: "/emojis/\(emoji.id!).png"
+            )
+            let data = try await request.run()
+            guard !data.isEmpty else {
+                await output.append(errorText: "No emoji image available")
+                return
             }
+
+            await output.append(.image(try CairoImage(pngData: data)))
+        } catch {
+            await output.append(error, errorText: "Could not fetch emoji image")
+        }
     }
 }
