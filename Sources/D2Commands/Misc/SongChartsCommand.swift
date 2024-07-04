4880b829f9b7d7e7550102ef1866858ebf810369
fwcd <fwcd@fwcd.dev>
2024-07-04 14:22:06 +0200

    Migrate SongChartsCommand to async

--- Sources/D2Commands/Misc/SongChartsCommand.swift
+++ Sources/D2Commands/Misc/SongChartsCommand.swift
@@ -18,7 +18,7 @@ public class SongChartsCommand: StringCommand {
         longDescription: "Repeatedly retrieves currently playing songs to provide a chart list of popular songs on the server",
         requiredPermissionLevel: .vip
     )
-    private var subcommands: [String: (CommandOutput, CommandContext) -> Void] = [:]
+    private var subcommands: [String: (CommandOutput, CommandContext) async -> Void] = [:]
 
     @AutoSerializing(filePath: "local/songCharts.json") private var songCharts: [GuildID: GuildSongCharts] = .init()
     @AutoSerializing(filePath: "local/songTrackedGuilds.json") private var trackedGuilds: Set<GuildID> = []
@@ -30,26 +30,26 @@ public class SongChartsCommand: StringCommand {
         subcommands = [
             "track": { [unowned self] output, context in
                 guard let guild = context.guild else {
-                    output.append(errorText: "No guild available.")
+                    await output.append(errorText: "No guild available.")
                     return
                 }
                 guard !self.trackedGuilds.contains(guild.id) else {
-                    output.append(errorText: "Already tracked.")
+                    await output.append(errorText: "Already tracked.")
                     return
                 }
                 self.trackedGuilds.insert(guild.id)
-                output.append(":white_check_mark: Successfully begun to track song charts in guild `\(guild.name)`")
+                await output.append(":white_check_mark: Successfully begun to track song charts in guild `\(guild.name)`")
             },
             "untrack": { [unowned self] output, context in
                 guard let guild = context.guild else {
-                    output.append(errorText: "No guild available.")
+                    await output.append(errorText: "No guild available.")
                     return
                 }
                 self.trackedGuilds.remove(guild.id)
-                output.append(":x: Successfully untracked guild `\(guild.name)`")
+                await output.append(":x: Successfully untracked guild `\(guild.name)`")
             },
             "tracked": { [unowned self] output, context in
-                output.append(Embed(
+                await output.append(Embed(
                     title: "Tracked Guilds",
                     description: self.trackedGuilds.compactMap { context.sink?.guild(for: $0) }.map { $0.name }.joined(separator: "\n"),
                     footer: Embed.Footer(text: "Guilds for which anonymized song statistics are collected")
@@ -57,22 +57,22 @@ public class SongChartsCommand: StringCommand {
             },
             "clear": { [unowned self] output, context in
                 guard let guild = context.guild else {
-                    output.append(errorText: "No guild available.")
+                    await output.append(errorText: "No guild available.")
                     return
                 }
                 self.songCharts[guild.id] = nil
-                output.append(":wastebasket: Successfully cleared song charts for `\(guild.name)`")
+                await output.append(":wastebasket: Successfully cleared song charts for `\(guild.name)`")
             },
             "debugPresence": { [] output, context in
                 guard let guild = context.guild else {
-                    output.append("Not on a guild.")
+                    await output.append("Not on a guild.")
                     return
                 }
                 guard let mentioned = context.message.mentions.first else {
-                    output.append("Please mention someone!")
+                    await output.append("Please mention someone!")
                     return
                 }
-                output.append(.compound([
+                await output.append(.compound([
                     .text("User `\(mentioned.username)` has the presence:"),
                     .code(guild.presences[mentioned.id].map { "\($0)" } ?? "nil", language: "swift")
                 ]))
@@ -85,13 +85,13 @@ public class SongChartsCommand: StringCommand {
             """
     }
 
-    public func invoke(with input: String, output: any CommandOutput, context: CommandContext) {
+    public func invoke(with input: String, output: any CommandOutput, context: CommandContext) async {
         if input.isEmpty {
             guard let charts = (context.guild?.id).flatMap({ songCharts[$0] }) else {
-                output.append(errorText: "No song charts available for this guild.")
+                await output.append(errorText: "No song charts available for this guild.")
                 return
             }
-            output.append(Embed(
+            await output.append(Embed(
                 title: ":star: Song Charts :musical_note:",
                 description: charts.playCounts
                     .sorted { $0.value > $1.value }
@@ -101,9 +101,9 @@ public class SongChartsCommand: StringCommand {
                     .joined(separator: "\n")
             ))
         } else if let subcommand = subcommands[String(input.split(separator: " ")[0])] {
-            subcommand(output, context)
+            await subcommand(output, context)
         } else {
-            output.append(errorText: "Unrecognized subcommand `\(input)`")
+            await output.append(errorText: "Unrecognized subcommand `\(input)`")
         }
     }
 
