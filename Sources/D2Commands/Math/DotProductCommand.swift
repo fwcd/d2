74934e56a4e48255619ec3d32950f5b532093d09
fwcd <fwcd@fwcd.dev>
2024-07-04 14:03:41 +0200

    Migrate DotProductCommand to async

--- Sources/D2Commands/Math/DotProductCommand.swift
+++ Sources/D2Commands/Math/DotProductCommand.swift
@@ -11,20 +11,20 @@ public class DotProductCommand: Command {
 
     public init() {}
 
-    public func invoke(with input: RichValue, output: any CommandOutput, context: CommandContext) {
+    public func invoke(with input: RichValue, output: any CommandOutput, context: CommandContext) async {
         guard let factors = input.asNDArrays else {
-            output.append(errorText: "Please specify the input in the form of vectors, e.g. `(1, 2, 3, 4) (1, 1, (1, 1)`")
+            await output.append(errorText: "Please specify the input in the form of vectors, e.g. `(1, 2, 3, 4) (1, 1, (1, 1)`")
             return
         }
         guard factors.count == 2 else {
-            output.append(errorText: "Please input exactly two vectors.")
+            await output.append(errorText: "Please input exactly two vectors.")
             return
         }
 
         do {
-            output.append(.ndArrays([NDArray(try factors[0].dot(factors[1]))]))
+            await output.append(.ndArrays([NDArray(try factors[0].dot(factors[1]))]))
         } catch {
-            output.append(error, errorText: "Could not compute dot product: \(error)")
+            await output.append(error, errorText: "Could not compute dot product: \(error)")
         }
     }
 }
