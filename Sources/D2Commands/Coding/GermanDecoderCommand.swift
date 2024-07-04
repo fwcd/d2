b92d189ede09ec32da6347540e95ab4b103817bc
fwcd <fwcd@fwcd.dev>
2024-07-04 13:05:46 +0200

    Migrate GermanDecoderCommand to async

--- Sources/D2Commands/Coding/GermanDecoderCommand.swift
+++ Sources/D2Commands/Coding/GermanDecoderCommand.swift
@@ -7,11 +7,11 @@ public class GermanDecoderCommand: StringCommand {
 
     public init() {}
 
-    public func invoke(with input: String, output: any CommandOutput, context: CommandContext) {
+    public func invoke(with input: String, output: any CommandOutput, context: CommandContext) async {
         guard let decoded = String(data: germanDecode(input), encoding: .utf8) else {
-            output.append(errorText: "Data is not UTF-8 encoded!")
+            await output.append(errorText: "Data is not UTF-8 encoded!")
             return
         }
-        output.append(decoded)
+        await output.append(decoded)
     }
 }
