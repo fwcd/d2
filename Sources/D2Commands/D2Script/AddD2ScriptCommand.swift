78f244dfb1adb9d9104f44f21377572f3ea6e0b8
fwcd <fwcd@fwcd.dev>
2024-07-04 13:07:50 +0200

    Migrate AddD2ScriptCommand to async

--- Sources/D2Commands/D2Script/AddD2ScriptCommand.swift
+++ Sources/D2Commands/D2Script/AddD2ScriptCommand.swift
@@ -20,28 +20,28 @@ public class AddD2ScriptCommand: StringCommand {
 
     public init() {}
 
-    public func invoke(with input: String, output: any CommandOutput, context: CommandContext) {
+    public func invoke(with input: String, output: any CommandOutput, context: CommandContext) async {
         if let code = try? codePattern.firstMatch(in: input)?.code {
             do {
                 let command = try D2ScriptCommand(script: try parser.parse(String(code)))
                 let name = command.name
                 guard !name.contains(" ") else {
-                    output.append(errorText: "Command name '\(name)' may not contain spaces")
+                    await output.append(errorText: "Command name '\(name)' may not contain spaces")
                     return
                 }
 
                 let registry = context.registry
                 registry[name] = command
-                output.append(":ballot_box_with_check: Added/updated command `\(name)`")
+                await output.append(":ballot_box_with_check: Added/updated command `\(name)`")
             } catch D2ScriptCommandError.noCommandDefined(let msg) {
-                output.append(errorText: "No command defined: \(msg)")
+                await output.append(errorText: "No command defined: \(msg)")
             } catch D2ScriptCommandError.multipleCommandsDefined(let msg) {
-                output.append(errorText: "Multiple commands defined: \(msg)")
+                await output.append(errorText: "Multiple commands defined: \(msg)")
             } catch {
-                output.append(error, errorText: "Could not parse code.")
+                await output.append(error, errorText: "Could not parse code.")
             }
         } else {
-            output.append(errorText: "Did not recognize code. \(info.helpText!)")
+            await output.append(errorText: "Did not recognize code. \(info.helpText!)")
         }
     }
 }
