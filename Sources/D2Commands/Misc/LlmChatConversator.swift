c6183ddf67b4748f91259a6c583f2b7cc56008bf
fwcd <fwcd@fwcd.dev>
2024-09-19 05:05:44 +0200

    Make LlmChatConversator an actor

--- Sources/D2Commands/Misc/LlmChatConversator.swift
+++ Sources/D2Commands/Misc/LlmChatConversator.swift
@@ -1,8 +1,9 @@
 import Utils
 import D2MessageIO
 
-public struct LlmChatConversator: Conversator {
+public actor LlmChatConversator: Conversator {
     private let session: NodePackage.JsonSession
+    private var isAnswering = false
 
     private struct Request: Codable {
         var message: String
@@ -18,6 +19,12 @@ public struct LlmChatConversator: Conversator {
     }
 
     public func answer(input: String, on guildId: GuildID) async throws -> String? {
+        // Since actors are reentrant, we need to guard against interleaving answer
+        // requests. Our solution is to simply make them return `nil`.
+        guard !isAnswering else { return nil }
+        isAnswering = true
+        defer { isAnswering = false }
+
         try session.send(Request(message: input))
         let response = try await session.receive(Response.self)
         return response.message
