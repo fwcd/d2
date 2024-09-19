475e930471e9e97f34ddc519c2cb8af26c1a1d17
fwcd <fwcd@fwcd.dev>
2024-09-19 04:56:16 +0200

    Implement LlmChatConversator

--- Sources/D2Commands/Misc/LlmChatConversator.swift
+++ Sources/D2Commands/Misc/LlmChatConversator.swift
@@ -2,10 +2,24 @@ import Utils
 import D2MessageIO
 
 public struct LlmChatConversator: Conversator {
-    public init() {}
+    private let session: NodePackage.JsonSession
 
-    public func answer(input: String, on guildId: GuildID) throws -> String? {
-        // TODO
-        nil
+    private struct Request: Codable {
+        var message: String
+        var systemMessage: String? = nil
+    }
+
+    private struct Response: Codable {
+        var message: String
+    }
+
+    public init() {
+        session = NodePackage(name: "llm-chat-client").startJsonSession()
+    }
+
+    public func answer(input: String, on guildId: GuildID) async throws -> String? {
+        try session.send(Request(message: input))
+        let response = try await session.receive(Response.self)
+        return response.message
     }
 }
