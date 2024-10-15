a70b488c9014c202aeb921ab1688e477b5f99792
fwcd <fwcd@fwcd.dev>
2024-10-15 16:16:14 +0200

    Migrate AddCronScheduleCommand to RegexCommand

--- Sources/D2Commands/Scripting/AddCronScheduleCommand.swift
+++ Sources/D2Commands/Scripting/AddCronScheduleCommand.swift
@@ -1,9 +1,7 @@
 import Utils
 import D2MessageIO
 
-fileprivate let argsPattern = #/(?<cron>[\*\s\d\-/,]*)\s+(?<name>\w+)\s+(?<command>.+)/#
-
-public class AddCronScheduleCommand: StringCommand {
+public class AddCronScheduleCommand: RegexCommand {
     public let info = CommandInfo(
         category: .scripting,
         shortDescription: "Adds a cron schedule",
@@ -15,25 +13,24 @@ public class AddCronScheduleCommand: StringCommand {
             """,
         requiredPermissionLevel: .admin
     )
+
+    public let inputPattern = #/(?<cron>[\*\s\d\-/,]*)\s+(?<name>\w+)\s+(?<command>.+)/#
+
     private let cronManager: CronManager
 
     public init(cronManager: CronManager) {
         self.cronManager = cronManager
     }
 
-    public func invoke(with input: String, output: any CommandOutput, context: CommandContext) async {
-        guard let parsedArgs = try? argsPattern.firstMatch(in: input) else {
-            await output.append(errorText: info.helpText!)
-            return
-        }
+    public func invoke(with input: Input, output: any CommandOutput, context: CommandContext) async {
         guard let channelId = context.channel?.id else {
             await output.append(errorText: "No channel id")
             return
         }
 
-        let cron = String(parsedArgs.cron)
-        let name = String(parsedArgs.name)
-        let command = String(parsedArgs.command)
+        let cron = String(input.cron)
+        let name = String(input.name)
+        let command = String(input.command)
 
         guard cronManager[name] == nil else {
             await output.append(errorText: "Schedule with name `\(name)` already exists, please unregister it first!")
