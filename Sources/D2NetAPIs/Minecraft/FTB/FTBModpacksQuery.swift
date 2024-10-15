e9bb921dc78b75f799683455d8705fdc8ecd7776
fwcd <fwcd@fwcd.dev>
2024-10-16 01:59:07 +0200

    Fix FTB modpack XML parsing

--- Sources/D2NetAPIs/Minecraft/FTB/FTBModpacksQuery.swift
+++ Sources/D2NetAPIs/Minecraft/FTB/FTBModpacksQuery.swift
@@ -6,7 +6,9 @@ public struct FTBModpacksQuery {
     public func perform() async throws -> [FTBModpack] {
         let request = try HTTPRequest(host: "ftb.forgecdn.net", path: "/FTB2/static/modpacks.xml")
         return try await withCheckedThrowingContinuation { continuation in
-            request.fetchXMLAsync(using: FTBModpacksXMLParserDelegate(then: continuation.resume(with:)))
+            request.fetchXMLAsync {
+                FTBModpacksXMLParserDelegate(continuation: continuation)
+            }
         }
     }
 }
