89fe8c684fc6e7ea4c1a0fc105ae34d31d314b7a
fwcd <fwcd@fwcd.dev>
2024-10-15 16:08:32 +0200

    Migrate PresenceCommand to RegexCommand

--- Sources/D2Commands/Meta/PresenceCommand.swift
+++ Sources/D2Commands/Meta/PresenceCommand.swift
@@ -9,54 +9,52 @@ fileprivate let activityTypes: [String: Presence.Activity.ActivityType] = [
     "listening": .listening
 ]
 fileprivate let availableStatusTypes = ["idle", "offline", "online", "dnd"]
-fileprivate let argsPattern = Regex {
-    Capture {
-        ChoiceOf(nonEmptyComponents: activityTypes.keys)
-    } transform: {
-        activityTypes[String($0)]
-    }
-    #/\s+/#
-    Optionally {
-        Capture {
-            ChoiceOf(nonEmptyComponents: availableStatusTypes)
-        } transform: {
-            Presence.Status(rawValue: String($0))
-        }
-        #/\s+/#
-    }
-    Capture {
-        #/.+/#
-    }
-}
 
-public class PresenceCommand: StringCommand {
+public class PresenceCommand: RegexCommand {
     public let info = CommandInfo(
         category: .meta,
         shortDescription: "Updates D2's presence",
         longDescription: "Updates the game activity and status",
+        helpText: "Syntax: [\(activityTypes.keys.joined(separator: "|"))] [\(availableStatusTypes.joined(separator: "|"))]? [custom text]",
         requiredPermissionLevel: .admin
     )
 
+    public let inputPattern = Regex {
+        Capture {
+            ChoiceOf(nonEmptyComponents: activityTypes.keys)
+        } transform: {
+            activityTypes[String($0)]
+        }
+        #/\s+/#
+        Optionally {
+            Capture {
+                ChoiceOf(nonEmptyComponents: availableStatusTypes)
+            } transform: {
+                Presence.Status(rawValue: String($0))
+            }
+            #/\s+/#
+        }
+        Capture {
+            #/.+/#
+        }
+    }
+
     public init() {}
 
-    public func invoke(with input: String, output: any CommandOutput, context: CommandContext) async {
-        if let parsedArgs = try? argsPattern.firstMatch(in: input) {
-            let activityType = parsedArgs.1!
-            let status = parsedArgs.2 ?? .online
-            let customText = String(parsedArgs.3)
+    public func invoke(with input: Input, output: any CommandOutput, context: CommandContext) async {
+        let activityType = input.1!
+        let status = input.2 ?? .online
+        let customText = String(input.3)
 
-            guard let sink = context.sink else {
-                await output.append(errorText: "No client found")
-                return
-            }
+        guard let sink = context.sink else {
+            await output.append(errorText: "No client found")
+            return
+        }
 
-            do {
-                try await sink.setPresence(PresenceUpdate(activities: [Presence.Activity(name: customText, type: activityType)], status: status))
-            } catch {
-                await output.append(error, errorText: "Could not set presence")
-            }
-        } else {
-            await output.append(errorText: "Syntax: [\(activityTypes.keys.joined(separator: "|"))] [\(availableStatusTypes.joined(separator: "|"))]? [custom text]")
+        do {
+            try await sink.setPresence(PresenceUpdate(activities: [Presence.Activity(name: customText, type: activityType)], status: status))
+        } catch {
+            await output.append(error, errorText: "Could not set presence")
         }
     }
 }
