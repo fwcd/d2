87e31738b60a4bf6892a224c472e0ce661cc6c58
fwcd <fwcd@fwcd.dev>
2024-10-16 04:19:55 +0200

    Expose SubscriptionSet.channelIds and remove Sequence conformance
    Conforming to Sequence seems to be impossible (?) if we wish to isolate
    the type to an actor.

--- Sources/D2Commands/SubscriptionSet.swift
+++ Sources/D2Commands/SubscriptionSet.swift
@@ -1,26 +1,22 @@
 import D2MessageIO
 
 @CommandActor
-public class SubscriptionSet: Sequence {
-    private var subscriptions: Set<ChannelID> = []
-    public var count: Int { subscriptions.count }
-    public var isEmpty: Bool { subscriptions.isEmpty }
+public class SubscriptionSet {
+    public private(set) var channelIds: Set<ChannelID> = []
+    public var count: Int { channelIds.count }
+    public var isEmpty: Bool { channelIds.isEmpty }
 
     public init() {}
 
     public func subscribe(to channelId: ChannelID) {
-        subscriptions.insert(channelId)
+        channelIds.insert(channelId)
     }
 
     public func unsubscribe(from channelId: ChannelID) {
-        subscriptions.remove(channelId)
+        channelIds.remove(channelId)
     }
 
     public func contains(_ channelId: ChannelID) -> Bool {
-        subscriptions.contains(channelId)
-    }
-
-    public func makeIterator() -> Set<ChannelID>.Iterator {
-        subscriptions.makeIterator()
+        channelIds.contains(channelId)
     }
 }
