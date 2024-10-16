b86c665ab507a11bfbbef72336a4a7032472397c
fwcd <fwcd@fwcd.dev>
2024-10-16 02:31:09 +0200

    Conform some more parse types to Sendable

--- Sources/D2Commands/Math/Parse/BinaryOperatorEntry.swift
+++ Sources/D2Commands/Math/Parse/BinaryOperatorEntry.swift
@@ -1,4 +1,4 @@
-struct BinaryOperatorEntry {
+struct BinaryOperatorEntry: Sendable {
     let precedence: Int
     let associativity: Associativity
     let factory: (any ExpressionASTNode, any ExpressionASTNode) -> any ExpressionASTNode
