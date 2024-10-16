130e5c991a75f8113a993cb9161dc32df88b5086
fwcd <fwcd@fwcd.dev>
2024-10-16 04:10:19 +0200

    Mark OutputChannel Sendable

--- Sources/D2Commands/Output/OutputChannel.swift
+++ Sources/D2Commands/Output/OutputChannel.swift
@@ -1,6 +1,6 @@
 import D2MessageIO
 
-public enum OutputChannel: Hashable {
+public enum OutputChannel: Hashable, Sendable {
     case guildChannel(ChannelID)
     case dmChannel(UserID)
     case defaultChannel
