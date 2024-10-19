8ca58d8ff00a4624526f60250f4bff021daa5250
fwcd <fwcd@fwcd.dev>
2024-10-19 21:39:01 +0200

    Mark exchangeRates as constant

--- Sources/D2Commands/Misc/CurrencyConversion.swift
+++ Sources/D2Commands/Misc/CurrencyConversion.swift
@@ -6,7 +6,7 @@ private let log = Logger(label: "D2Commands.CurrencyConversion")
 
 @CommandActor
 public struct CurrencyConversion: AsyncBijection {
-    private static var exchangeRates = AsyncLazyExpiring(in: 240.0 /* seconds */) {
+    private static let exchangeRates = AsyncLazyExpiring(in: 240.0 /* seconds */) {
         try await ExchangeApiQuery().perform()
     }
 
