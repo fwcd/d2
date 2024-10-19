e5036db9bd41e758e0db8d48ace2af2612f78205
fwcd <fwcd@fwcd.dev>
2024-10-19 23:20:02 +0200

    Fix CommandActor.run closure type

--- Sources/D2Commands/CommandActor.swift
+++ Sources/D2Commands/CommandActor.swift
@@ -7,7 +7,7 @@ public actor CommandActor: GlobalActor {
     /// Runs a closure on the CommandActor.
     ///
     /// See https://jaimzuber.com/swift-concurrency/run-actor-run
-    public static func run<T>(resultType: T.Type = T.self, body: @CommandActor @Sendable () throws -> T) async rethrows -> T where T: Sendable {
+    public static func run<T>(resultType: T.Type = T.self, body: @CommandActor @Sendable () async throws -> T) async rethrows -> T where T: Sendable {
         try await body()
     }
 }
