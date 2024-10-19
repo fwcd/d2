74df2107f4de9c170be98c004e71c5bbbd1fe933
fwcd <fwcd@fwcd.dev>
2024-10-19 23:17:44 +0200

    Add CommandActor.run

--- Sources/D2Commands/CommandActor.swift
+++ Sources/D2Commands/CommandActor.swift
@@ -3,4 +3,11 @@ public actor CommandActor: GlobalActor {
     public static let shared = CommandActor()
 
     private init() {}
+
+    /// Runs a closure on the CommandActor.
+    ///
+    /// See https://jaimzuber.com/swift-concurrency/run-actor-run
+    public static func run<T>(resultType: T.Type = T.self, body: @CommandActor @Sendable () throws -> T) async rethrows -> T where T: Sendable {
+        try await body()
+    }
 }
