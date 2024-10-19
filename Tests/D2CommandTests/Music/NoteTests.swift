9839f23fbf66b8261af197a865e954a62da1b434
fwcd <fwcd@fwcd.dev>
2024-10-20 01:53:11 +0200

    Migrate NoteTests to swift-testing

--- Tests/D2CommandTests/Music/NoteTests.swift
+++ Tests/D2CommandTests/Music/NoteTests.swift
@@ -1,25 +1,25 @@
-import XCTest
+import Testing
 import MusicTheory
 @testable import D2Commands
 
-final class NoteTests: XCTestCase {
-    func testNote() throws {
+struct NoteTests {
+    @Test func note() throws {
         let c4 = try Note(parsing: "C4")
         let d3 = try Note(parsing: "d3")
         let aSharp2 = try Note(parsing: "A#2")
         let fSharp7 = try Note(parsing: "F#7")
         let gFlat1 = try Note(parsing: "Gb1")
 
-        XCTAssertEqual(c4 + .majorSecond, try Note(parsing: "D4"))
-        XCTAssertEqual(c4 - .minorSecond, try Note(parsing: "B3"))
-        XCTAssertEqual(c4 - .majorSecond, try Note(parsing: "Bb3"))
-        XCTAssertEqual(c4 + .octaves(2), try Note(parsing: "C6"))
-        XCTAssertEqual(c4 + .octaves(-1), try Note(parsing: "C3"))
-        XCTAssertEqual(c4 - .octaves(3), try Note(parsing: "C1"))
-        XCTAssertEqual(fSharp7 - .majorSeventh, try Note(parsing: "G6"))
-        XCTAssertEqual(d3 + .minorThird, try Note(parsing: "F3"))
-        XCTAssertEqual(d3 + .majorThird, try Note(parsing: "F#3"))
-        XCTAssertEqual(aSharp2 + .unison, aSharp2)
-        XCTAssertEqual(gFlat1 + .octave, try Note(parsing: "Gb2"))
+        #expect(try c4 + .majorSecond == Note(parsing: "D4"))
+        #expect(try c4 - .minorSecond == Note(parsing: "B3"))
+        #expect(try c4 - .majorSecond == Note(parsing: "Bb3"))
+        #expect(try c4 + .octaves(2) == Note(parsing: "C6"))
+        #expect(try c4 + .octaves(-1) == Note(parsing: "C3"))
+        #expect(try c4 - .octaves(3) == Note(parsing: "C1"))
+        #expect(try fSharp7 - .majorSeventh == Note(parsing: "G6"))
+        #expect(try d3 + .minorThird == Note(parsing: "F3"))
+        #expect(try d3 + .majorThird == Note(parsing: "F#3"))
+        #expect(aSharp2 + .unison == aSharp2)
+        #expect(try gFlat1 + .octave == Note(parsing: "Gb2"))
     }
 }
