718b5b1449e9f2e7f76af6a4b228f39a65d460d9
fwcd <fwcd@fwcd.dev>
2024-10-20 01:20:09 +0200

    Fix CountToNHandler which didn't use throws either

--- Sources/D2Handlers/Message/Handler/CountToNHandler.swift
+++ Sources/D2Handlers/Message/Handler/CountToNHandler.swift
@@ -7,7 +7,7 @@ nonisolated(unsafe) private let countToNPattern = #/count\s+to\s+(?<n>\d+)/#
 fileprivate let log = Logger(label: "D2Handlers.CountToNHandler")
 
 public struct CountToNHandler: MessageHandler {
-    public func handle(message: Message, sink: any Sink) async throws -> Bool {
+    public func handle(message: Message, sink: any Sink) async -> Bool {
         if let parsed = try? countToNPattern.firstMatch(in: message.content.lowercased()), let n = UInt(parsed.n), n <= 300, let channelId = message.channelId {
             do {
                 try await sink.sendMessage(Message(content: "Here you go: \((1...n).map(String.init).joined(separator: ", "))"), to: channelId)
