59961ed88800997633ba23a8047d0128628b03a2
fwcd <fwcd@fwcd.dev>
2024-10-20 04:29:05 +0200

    Fix syllable loading

--- Sources/D2Datasets/Syllables.swift
+++ Sources/D2Datasets/Syllables.swift
@@ -1,15 +1,18 @@
 import Foundation
+import Logging
 
-nonisolated(unsafe) private let linePattern = #/(?<word>[^,]+),(?<count>\d+)/#
+private let log = Logger(label: "D2Datasets.Syllables")
+nonisolated(unsafe) private let linePattern = #/^(?<word>[^,]+),(?<count>\d+)/#
 
 public enum Syllables {
     public static let german = loadSyllables(name: "german")
 
     private static func loadSyllables(name: String) -> [String: Int] {
-        let raw = (try? String(contentsOfFile: "Resources/Syllables/\(name)", encoding: .utf8)) ?? ""
+        log.info("Loading \(name) syllables...")
+        let raw = (try? String(contentsOfFile: "Resources/Syllables/\(name).csv", encoding: .utf8)) ?? ""
         let lines = raw.split(separator: "\n")
         return Dictionary(uniqueKeysWithValues: lines.compactMap { line in
-            (try? linePattern.wholeMatch(in: line)).map { (String($0.word), Int($0.count)!) }
+            (try? linePattern.firstMatch(in: line)).map { (String($0.word), Int($0.count)!) }
         })
     }
 }
