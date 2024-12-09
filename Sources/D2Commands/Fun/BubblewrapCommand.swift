13b671c116053e14751d83320fbd9df6c5e0856a
fwcd <fwcd@fwcd.dev>
2024-12-09 03:00:05 +0100

    Make the number of bubbles customizable

--- Sources/D2Commands/Fun/BubblewrapCommand.swift
+++ Sources/D2Commands/Fun/BubblewrapCommand.swift
@@ -1,17 +1,19 @@
-public class BubblewrapCommand: VoidCommand {
+public class BubblewrapCommand: StringCommand {
     public let info = CommandInfo(
         category: .fun,
         shortDescription: "Some bubble wrap for you",
+        helpText: "Syntax: [bubbles]?",
         requiredPermissionLevel: .basic
     )
 
-    private let bubbles: Int
+    private let defaultBubbles: Int
 
-    public init(bubbles: Int = 128) {
-        self.bubbles = bubbles
+    public init(defaultBubbles: Int = 128) {
+        self.defaultBubbles = defaultBubbles
     }
 
-    public func invoke(output: any CommandOutput, context: CommandContext) async {
+    public func invoke(with input: String, output: any CommandOutput, context: CommandContext) async {
+        let bubbles = Int(input) ?? defaultBubbles
         await output.append(Array(repeating: "||pop!||", count: bubbles).joined(separator: " "))
     }
 }
